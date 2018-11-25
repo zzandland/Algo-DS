867cc07bdea4dc55a6de7671700c802ef31ba9aa
Si Yong Kim <zzandland@gmail.com>
2018-11-25 07:53:47 +0000

    Sync LeetCode submission - 3Sum (javascript)

--- problems/3sum/solution.js
+++ problems/3sum/solution.js
@@ -2,77 +2,44 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-var binarySort = function(arr) {
-  var sorted = [arr[0]];
-  var left, right;
-  for (var i = 1; i < arr.length; i++) {
-    left = 0;
-    right = sorted.length - 1;
-    var found = false;
-    while (right > left && !found) {
-      var mid = left + Math.floor((right - left) / 2);
-      if (sorted[mid] === arr[i]) {
-        sorted.splice(mid, 0, arr[i]);
-        found = true;
-      } else if (sorted[mid] > arr[i]) {
-        right = mid;
-      } else {
-        left = mid + 1;
-      }
-    }
-    if (!found) {
-      if (sorted[left] > arr[i]) {
-        sorted.splice(left, 0, arr[i]);
-      } else {
-        sorted.splice(left + 1, 0, arr[i]);
-      }
-    }
-  }
-  return sorted;  
-}
-
 var threeSum = function(nums) {
-  if (nums.length < 3) {
-    return [];
-  }
   var output = [];
-  var cuts = [];
-  var dic = {};
-  var set = new Set();
+  var temp;
+  var sorted;
   for (var i = 0; i < nums.length; i++) {
-    if (!dic[nums[i]]) {
-      dic[nums[i]] = 1;
-    } else {
-      dic[nums[i]] += 1;
+    sorted = true;
+    for (var j = 0; j < nums.length - 1; j++) {
+      if (nums[j] > nums[j + 1]) {
+        temp = nums[j + 1];
+        nums[j + 1] = nums[j];
+        nums[j] = temp;
+        sorted = false;
+      }
     }
+    if (sorted) break;
   }
-  for (var num in dic) {
-    var occur;
-    if (dic[num] > 3) {
-      occur = 3;
-    } else {
-      occur = dic[num];
+  var target, left, right, sum, subArr;
+  for (var j = 0; j < nums.length - 2; j++) {
+    if (nums[j] === nums[j - 1] && j < nums.length - 2) {
+      continue;
     }
-    while (occur > 0) {
-      cuts.push(parseInt(num, 10));
-      occur--;
-    }
-  }
-  var sorted = binarySort(cuts);
-  for (var j = 0; j < sorted.length; j++) {
-    var target = 0 - sorted[j];
-    var without = sorted.slice(0, j).concat(sorted.slice(j + 1));
-    var left = 0; 
-    var right = without.length - 1;
+    target = 0 - nums[j];
+    left = j + 1; 
+    right = nums.length - 1;
     while (right > left) {
-      var sum = without[left] + without[right];
-      if (sum === target) {
-        var subArr = binarySort([without[left], without[right], sorted[j]]);
-        if (!set.has(subArr.join(''))) {
-          output.push(subArr);
-          set.add(subArr.join(''));
-        }
+      if (nums[left] === nums[left - 1] && left < nums.length - 1 && left - 1 !== j) {
+        left++;
+        continue;
+      }
+      if (nums[right] === nums[right + 1] && right > left) {
         right--;
+        continue;
+      }
+      sum = nums[left] + nums[right];
+      if (sum === target) {
+        subArr = [nums[j], nums[left], nums[right]];
+        output.push(subArr);
+        left++;
       } else if (sum > target) {
         right--;
       } else {
