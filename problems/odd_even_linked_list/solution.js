a4c8c43bd3017a404cb972328fda9a508f67d8b5
Si Yong Kim <zzandland@gmail.com>
2018-12-22 17:19:53 +0000

    Sync LeetCode submission - Odd Even Linked List (javascript)

---
+++ problems/odd_even_linked_list/solution.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var oddEvenList = function(head) {
+  if (!head) return null;
+  var odd, even, evenStart;
+  odd = head;
+  even = evenStart = head.next;
+  while (odd && even && odd.next && even.next) {
+    odd.next = odd.next.next;
+    even.next = even.next.next;
+    odd = odd.next;
+    even = even.next;
+  }
+  odd.next = evenStart;
+  return head;
+};
