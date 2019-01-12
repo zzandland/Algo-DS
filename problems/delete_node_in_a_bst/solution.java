a8335125391d15c2b167a29b17affea56999c60c
Si Yong Kim <zzandland@gmail.com>
2019-01-12 01:32:32 +0000

    Sync LeetCode submission - Delete Node in a BST (java)

---
+++ problems/delete_node_in_a_bst/solution.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;zx
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+  public TreeNode deleteNode(TreeNode root, int key) {
+    if (root == null) return null;
+    if (root.val == key) {
+      if (root.left == null) return root.right;
+      if (root.right == null) return root.left;
+      TreeNode right = getSuccessor(root.right);
+      root.val = right.val;
+      root.right = deleteNode(root.right, right.val);
+      return root;
+    }
+    if (root.val > key) root.left = deleteNode(root.left, key);
+    else root.right = deleteNode(root.right, key);
+    return root;
+  }
+  
+  public TreeNode getSuccessor(TreeNode root) {
+    if (root.left == null) return root;
+    return getSuccessor(root.left);
+  }
+}
