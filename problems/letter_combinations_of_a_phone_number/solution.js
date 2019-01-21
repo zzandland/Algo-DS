b86c691eea3e27ca8eb7a1175fa82c38fc29b131
Si Yong Kim <zzandland@gmail.com>
2019-01-21 21:53:10 +0000

    Sync LeetCode submission - Letter Combinations of a Phone Number (javascript)

--- problems/letter_combinations_of_a_phone_number/solution.js
+++ problems/letter_combinations_of_a_phone_number/solution.js
@@ -2,46 +2,35 @@
  * @param {string} digits
  * @return {string[]}
  */
+const numbers = {
+  2: ['a', 'b', 'c'],
+  3: ['d', 'e', 'f'],
+  4: ['g', 'h', 'i'],
+  5: ['j', 'k', 'l'],
+  6: ['m', 'n', 'o'],
+  7: ['p', 'q', 'r', 's'],
+  8: ['t', 'u', 'v'],
+  9: ['w', 'x', 'y', 'z'],
+}
+
 var letterCombinations = function(digits) {
-  if (digits.length === 0) {
-    return [];
-  }
-  var dic = {};
-  var three;
-  var charCode = 97;
-  for (var i = 2; i < 10; i++) {
-    dic[i] = [];
-    if (i === 7 || i === 9) {
-      three = charCode + 4;
-    } else {
-      three = charCode + 3;
-    }
-    while (charCode < three) {
-      dic[i].push(String.fromCharCode(charCode));
-      charCode++;
-    }
-  }
-  var output = [];
-  var num;
-  var innerFunc = function(index, arr) {
-    if (!arr) {
-      arr = [];
-    }
-    if (index === digits.length) {
-      output.push(arr.join(''));
-      return;
-    }
-    num = digits.charAt(index);
-    if (num < 2 && num > 9) {
-      throw Error("invalid number at index: " + index);
-    }
-    var comb = dic[num];
-    for (var i = 0; i < comb.length; i++) {
-      arr.push(comb[i]);
-      innerFunc(index + 1, arr);
-      arr.pop();
-    }
-  }
-  innerFunc(0);
+  const output = [];
+  if (digits === '') return output;
+  helper(output, digits, [], 0);
   return output;
 };
+
+function helper(output, digits, arr, index) {
+  if (index === digits.length) {
+    output.push(arr.join(''));
+    return;
+  }
+  const chars = numbers[digits[index]];
+  for (let i = 0; i < chars.length; i++) {
+    arr.push(chars[i]);
+    index++;
+    helper(output, digits, arr, index);
+    arr.pop();
+    index--;
+  }
+}
