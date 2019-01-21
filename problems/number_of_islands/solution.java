ee4c0e891c9392534110448cf2e3299bfeb0c5df
Si Yong Kim <zzandland@gmail.com>
2019-01-21 21:35:40 +0000

    Sync LeetCode submission - Number of Islands (java)

--- problems/number_of_islands/solution.java
+++ problems/number_of_islands/solution.java
@@ -1,48 +1,23 @@
 class Solution {
   public int numIslands(char[][] grid) {
-        Queue<int[]> queue = new LinkedList<>();
-        int shapes = 0;
-        int[] coord = new int[2];
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[i].length; j++) {
-                if (grid[i][j] == '1') {
-                    shapes++;
-                    grid[i][j] = '0';
-                    coord[0] = i;
-                    coord[1] = j;
-                    queue.add(coord.clone());
-                    while (queue.peek() != null) {
-                        coord = queue.poll();
-                        int y = coord[0];
-                        int x = coord[1]; 
-                        if (y - 1 >= 0 && grid[y - 1][x] == '1') {
-                            coord[0] = y - 1;
-                            coord[1] = x;
-                            grid[y - 1][x] = '0';
-                            queue.add(coord.clone());
-                        }
-                        if (y + 1 < grid.length && grid[y + 1][x] == '1') {
-                            coord[0] = y + 1;
-                            coord[1] = x;
-                            grid[y + 1][x] = '0';
-                            queue.add(coord.clone());
-                        }
-                        if (x - 1 >= 0 && grid[y][x - 1] == '1') {
-                            coord[0] = y;
-                            coord[1] = x - 1;
-                            grid[y][x - 1] = '0';
-                            queue.add(coord.clone());
-                        }
-                        if (x + 1 < grid[i].length && grid[y][x + 1] == '1') {
-                            coord[0] = y;
-                            coord[1] = x + 1;
-                            grid[y][x + 1] = '0';
-                            queue.add(coord.clone());
-                        }
-                    }
-                }
-            }
+    int count = 0;
+    for (int i = 0; i < grid.length; i++) {
+      for (int j = 0; j < grid[0].length; j++) {
+        if (grid[i][j] == '1') {
+          count++;
+          helper(grid, i, j);
         }
-        return shapes;
+      }
+    }
+    return count;
+  }
+  
+  private void helper(char[][] grid, int x, int y) {
+    if (grid[x][y] == '0') return;
+    grid[x][y] = '0';
+    if (x - 1 >= 0) helper(grid, x - 1, y);
+    if (x + 1 < grid.length) helper(grid, x + 1, y);
+    if (y - 1 >= 0) helper(grid, x, y - 1);
+    if (y + 1 < grid[0].length) helper(grid, x, y + 1);
   }
 }
