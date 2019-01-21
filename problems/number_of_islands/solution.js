cea2d083a5f1d461d25cfa15f03ac10b397bf213
Si Yong Kim <zzandland@gmail.com>
2019-01-21 21:29:53 +0000

    Sync LeetCode submission - Number of Islands (javascript)

--- problems/number_of_islands/solution.js
+++ problems/number_of_islands/solution.js
@@ -3,58 +3,23 @@
  * @return {number}
  */
 var numIslands = function(grid) {
-    debugger;
-  var output = 0;
-  var set = new Set();
-  var queue;
-  var checkIsland = function(x, y) {
-    if (!set.has([x, y].toString())) {
-      output++;
-      queue = [[x, y]];
-      var coor;
-      while (queue.length) {
-          coor = queue.pop();
-          var X = coor[0];
-          var Y = coor[1];
-          set.add(coor.toString());
-          if (
-            grid[X + 1] 
-            && grid[X + 1][Y] === '1'
-            && !set.has([X + 1, Y].toString())) {
-                set.add([X + 1, Y].toString());
-            queue.unshift([X + 1, Y]);
-          }
-          if (
-            grid[X - 1] 
-            && grid[X - 1][Y] === '1'
-            && !set.has([X - 1, Y].toString())) {
-                set.add([X - 1, Y].toString());
-            queue.unshift([X - 1, Y]);
-          }
-          if (
-            grid[X][Y + 1] 
-            && grid[X][Y + 1] === '1'
-            && !set.has([X, Y + 1].toString())) {
-                set.add([X, Y + 1].toString());
-            queue.unshift([X, Y + 1]);
-          }
-          if (
-            grid[X][Y - 1] 
-            && grid[X][Y - 1] === '1'
-            && !set.has([X, Y - 1].toString())) {
-                set.add([X, Y - 1].toString());
-            queue.unshift([X, Y - 1]);
-          }    
-        
-      }
-    }
-  }
-  for (var i = 0; i < grid.length; i++) {
-    for (var j = 0; j < grid[i].length; j++) {
+  let count = 0;
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[0].length; j++) {
       if (grid[i][j] === '1') {
-        checkIsland(i, j);
+        count++;
+        helper(grid, i, j); 
       }
-    }
+    } 
   }
-  return output;
+  return count;
 };
+
+function helper(grid, x, y) {
+  if (!grid[x] || !grid[x][y] || grid[x][y] === '0') return;
+  grid[x][y] = '0';
+  helper(grid, x - 1, y);
+  helper(grid, x + 1, y);
+  helper(grid, x, y - 1);
+  helper(grid, x, y + 1);
+}
