96fd69bd1138b1e96d21ad16220facdc2e53ce50
Si Yong Kim <zzandland@gmail.com>
2019-01-22 19:58:26 +0000

    Sync LeetCode submission - Guess Number Higher or Lower (java)

--- problems/guess_number_higher_or_lower/solution.java
+++ problems/guess_number_higher_or_lower/solution.java
@@ -4,20 +4,16 @@
       int guess(int num); */
 
 public class Solution extends GuessGame {
-    public int guessNumber(int n) {
-      int mid;
-      int left = 1;
-      int right = n;
-      do {
-        mid = (left + (right - left) / 2);
-        if (guess(mid) == 0) {
-          return mid;
-        } else if (guess(mid) == 1) {
-          left = mid + 1;
-        } else if (guess(mid) == -1) {
-          right = mid - 1;
-        }
-      } while (right > left);
-      return left;
+  public int guessNumber(int n) {
+    int left = 1;
+    int right = n;
+    int mid;
+    while (right >= left) {
+      mid = left + (right - left) / 2;
+      if (guess(mid) == 0) return mid;
+      if (guess(mid) == 1) left = mid + 1;
+      else right = mid - 1;
     }
+    return left;
+  }
 }
