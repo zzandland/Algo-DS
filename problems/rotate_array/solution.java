57571b302e59134220b8107e0d467e706855de50
Si Yong Kim <zzandland@gmail.com>
2019-02-06 21:37:50 +0000

    Sync LeetCode submission - Rotate Array (java)

--- problems/rotate_array/solution.java
+++ problems/rotate_array/solution.java
@@ -1,18 +1,19 @@
-import java.util.*;
-
 class Solution {
   public void rotate(int[] nums, int k) {
-    List<Integer> numsList = new ArrayList<Integer>(nums.length);
-    for (int i = 0; i < nums.length; i++) {
-      numsList.add(nums[i]);
-    }
-    System.out.println(numsList);
-    for (int j = 0 ; j < k; j++) {
-      int temp = numsList.remove(numsList.size() - 1);
-      numsList.add(0, temp);
-    }
-    for (int l = 0; l < nums.length; l++) {
-      nums[l] = numsList.get(l);
+    if (k == 0) return;
+    k %= nums.length;
+    int count = 0;
+    for (int start = 0; count < nums.length; start++) {
+      int pos = start;
+      int prev = nums[start];
+      do {
+        int next = (pos + k) % nums.length;
+        int save = nums[next];
+        nums[next] = prev;
+        pos = next;
+        prev = save;
+        count++;
+      } while (pos != start);
     }
   }
 }
