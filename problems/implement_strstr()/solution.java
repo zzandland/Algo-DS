d9a5714117a3b4778d618c738f41fa89b1d03400
Si Yong Kim <zzandland@gmail.com>
2019-02-17 07:27:34 +0000

    Sync LeetCode submission - Implement strStr() (java)

--- problems/implement_strstr()/solution.java
+++ problems/implement_strstr()/solution.java
@@ -1,16 +1,12 @@
+import java.util.regex.*;
+
 class Solution {
-    public int strStr(String haystack, String needle) {
-      if (needle.length() == 0) return 0;
-      char[] haystackArr = haystack.toCharArray();
-      char[] needleArr = needle.toCharArray();
-      for (int i = 0; i < haystackArr.length; i++) {
-        if (haystackArr[i] == needleArr[0]) {
-          for (int j = 0; j < needleArr.length; j++) {
-            if (i + j == haystackArr.length || haystackArr[i + j] != needleArr[j]) break; 
-            if (j == needleArr.length - 1) return i;
-          }
-        }
-      } 
-      return -1;
+  public int strStr(String haystack, String needle) {
+    if (needle.length() == 0) return 0;
+    Matcher m = Pattern.compile(needle).matcher(haystack);
+    if (m.find()) {
+      return m.start();
     }
+    return -1;
+  }
 }
