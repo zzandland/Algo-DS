2c279f3cfd863e238f1e2bf1f7891c7493fa5a2a
Si Yong Kim <zzandland@gmail.com>
2019-02-19 22:35:31 +0000

    Sync LeetCode submission - Longest Common Prefix (java)

--- problems/longest_common_prefix/solution.java
+++ problems/longest_common_prefix/solution.java
@@ -1,22 +1,30 @@
 class Solution {
-    public String longestCommonPrefix(String[] strs) {
-      if (strs.length == 0) return "";
-      StringBuilder output = new StringBuilder();
-      int min = strs[0].length();
-      boolean isSame = true;
-      for (String str : strs) {
-        if (str.length() < min) min = str.length();
-      }
-      for (int i = 0; i < min && isSame; i++) {
-        char common = strs[0].charAt(i);
-        for (int j = 1; j < strs.length; j++) {
-          if (strs[j].charAt(i) != common) {
-            isSame = false;
-            break;
-          } 
-        }
-        if (isSame) output.append(common);
-      }
-      return output.toString();
+  public String longestCommonPrefix(String[] strs) {
+    if (strs.length <= 0) return "";
+    StringBuilder output = new StringBuilder();
+    String smallest = getSmallestLen(strs);
+    char cha;
+    boolean matched = true;
+    for (int i = 0; matched && i < smallest.length(); i++) {
+      cha = smallest.charAt(i);
+      if (checkCharMatch(strs, i, cha)) output.append(cha);
+      else matched = false;
     }
+    return output.toString();
+  }
+  
+  private boolean checkCharMatch(String strs[], int index, char cha) {
+    for (String str : strs) {
+      if (str.charAt(index) != cha) return false; 
+    }
+    return true;
+  }
+  
+  private String getSmallestLen(String strs[]) {
+    String smallest = strs[0];
+    for (int i = 0; i < strs.length; i++) {
+      if (strs[i].length() < smallest.length()) smallest = strs[i];
+    }
+    return smallest;
+  }
 }
