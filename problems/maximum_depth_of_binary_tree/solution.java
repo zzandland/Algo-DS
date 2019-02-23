670c293e9c2a790a1614c14131ce2062dd5a99f7
Si Yong Kim <zzandland@gmail.com>
2019-02-23 21:17:05 +0000

    Sync LeetCode submission - Maximum Depth of Binary Tree (java)

--- problems/maximum_depth_of_binary_tree/solution.java
+++ problems/maximum_depth_of_binary_tree/solution.java
@@ -10,21 +10,7 @@
 
 class Solution {
   public int maxDepth(TreeNode root) {
-    int output = 0;
-    if (root == null) return output;
-    return traversal(root, 1, output);
-  }
-  
-  public int traversal(TreeNode node, int depth, int max) {
-    max = Math.max(depth, max);
-    if (node.left != null) {
-      max = traversal(node.left, ++depth, max);
-      depth--;
-    }
-    if (node.right != null) {
-      max = traversal(node.right, ++depth, max);  
-      depth--;
-    }
-    return max;
+    if (root == null) return 0;
+    return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
   }
 }
