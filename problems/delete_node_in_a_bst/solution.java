e5d14201efa21a55ac68a0d8f0a7c7ddea2ab701
Si Yong Kim <zzandland@gmail.com>
2019-02-24 17:26:38 +0000

    Sync LeetCode submission - Delete Node in a BST (java)

--- problems/delete_node_in_a_bst/solution.java
+++ problems/delete_node_in_a_bst/solution.java
@@ -9,22 +9,34 @@
  */
 class Solution {
   public TreeNode deleteNode(TreeNode root, int key) {
-    if (root == null) return null;
+    if (root == null) return root;
     if (root.val == key) {
-      if (root.left == null) return root.right;
-      if (root.right == null) return root.left;
-      TreeNode right = getSuccessor(root.right);
-      root.val = right.val;
-      root.right = deleteNode(root.right, right.val);
-      return root;
+      // if target has both child
+      if (root.left != null && root.right != null) {
+        TreeNode next = findSuccessor(root.right);
+        root.val = next.val;
+        root.right = deleteNode(root.right, next.val);
+        return root;
+      }
+      // if target has left branch
+      if (root.left != null) {
+        return root.left;
+      }  
+      // if target has right branch
+      if (root.right != null) {
+        return root.right;
+      }
+      // if node is a leaf
+      return null;
     }
     if (root.val > key) root.left = deleteNode(root.left, key);
     else root.right = deleteNode(root.right, key);
+    
     return root;
-  }
+  }  
   
-  public TreeNode getSuccessor(TreeNode root) {
-    if (root.left == null) return root;
-    return getSuccessor(root.left);
+  private TreeNode findSuccessor(TreeNode root) {
+    if (root.left != null) return findSuccessor(root.left);
+    return root;
   }
 }
