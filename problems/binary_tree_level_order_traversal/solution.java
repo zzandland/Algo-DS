7c1bc093c8c9507af934ac09a5895a4c7e21020a
Si Yong Kim <zzandland@gmail.com>
2019-02-28 20:34:21 +0000

    Sync LeetCode submission - Binary Tree Level Order Traversal (java)

--- problems/binary_tree_level_order_traversal/solution.java
+++ problems/binary_tree_level_order_traversal/solution.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Queue;
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -7,60 +10,23 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
-
-class TreeNodeQueue {
-  private static class QueueNode {
-    private TreeNode data;
-    private QueueNode next;
-    public QueueNode(TreeNode node) {
-      this.data = node;
-    }
-  }
-  private QueueNode first;
-  private QueueNode last;
-  private int size = 0;
-  
-  public void enqueue(TreeNode node) {
-    size++;
-    QueueNode t = new QueueNode(node);
-    if (last != null) last.next = t;
-    last = t;
-    if (first == null) first = last;
-  }
-  
-  public TreeNode dequeue() {
-    size--;
-    TreeNode n = first.data;
-    first = first.next;
-    if (first == null) last = null; 
-    return n;
-  }
-  
-  public boolean isEmpty() {
-    return first == null;
-  }
-  
-  public int size() {
-    return size;
-  }
-}
-
 class Solution {
   public List<List<Integer>> levelOrder(TreeNode root) {
     List<List<Integer>> output = new ArrayList<List<Integer>>();
     if (root == null) return output;
-    TreeNodeQueue queue = new TreeNodeQueue();
-    queue.enqueue(root);
-    while (!queue.isEmpty()) {
-      List<Integer> level = new ArrayList<Integer>();
-      int size = queue.size();
+    Queue<TreeNode> q = new LinkedList<TreeNode>();
+    
+    q.add(root);
+    while (!q.isEmpty()) {
+      int size = q.size();
+      List<Integer> sub = new ArrayList<Integer>();
       for (int i = 0; i < size; i++) {
-        TreeNode node = queue.dequeue();
-        level.add(node.val);
-        if (node.left != null) queue.enqueue(node.left);
-        if (node.right != null) queue.enqueue(node.right);
+        TreeNode node = q.poll();
+        sub.add(node.val);
+        if (node.left != null) q.add(node.left);
+        if (node.right != null) q.add(node.right);
       }
-      output.add(level);
+      output.add(sub);
     }
     return output;
   }
