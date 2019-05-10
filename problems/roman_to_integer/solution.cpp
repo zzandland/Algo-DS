fbf66a75d5ecabc46dc55bef8831f94f1b4c9cb9
Si Yong Kim <zzandland@gmail.com>
2019-05-10 22:46:32 +0000

    Sync LeetCode submission - Roman to Integer (cpp)

--- problems/roman_to_integer/solution.cpp
+++ problems/roman_to_integer/solution.cpp
@@ -1,24 +1,14 @@
 class Solution {
 public:
   int romanToInt(string s) {
-    map<char, int> dic = {
-      {'I', 1},
-      {'V', 5},
-      {'X', 10},
-      {'L', 50},
-      {'C', 100},
-      {'D', 500},
-      {'M', 1000}
-    };
-    int sum = 0;
-    for (size_t i = 0; i < s.length(); ++i) {
-      sum += dic[s[i]];
+    int output = 0;
+    map<char, int> roman = {{'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}};
+    for (int i = 0; i < (int)s.length(); ++i) {
+      if (i + 1 < (int)s.length() && roman[s[i + 1]] > roman[s[i]])
+        output -= roman[s[i]];
+      else
+        output += roman[s[i]];
     }
-    
-    if (s.find("IV") != string::npos || s.find("IX") != string::npos) sum -= 2;
-    if (s.find("XL") != string::npos || s.find("XC") != string::npos) sum -= 20;
-    if (s.find("CD") != string::npos || s.find("CM") != string::npos) sum -= 200;
-
-    return sum;
+    return output;
   }
 };
