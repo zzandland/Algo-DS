82ede347b976823424d9a038c70bb6ffbbeec5f3
Si Yong Kim <zzandland@gmail.com>
2019-05-16 03:17:53 +0000

    Sync LeetCode submission - Group Anagrams (cpp)

--- problems/group_anagrams/solution.cpp
+++ problems/group_anagrams/solution.cpp
@@ -1,32 +1,23 @@
 class Solution {
 public:
-  void CompareString(string str, map<vector<int>, vector<string>>& dic) {
-    vector<int> count = LetterCount(str);
-    if (dic.find(count) == dic.end()) {
-      dic.insert({count, {str}});
-    } else {
-      dic[count].push_back(str);
-    }
-  }
-  
-  vector<int> LetterCount(string str) {
-    vector<int> count(27);
-    for (int i = 0; i < str.length(); i++) {
-      count[(int)(str.at(i) - 'a')]++;
-    }
-    return count;
-  }
-  
-  vector<vector<string>> groupAnagrams(vector<string>& strs) {
-    map<vector<int>, vector<string>> dic;
-    for (string str : strs) {
-      CompareString(str, dic);
-    }
-    vector<vector<string>> output;
-    for (auto it = dic.begin(); it != dic.end(); ++it) {
-      output.push_back(it->second);
-    }
+  string generateCharCount(string s) {
+    vector<int> counts(27);
+    for (char c : s)  
+      counts[c - 'a']++;
+    string output = "";
+    for (int i : counts)
+      output += i - '0';
     return output;
   }
   
+  vector<vector<string>> groupAnagrams(vector<string>& strs) {
+    unordered_map<string, vector<string>> charMap;
+    for (string s : strs) {
+      charMap[generateCharCount(s)].push_back(s);
+    }
+    vector<vector<string>> output;
+    for (auto it = charMap.begin(); it != charMap.end(); ++it)
+      output.push_back(it->second);
+    return output;
+  }
 };
