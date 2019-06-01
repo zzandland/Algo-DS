4c86ea16bdfd088a0c3c0f079de51e559e67e0c9
Si Yong Kim <zzandland@gmail.com>
2019-06-01 16:13:10 +0000

    Sync LeetCode submission - Longest Substring Without Repeating Characters (cpp)

--- problems/longest_substring_without_repeating_characters/solution.cpp
+++ problems/longest_substring_without_repeating_characters/solution.cpp
@@ -1,28 +1,64 @@
-#include <vector>
-
+/*
+ * @lc app=leetcode id=3 lang=cpp
+ *
+ * [3] Longest Substring Without Repeating Characters
+ *
+ * https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+ *
+ * algorithms
+ * Medium (28.38%)
+ * Total Accepted:    934.4K
+ * Total Submissions: 3.3M
+ * Testcase Example:  '"abcabcbb"'
+ *
+ * Given a string, find the length of the longest substring without repeating
+ * characters.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: "abcabcbb"
+ * Output: 3 
+ * Explanation: The answer is "abc", with the length of 3. 
+ * 
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: "bbbbb"
+ * Output: 1
+ * Explanation: The answer is "b", with the length of 1.
+ * 
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: "pwwkew"
+ * Output: 3
+ * Explanation: The answer is "wke", with the length of 3. 
+ * ⁠            Note that the answer must be a substring, "pwke" is a
+ * subsequence and not a substring.
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
 class Solution {
 public:
   int lengthOfLongestSubstring(string s) {
-    vector<int> c_map(128, -1);
-    int l, r, longest;
-    l = -1; r = 0, longest = 0;
-    for (; r < (int)s.length(); ++r) {
-      int c = s[r];
-      if (c_map[c] != -1)
-        l = max(l, c_map[c]);
-      c_map[c] = r;
-      longest = max(longest, r - l);
+    vector<int> c_set(128, -1);
+    int max_len = 0;
+    for (int i = 0, j = 0; i < s.length(); ++i) {
+      char c = s[i];
+      if (c_set[c] >= j)
+        j = c_set[c] + 1;
+      c_set[c] = i;
+      max_len = max(max_len, i - j + 1);
     }
-    return longest;
-  }
-  
-  int lengthOfLongestSubstring(string s, int l, int r, set<char>& c_set) {
-    if (r == (int)s.length()) return 0;
-    char c = s[r];
-    if (c_set.find(c) == c_set.end())
-      c_set.insert(c);
-    else 
-      while (s[++l] != c) c_set.erase(s[l]);
-    return max(r - l, lengthOfLongestSubstring(s, l, r + 1, c_set));
+    return max_len;
   }
 };
