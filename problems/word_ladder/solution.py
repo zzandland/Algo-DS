9eaa8e5ca62625c1be7468e3f29bafcad6af2116
Si Yong Kim <zzandland@gmail.com>
2019-06-06 21:52:19 +0000

    Sync LeetCode submission - Word Ladder (python3)

--- problems/word_ladder/solution.py
+++ problems/word_ladder/solution.py
@@ -1,27 +1,33 @@
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        word_set = set(wordList)
-        if endWord not in word_set:
+        wordSet = set(wordList)
+        if endWord not in wordSet:
             return 0
-        q = collections.deque()
-        q.append(beginWord)
-        word_dic = collections.defaultdict(list)  
+        wordDic = {}
         for word in wordList:
             for i in range(len(word)):
-                word_dic[word[:i] + "*" + word[i+1:]].append(word)
-        output = 0
-        while len(q) > 0:
-            output += 1
-            k = len(q)
-            for i in range(k):
-                curr = q.popleft()
-                for j in range(len(curr)):
-                    tmp = curr[:j] + "*" + curr[j+1:]
-                    for match in word_dic[tmp]:
-                        if match == endWord:
-                            return output + 1
-                        if match in word_set:
-                            q.append(match)
-                            word_set.remove(match)
-                    word_dic[tmp] = []
+                tmp = word[:i] + "*" + word[i+1:]
+                if tmp not in wordDic:
+                    wordDic[tmp] = []
+                wordDic[tmp].append(word)
+        q = (collections.deque(), collections.deque())        
+        q[0].append(beginWord)
+        q[1].append(endWord)    
+        wordSet.remove(endWord)
+        l = [0, 1]
+        while len(q[0]) > 0 or len(q[1]) > 0:
+            for i in range(2):
+                l[i] += 1
+                for j in range(len(q[i])):
+                    word = q[i].popleft()
+                    for k in range(len(word)):
+                        tmp = word[:k] + "*" + word[k+1:]
+                        if tmp in wordDic:
+                            for match in wordDic[tmp]:
+                                if match in q[(i + 1) % 2]:
+                                    return l[0] + l[1]
+                                if match in wordSet:
+                                    q[i].append(match)
+                                    wordSet.remove(match)
+                        wordDic.pop(tmp, None)            
         return 0                
