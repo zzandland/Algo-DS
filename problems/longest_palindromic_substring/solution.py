30c2148bdcda42dbd645466c2be585637f23a067
Si Yong Kim <zzandland@gmail.com>
2019-06-15 21:08:14 +0000

    Sync LeetCode submission - Longest Palindromic Substring (python3)

--- problems/longest_palindromic_substring/solution.py
+++ problems/longest_palindromic_substring/solution.py
@@ -1,21 +1,16 @@
 class Solution:
-    def longestPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: str
-        """
-        dic = {}
-        output = ''
-        for i in range(len(s)):
-          if s[i] not in dic:
-            dic[s[i]] = [i]
-            if output == '':
-              output = s[i]
-          else:
-            dic[s[i]].append(i)
-            for j in range(len(dic[s[i]]) - 1):
-              substr = s[dic[s[i]][j]:i + 1]
-              if len(substr) > len(output):
-                if substr == substr[::-1]:
-                  output = substr
-        return output
+    def longestPalindrome(self, s: str) -> str:
+        dp = [[False for _, _ in enumerate(s)] for _, _ in enumerate(s)]
+        for i, _ in enumerate(s):
+            dp[i][i] = True
+        max_len, max_coord = 0, (0, 0)
+        for i in range(len(s) - 2, -1, -1):
+            for j in range(i + 1, len(s)):
+                if s[i] == s[j]:
+                    new_len = j - i + 1
+                    if new_len == 2 or dp[i + 1][j - 1]:
+                        if new_len > max_len:
+                            max_len = new_len
+                            max_coord = (i, j)
+                        dp[i][j] = True
+        return s[max_coord[0]: max_coord[1] + 1]            
