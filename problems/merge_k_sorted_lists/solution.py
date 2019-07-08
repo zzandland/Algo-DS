ac3ef253bfe51fb30c49e9ca894aaaf0fa716c69
Si Yong Kim <zzandland@gmail.com>
2019-07-08 03:13:23 +0000

    Sync LeetCode submission - Merge k Sorted Lists (python3)

--- problems/merge_k_sorted_lists/solution.py
+++ problems/merge_k_sorted_lists/solution.py
@@ -1,31 +1,26 @@
-from typing import Dict
-
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
 
+import random
+
 class Solution:
     def mergeKLists(self, lists: List[ListNode]) -> ListNode:
-        node_map = self.generate_map(lists)
-        head, first = None, None
-        for _, lst in sorted(node_map.items()):
-            for node in lst:
-                if head is not None:
-                    head.next = node
-                head = node
-                if first is None:
-                    first = head
-        return first            
-    
-    def generate_map(self, lists: List[ListNode]) -> Dict[int, List[ListNode]]:
-        node_map = {}
-        for lstNode in lists:
-            while lstNode is not None:
-                if lstNode.val not in node_map:
-                    node_map[lstNode.val] = [lstNode]
-                else:
-                    node_map[lstNode.val].append(lstNode)    
-                lstNode = lstNode.next    
-        return node_map        
+        hp, n, merge, out = [], None, None, None
+        for lst in lists:
+            if lst:
+                heapq.heappush(hp,(lst.val, str(lst), lst))
+        while hp:    
+            _, s, n = heapq.heappop(hp)    
+            tmp = n
+            n = n.next
+            if n:
+                heapq.heappush(hp, (n.val, s, n))
+            if merge:
+                merge.next = tmp
+            merge = tmp    
+            if not out:
+                out = merge
+        return out        
