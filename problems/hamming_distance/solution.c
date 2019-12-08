e119d9f18fc53bac03f4e525cedf3ed19d05bece
Si Yong Kim <zzandland@gmail.com>
2019-12-08 06:24:13 +0000

    Sync LeetCode submission - Hamming Distance (c)

---
+++ problems/hamming_distance/solution.c
@@ -0,0 +1,11 @@
+int hammingDistance(int x, int y) {
+  int distance = 0;
+  while (x > 0 || y > 0) {
+    if ((x & 1) ^ (y & 1)) {
+      distance++;
+    }
+    x >>= 1;
+    y >>= 1;
+  }
+  return distance;
+}
