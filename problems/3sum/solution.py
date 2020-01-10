061a38d26bb20433d410f1fe140341f6ddf0ac34
Si Yong Kim <zzandland@gmail.com>
2020-01-10 08:08:01 +0000

    Sync LeetCode submission - 3Sum (python3)

--- problems/3sum/solution.py
+++ problems/3sum/solution.py
@@ -1,18 +1,22 @@
 class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
-        nums.sort()
-        output, dup = [], set()
-        for i in range(len(nums) - 2):
-            if i > 0 and nums[i] == nums[i-1]:
-                continue
-            l, r = i+1, len(nums)-1
-            diff = -1 * nums[i]
-            while l < r:
-                if nums[l] + nums[r] == diff and (nums[i], nums[l], nums[r]) not in dup: 
-                    output.append([nums[i], nums[l], nums[r]])
-                    dup.add((nums[i], nums[l], nums[r]))
-                if nums[l] + nums[r] < diff:
-                    l += 1
-                else:
-                    r -= 1    
-        return output            
+  def threeSum(self, nums: List[int]) -> List[List[int]]:
+    output = []
+    if len(nums) > 2:
+      o = set()
+      nums.sort()
+      for i, num in enumerate(nums):
+        if i > 0 and num == nums[i - 1]:
+          continue
+        l, r = i + 1, len(nums) - 1
+        while l < r:
+          lNum, rNum = nums[l], nums[r]
+          totalSum = num + lNum + rNum
+          if totalSum == 0:
+            o.add((num, lNum, rNum))
+          elif totalSum < 0:  
+            l += 1
+            continue
+          r -= 1  
+      for triple in o:
+        output.append(list(triple))    
+    return output    
