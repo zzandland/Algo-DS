83f0d1d80f5850a99adbace5c2868155cd961c2a
Si Yong Kim <zzandland@gmail.com>
2020-01-20 02:11:44 +0000

    Sync LeetCode submission - Group Anagrams (python3)

--- problems/group_anagrams/solution.py
+++ problems/group_anagrams/solution.py
@@ -1,6 +1,14 @@
+from collections import defaultdict
+
 class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        dic = collections.defaultdict(list)
-        for word in strs:
-            dic[''.join(sorted(word))].append(word)
-        return list(dic.values())    
+  def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+    dic = defaultdict(list);
+    for word in strs:
+      lst = [0] * 27
+      for c in word:
+        lst[ord(c) - ord('a')] += 1
+      dic[str(lst)].append(word)
+    output = []  
+    for _, v in dic.items():
+      output.append(v)
+    return output  
