16644095d9ab75a54bfdc4476f126c7d75402e6b
Si Yong Kim <zzandland@gmail.com>
2020-03-02 00:15:03 -0800

    Solve 47 with c++

--- leetcode/47.permutations-ii.cpp
+++ leetcode/47.permutations-ii.cpp
@@ -29,12 +29,49 @@
  */
 #include <iostream>
 #include <vector>
+#include <set>
 
 using namespace std;
 
 class Solution {
 public:
   vector<vector<int>> permuteUnique(vector<int>& nums) {
-    
+    vector<vector<int>> output;
+    if (nums.size() == 0) {
+      output.push_back(vector<int>());
+      return output;
+    }
+    if (nums.size() == 1) {
+      output.push_back(nums);
+      return output;
+    }
+    vector<int> st;
+    while (nums.size() > 1) {
+      st.push_back(nums.back());
+      nums.pop_back();
+    }
+    int out;
+    set<vector<int>> prev_set;
+    set<vector<int>> out_set;
+    prev_set.insert(nums);
+    while (!st.empty()) {
+      out = st.back();
+      st.pop_back();
+      out_set.clear();
+      for (auto it = prev_set.begin(); it != prev_set.end(); ++it) {
+        vector<int> cpy = *it;
+        for (int i = 0; i <= cpy.size(); ++i) {
+          cpy.insert(cpy.begin() + i, out);
+          out_set.insert(cpy);
+          cpy.erase(cpy.begin() + i);
+        }
+      }
+      prev_set = out_set;
+    }
+    for (auto it = out_set.begin(); it != out_set.end(); ++it) {
+      vector<int> v = *it;
+      output.push_back(v);
+    }
+    return output;
   }
 };
