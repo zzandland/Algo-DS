d9460bf53588e8fc305c1d6e609355b4350f37bd
Si Yong Kim <zzandland@gmail.com>
2020-03-31 07:19:24 +0000

    Sync LeetCode submission - Copy List with Random Pointer (python3)

--- problems/copy_list_with_random_pointer/solution.py
+++ problems/copy_list_with_random_pointer/solution.py
@@ -1,35 +1,23 @@
 """
 # Definition for a Node.
 class Node:
-    def __init__(self, val, next, random):
-        self.val = val
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
         self.next = next
         self.random = random
 """
+
 class Solution:
     def copyRandomList(self, head: 'Node') -> 'Node':
-        if head is None:
-            return None
-        copy = Node(head.val, None, None)
-        copy_map = {head: copy}
-        copy_head = None
-        while head is not None:
-            if head.next is not None:
-                if head.next in copy_map:
-                    copy.next = copy_map[head.next]
-                else:
-                    next_node = Node(head.next.val, None, None)
-                    copy.next = next_node
-                    copy_map[head.next] = next_node
-            if head.random is not None:
-                if head.random in copy_map:
-                    copy.random = copy_map[head.random]
-                else:
-                    rand_node = Node(head.random.val, None, None)
-                    copy.random = rand_node
-                    copy_map[head.random] = rand_node                
-            if copy_head is None:
-                copy_head = copy       
-            head = head.next
-            copy = copy.next    
-        return copy_head     
+        dic, cur = {}, head
+        dic[None] = None
+        while cur:
+            dic[cur] = Node(cur.val)
+            cur = cur.next
+        cur = head    
+        while cur:
+            n = dic[cur]
+            n.next = dic[cur.next]
+            n.random = dic[cur.random]
+            cur = cur.next
+        return dic[head]    
