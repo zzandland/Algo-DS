f417f0c36e99e052db4542e4a845d0614fcc114f
Si Yong Kim <zzandland@gmail.com>
2020-03-31 07:38:43 +0000

    Sync LeetCode submission - Merge Two Sorted Lists (python3)

--- problems/merge_two_sorted_lists/solution.py
+++ problems/merge_two_sorted_lists/solution.py
@@ -6,25 +6,15 @@
 
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
-        if l1 is None:
-            return l2
-        if l2 is None:
-            return l1
-        output = runner = None
-        while l1 is not None and l2 is not None:    
-            if l1.val > l2.val:
-                tmp = l2
-                l2 = l2.next
-            else:
-                tmp = l1    
+        head = ListNode(0)
+        n = head
+        while l1 and l2:
+            if l1.val < l2.val:
+                n.next = l1
                 l1 = l1.next
-            if runner is not None:
-                runner.next = tmp
-            runner = tmp    
-            if output is None:
-                output = runner
-        if l1 is not None:
-            runner.next = l1        
-        elif l2 is not None:
-            runner.next = l2    
-        return output    
+            else:
+                n.next = l2
+                l2 = l2.next
+            n = n.next    
+        n.next = l1 or l2        
+        return head.next
