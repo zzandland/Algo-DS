d5e9270508cf6ff27db361c95a9656be83aa6633
Si Yong Kim <zzandland@gmail.com>
2020-04-02 21:59:15 +0000

    Sync LeetCode submission - Longest Palindromic Substring (python3)

--- problems/longest_palindromic_substring/solution.py
+++ problems/longest_palindromic_substring/solution.py
@@ -1,15 +1,12 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        dp = [[False for _ in range(len(s))] for _ in range(len(s))]
-        for a in range(len(s)):
-            dp[a][a] = True
-        max_len, coord = 1, (0, 0)    
-        for i in range(len(s) - 2, -1, -1):
-            for j in range(i + 1, len(s)):
-                if s[i] == s[j]:
-                    if j - i == 1 or dp[i + 1][j - 1]:
-                        dp[i][j] = True
-                        if j - i + 1 > max_len:
-                            max_len = j - i + 1
-                            coord = (i, j)
-        return s[coord[0]:coord[1] + 1]                
+        dp, out = [[False for _ in range(len(s))] for _ in range(len(s))], ''
+        for i in range(len(s)):
+            dp[i][i] = True
+            out = s[i]
+        for c in range(1, len(s)):
+            for r in range(c-1, -1, -1):
+                if s[r] == s[c] and (dp[r+1][c-1] or c-r == 1):
+                    dp[r][c] = True
+                    if c-r+1 > len(out): out = s[r:c+1]
+        return out        
