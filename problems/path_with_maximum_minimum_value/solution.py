494309fafc7dc37392ab22e1191e3a5b128b185a
Si Yong Kim <zzandland@gmail.com>
2020-04-08 06:45:42 +0000

    Sync LeetCode submission - Path With Maximum Minimum Value (python3)

--- problems/path_with_maximum_minimum_value/solution.py
+++ problems/path_with_maximum_minimum_value/solution.py
@@ -1,17 +1,20 @@
-import heapq
-
 class Solution:
     def maximumMinimumPath(self, A: List[List[int]]) -> int:
-        row, col = len(A), len(A[0])
-        hp, min_, visited = [(-A[0][0], 0, 0)], A[0][0], [[False for _ in range(col)] for _ in range(row)]
-        dir_ = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-        while True:
-            val, y, x = heapq.heappop(hp)
-            visited[y][x] = True
-            min_ = min(min_, A[y][x])
-            if y == row-1 and x == col-1: return min_
-            for r, c in dir_:
-                ny, nx = y+r, x+c
-                if 0 <= ny < row and 0 <= nx < col and not visited[ny][nx]:
-                    heapq.heappush(hp, (-A[ny][nx], ny, nx))
-        return min_
+        R, C = len(A), len(A[0])
+        uf, seen, dir_ = [i for i in range(R * C)], [[False for _ in range(C)] for _ in range(R)], [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        def find(x: int) -> int:
+            if x != uf[x]:
+                uf[x] = find(uf[x])
+            return uf[x]
+        def union(x: int, y: int) -> None:
+            ux, uy = find(x), find(y)
+            if ux != uy:
+                uf[ux] = uy
+        for y, x in sorted([(r, c) for r in range(R) for c in range(C)], key=lambda x: A[x[0]][x[1]], reverse=True):
+            seen[y][x] = True
+            for ver, hor in dir_:
+                ny, nx = y+ver, x+hor
+                if 0 <= ny < R and 0 <= nx < C and seen[ny][nx]:
+                    union(ny*C+nx, y*C+x)
+                if find(0) == find(R*C-1):
+                    return A[y][x]
