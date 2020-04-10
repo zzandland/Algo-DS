df4c69abb858f17c83bdda9c8af9e20e7eb6aaba
Si Yong Kim <zzandland@gmail.com>
2020-04-10 23:29:06 +0000

    Sync LeetCode submission - Search Suggestions System (python3)

--- problems/search_suggestions_system/solution.py
+++ problems/search_suggestions_system/solution.py
@@ -2,19 +2,8 @@ from collections import defaultdict
 
 class Solution:
     def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
-        trie, output = {}, []
-        for pd in products:
-            n = trie
-            for c in pd:
-                if c not in n:
-                    n[c] = {}
-                    n[c]['words'] = []
-                n[c]['words'].append(pd)    
-                n = n[c]
-        n = trie        
-        for ch in searchWord:
-            if ch not in n: break
-            output.append(sorted(n[ch]['words'])[:3])
-            n = n[ch]
-        while len(output) < len(searchWord): output.append([])    
-        return output    
+        t = defaultdict(list)
+        for product in products:
+            for i in range(1, len(product)+1):
+                t[product[:i]].append(product)
+        return [sorted(t[searchWord[:j]])[:3] for j in range(1, len(searchWord)+1)]
