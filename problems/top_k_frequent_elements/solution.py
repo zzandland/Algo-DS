0537ea4e86e12b6202289a4cc140dbe801cd2e19
Si Yong Kim <zzandland@gmail.com>
2020-04-10 06:29:46 +0000

    Sync LeetCode submission - Top K Frequent Elements (python3)

--- problems/top_k_frequent_elements/solution.py
+++ problems/top_k_frequent_elements/solution.py
@@ -1,14 +1,8 @@
+from collections import Counter
+import heapq
+
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count_map = {}
-        for num in nums:
-            if num not in count_map:
-                count_map[num] = 0
-            count_map[num] += 1    
-        lst = []
-        for num, cnt in count_map.items():
-            heapq.heappush(lst, (-1 * cnt, num))
-        output = []
-        for i in range(k):
-            output.append(heapq.heappop(lst)[1])
-        return output    
+        hp = [(-cnt, num) for (num, cnt) in Counter(nums).items()]
+        heapq.heapify(hp)
+        return [heapq.heappop(hp)[1] for _ in range(k)]
