cb3c636296a9c04ccf7e73b4ba5a62cf12f11c02
Si Yong Kim <zzandland@gmail.com>
2020-04-10 23:38:36 +0000

    Sync LeetCode submission - Number of Islands (python3)

--- problems/number_of_islands/solution.py
+++ problems/number_of_islands/solution.py
@@ -1,23 +1,12 @@
-from collections import deque
-
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         if not grid: return 0
-        row, col = len(grid), len(grid[0])
-        checked = [[False for _ in range(col)] for _ in range(row)]
-        directions, count = [(0, 1), (0, -1), (1, 0), (-1, 0)], 0
-        
-        def dfs(y, x):
-            if grid[y][x] == '0' or checked[y][x]: return
-            checked[y][x] = True
-            for r, c in directions:
-                ny, nx = y+r, x+c
-                if 0 <= ny < row and 0 <= nx < col:
-                    dfs(y+r, x+c)
-            
-        for y, r in enumerate(grid):
-            for x, c in enumerate(r):
-                if c == '1' and not checked[y][x]:
-                    count += 1
-                    dfs(y, x)
-        return count                        
+        R, C = len(grid), len(grid[0])
+        dir_ = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        def fn(y: int, x: int) -> int:
+            if not (0 <= y < R and 0 <= x < C) or grid[y][x] == '0': return 0
+            grid[y][x] = '0'
+            for r, c in dir_:
+                fn(y+r, x+c)
+            return 1    
+        return sum([fn(y, x) for y in range(R) for x in range(C)])
