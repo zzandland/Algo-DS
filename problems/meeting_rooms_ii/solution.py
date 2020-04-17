149c9c21e18fac70b86c4ee6da810c07cb984d89
Si Yong Kim <zzandland@gmail.com>
2020-04-17 18:37:16 +0000

    Sync LeetCode submission - Meeting Rooms II (python3)

--- problems/meeting_rooms_ii/solution.py
+++ problems/meeting_rooms_ii/solution.py
@@ -1,15 +1,11 @@
+import heapq
+
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        if not intervals:
-            return 0
-        intervals.sort(key=lambda x: x[0])
-        hp = [(intervals[0][1], intervals[0])]
-        for i in range(1, len(intervals)):
-            inter = intervals[i]
-            end, curr = heapq.heappop(hp)
-            if end <= inter[0]:
-                curr[1] = inter[1]
-            else:
-                heapq.heappush(hp, (inter[1], inter))
-            heapq.heappush(hp, (curr[1], curr))
-        return len(hp)    
+        hp = []
+        for start, end in sorted(intervals, key=lambda x: x[0]):
+            if hp:
+                minEnd = heapq.heappop(hp)
+                if minEnd > start: heapq.heappush(hp, minEnd)
+            heapq.heappush(hp, end)    
+        return len(hp)        
