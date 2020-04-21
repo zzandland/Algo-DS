e8495b07037a2110c353acdf531570ee3209371c
Si Yong Kim <zzandland@gmail.com>
2020-04-21 19:34:34 +0000

    Sync LeetCode submission - Number of Islands (python3)

--- problems/number_of_islands/solution.py
+++ problems/number_of_islands/solution.py
@@ -2,11 +2,15 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         if not grid: return 0
         R, C = len(grid), len(grid[0])
-        dir_ = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-        def fn(y: int, x: int) -> int:
-            if not (0 <= y < R and 0 <= x < C) or grid[y][x] == '0': return 0
+        def fn(y: int, x: int) -> None:
             grid[y][x] = '0'
-            for r, c in dir_:
-                fn(y+r, x+c)
-            return 1    
-        return sum([fn(y, x) for y in range(R) for x in range(C)])
+            for r, c in ((1, 0), (-1, 0), (0, 1), (0, -1)):
+                ny, nx = y+r, x+c
+                if 0 <= ny < R and 0 <= nx < C and grid[ny][nx] == '1': fn(ny, nx)
+        output = 0            
+        for y in range(R):
+            for x in range(C):
+                if grid[y][x] == '1':
+                    output += 1
+                    fn(y, x)
+        return output            
