f78b2165395791a6df227222da173f696c1ecf4b
Si Yong Kim <zzandland@gmail.com>
2020-04-26 02:13:42 +0000

    Sync LeetCode submission - Walls and Gates (python3)

--- problems/walls_and_gates/solution.py
+++ problems/walls_and_gates/solution.py
@@ -1,20 +1,17 @@
-from collections import deque
-
 class Solution:
     def wallsAndGates(self, rooms: List[List[int]]) -> None:
         """
         Do not return anything, modify rooms in-place instead.
         """
-        if not rooms: return None
-        R, C, cnt = len(rooms), len(rooms[0]), 0
-        q, dir_ = deque([(y, x) for y in range(R) for x in range(C) if rooms[y][x] == 0]), [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        if not rooms: return
+        R, C, INF = len(rooms), len(rooms[0]), 2147483647
+        q, nq, d = {(y, x) for y in range(R) for x in range(C) if rooms[y][x] == 0}, set(), 0
         while q:
-            cnt += 1
-            l = len(q)
-            for _ in range(l):
-                y, x = q.popleft()
-                for r, c in dir_:
+            d += 1
+            for y, x in q:
+                for r, c in ((0, 1), (0, -1), (1, 0), (-1, 0)):
                     ny, nx = y+r, x+c
-                    if 0 <= ny < R and 0 <= nx < C and rooms[ny][nx] == 2147483647:
-                        rooms[ny][nx] = cnt
-                        q.append((ny, nx))
+                    if 0 <= ny < R and 0 <= nx < C and rooms[ny][nx] == INF:
+                        rooms[ny][nx] = d
+                        nq.add((ny, nx))
+            q, nq = nq, set()
