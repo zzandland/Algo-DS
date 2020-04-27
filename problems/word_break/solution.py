30c4defdc2adf6e13bf47665ea5effd2f4460340
Si Yong Kim <zzandland@gmail.com>
2020-04-27 03:05:32 +0000

    Sync LeetCode submission - Word Break (python3)

--- problems/word_break/solution.py
+++ problems/word_break/solution.py
@@ -1,13 +1,11 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        if not s or not wordDict:
-            return False
-        wordDictSet = set(wordDict)
-        dp = [False for _ in range(len(s) + 1)]
-        dp[0] = True
-        for i in range(1, len(s) + 1):
-            for j in range(0, i):
-                if dp[j] and s[j : i] in wordDictSet:
+        if not wordDict or not s: return False
+        S = len(s)
+        dp = [False if i > 0 else True for i in range(S+1)]
+        for i in range(1, S+1):
+            for j in range(i):
+                if dp[j] and s[j:i] in wordDict:
                     dp[i] = True
                     break
-        return dp[-1]            
+        return dp[S]            
