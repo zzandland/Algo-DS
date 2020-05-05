89f998305f18d4c76dc07f5ae927990f84b01ab2
Si Yong Kim <zzandland@gmail.com>
2020-05-05 01:48:20 +0000

    Sync LeetCode submission - Serialize and Deserialize Binary Tree (python3)

--- problems/serialize_and_deserialize_binary_tree/solution.py
+++ problems/serialize_and_deserialize_binary_tree/solution.py
@@ -5,43 +5,31 @@
 #         self.left = None
 #         self.right = None
 
-class Codec:
+from collections import deque
 
+class Codec:
     def serialize(self, root):
         """Encodes a tree to a single string.
         
         :type root: TreeNode
         :rtype: str
         """
-        output = []
-        if root is None:
-            return output
-        output.append(root.val)
-        q = collections.deque()
-        q.append(root)
+        if not root: return ''
+        output, q = [str(root.val)], deque([root])
         while q:
             n = q.popleft()
             if n.left:
+                output.append(str(n.left.val))
                 q.append(n.left)
-                output.append(n.left.val)
             else:
-                output.append("null")
-            if n.right:
+                output.append('null')
+            if n.right:    
+                output.append(str(n.right.val))
                 q.append(n.right)
-                output.append(n.right.val)
             else:
-                output.append("null")
-        while output[-1] == "null":
-            output.pop()
-        buff = "["    
-        for e in output:
-            if e == 'null':
-                buff += e
-            else:
-                buff += str(e)
-            buff += ','    
-        return buff[:-1] + ']'        
-                
+                output.append('null')    
+        while output[-1] == 'null': output.pop()        
+        return ','.join(output)
 
     def deserialize(self, data):
         """Decodes your encoded data to tree.
@@ -49,27 +37,24 @@ class Codec:
         :type data: str
         :rtype: TreeNode
         """
-        if len(data) < 3:
-            return None
-        nodes = data[1:-1].split(",")
-        root = TreeNode(int(nodes[0]))
-        q = collections.deque()
-        q.append(root)
-        i = 1
-        while q:
+        if not data: return None
+        buf = deque(data.split(','))
+        root = TreeNode(int(buf.popleft()))
+        q = deque([root])
+        while q and buf:
             n = q.popleft()
-            if i < len(nodes) and nodes[i] != 'null':
-                left_t = TreeNode(int(nodes[i]))
-                n.left = left_t
-                q.append(left_t)
-            i += 1    
-            if i < len(nodes) and nodes[i] != 'null':
-                right_t = TreeNode(int(nodes[i]))
-                n.right = right_t
-                q.append(right_t)
-            i += 1    
+            s = buf.popleft()
+            if s != 'null':
+                l = TreeNode(int(s))
+                n.left = l
+                q.append(l)
+            if not buf: break    
+            s = buf.popleft()
+            if s != 'null':
+                r = TreeNode(int(s))
+                n.right = r
+                q.append(r)
         return root        
-            
 
 # Your Codec object will be instantiated and called as such:
 # codec = Codec()
