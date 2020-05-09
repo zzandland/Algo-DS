be0784fc9acece80db3fe6a7e2be65eface12318
Si Yong Kim <zzandland@gmail.com>
2020-05-09 22:37:14 +0000

    Sync LeetCode submission - License Key Formatting (python3)

--- problems/license_key_formatting/solution.py
+++ problems/license_key_formatting/solution.py
@@ -1,10 +1,8 @@
 class Solution:
     def licenseKeyFormatting(self, S: str, K: int) -> str:
-        cut = S.replace('-', '').upper()
-        counter = 1
-        for i in range(len(cut))[::-1]:
-            if i > 0 and counter == K:
-                cut = cut[:i] + '-' + cut[i:]
-                counter = 0
-            counter += 1    
-        return cut    
+        s = S.replace('-', '')
+        r = len(s) % K
+        o, s = [s[:r].upper()] if r > 0 else [], s[r:]
+        for i in range(0, len(s), K):
+            o.append(s[i:i+K].upper())
+        return '-'.join(o)    
