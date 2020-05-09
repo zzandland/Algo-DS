37f1f8cb07c75097044a26f2f5af948d6e003453
Si Yong Kim <zzandland@gmail.com>
2020-05-09 23:48:04 +0000

    Sync LeetCode submission - Fruit Into Baskets (python3)

--- problems/fruit_into_baskets/solution.py
+++ problems/fruit_into_baskets/solution.py
@@ -1,16 +1,16 @@
 class Solution:
     def totalFruit(self, tree: List[int]) -> int:
-        cnt, dic, s, e = 0, collections.defaultdict(int), 0, 0
-        max_len = 0
-        while e < len(tree):
-            if dic[tree[e]] == 0:
-                cnt += 1
-            dic[tree[e]] += 1                
-            e += 1
-            while cnt > 2:
-                dic[tree[s]] -= 1
-                if dic[tree[s]] == 0:
-                    cnt -= 1
-                s += 1
-            max_len = max(max_len, e - s)
-        return max_len        
+        i = j = 0
+        st, T, mx = set(), len(tree), 0
+        while i < T:
+            st.add(tree[i])
+            if len(st) > 2:
+                j, st = i, set([tree[i]])
+                while len(st) <= 2:
+                    j -= 1
+                    st.add(tree[j])
+                st.remove(tree[j])    
+                j += 1
+            i += 1
+            mx = max(mx, i-j)
+        return mx  
