b0e9c1d2df61d610a2bbf341459d65a25940901f
Si Yong Kim <zzandland@gmail.com>
2020-05-10 03:19:33 +0000

    Sync LeetCode submission - Valid Perfect Square (python3)

---
+++ problems/valid_perfect_square/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        l, r = 1, num
+        while l <= r:
+            m = l + (r-l)//2
+            if m**2 == num: return True
+            if m**2 > num: r = m-1
+            else: l = m+1
+        return False        
