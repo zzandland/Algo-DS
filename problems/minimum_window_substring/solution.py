74725d065da0715ae098ddb25005c8c587914628
Si Yong Kim <zzandland@gmail.com>
2020-05-10 19:22:39 +0000

    Sync LeetCode submission - Minimum Window Substring (python3)

--- problems/minimum_window_substring/solution.py
+++ problems/minimum_window_substring/solution.py
@@ -1,25 +1,21 @@
+from collections import defaultdict
+
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        head = tail = 0
-        c_map, cnt = [0] * 256, len(t)
-        min_len,  min_h = float('inf'), 0
-        for c in t:
-            c_map[ord(c)] += 1
-        while tail < len(s):
-            c_index = ord(s[tail])
-            if c_map[c_index] > 0:
-                cnt -= 1
-            c_map[c_index] -= 1
-            tail += 1
-            while cnt == 0:
-                if tail - head < min_len:
-                    min_len = tail - head
-                    min_h = head
-                c_index = ord(s[head])    
-                if c_map[c_index] == 0:
-                    cnt += 1
-                c_map[c_index] += 1    
-                head += 1
-        if min_len == float('inf'):
-            return ""
-        else: return s[min_h:min_h+min_len]        
+        if not s or not t: return ''
+        dic, zero, ts, mn, output = defaultdict(int), set(list(t)), set(list(t)), float('inf'), ''
+        for tc in t:
+            dic[tc] += 1
+        j = 0
+        for i, sc in enumerate(s):
+            dic[sc] -= 1
+            if dic[sc] == 0 and sc in zero: zero.remove(sc)
+            while not zero:
+                if i-j+1 < mn:
+                    mn = i-j+1
+                    output = s[j:i+1]
+                dic[s[j]] += 1
+                if s[j] in ts and dic[s[j]] > 0:
+                    zero.add(s[j])
+                j+=1    
+        return output            
