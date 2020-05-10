da125ddae6c2df2e5547fe994348ab887e3c906e
Si Yong Kim <zzandland@gmail.com>
2020-05-10 22:46:41 +0000

    Sync LeetCode submission - Missing Ranges (python3)

--- problems/missing_ranges/solution.py
+++ problems/missing_ranges/solution.py
@@ -1,22 +1,7 @@
 class Solution:
     def findMissingRanges(self, nums: List[int], lower: int, upper: int) -> List[str]:
-        output = []
-        if len(nums) == 0:
-            l, r = lower, upper
-            tmp = str(l) + '->' + str(r) if l != r else str(l)
-            output.append(tmp)
-            return output
-        if lower != nums[0]:
-            l, r = lower, nums[0] - 1
-            tmp = str(l) + '->' + str(r) if l != r else str(l)
-            output.append(tmp)
-        for i in range(len(nums) - 1):
-            if nums[i+1] - nums[i] > 1:
-                l, r = nums[i] + 1, nums[i+1] - 1
-                tmp = str(l) + '->' + str(r) if l != r else str(l)
-                output.append(tmp)
-        if upper != nums[-1]:
-            l, r = nums[-1] + 1, upper
-            tmp = str(l) + '->' + str(r) if l != r else str(l)
-            output.append(tmp)
-        return output            
+        nums, output = [lower-1] + nums + [upper+1], []
+        for i, num in enumerate(nums):
+            if num - nums[i-1] > 2: output.append('{}->{}'.format(nums[i-1]+1, num-1))
+            elif num - nums[i-1] == 2: output.append(str(num-1))
+        return output
