ff0cfd6b97f9a5887e068795f923046035493f15
Si Yong Kim <zzandland@gmail.com>
2020-05-10 04:40:40 +0000

    Sync LeetCode submission - Rotate Image (python3)

--- problems/rotate_image/solution.py
+++ problems/rotate_image/solution.py
@@ -3,8 +3,8 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        n = len(matrix)
-        off = n - 1
-        for i in range(round(n/2)):
-            for j in range(i, off-i):
-                matrix[i][j], matrix[off-j][i], matrix[off-i][off-j], matrix[j][off-i] = matrix[off-j][i], matrix[off-i][off-j], matrix[j][off-i], matrix[i][j]
+        N = len(matrix)
+        for i in range(N//2):
+            l = N-1
+            for j in range(i, l-i):
+                matrix[i][j], matrix[j][l-i], matrix[l-i][l-j], matrix[l-j][i] = matrix[l-j][i], matrix[i][j], matrix[j][l-i], matrix[l-i][l-j] 
