0381e161c360a2a5641ac3e3eac70daaacb591f9
Si Yong Kim <zzandland@gmail.com>
2020-05-11 19:34:48 +0000

    Sync LeetCode submission - Backspace String Compare (python3)

--- problems/backspace_string_compare/solution.py
+++ problems/backspace_string_compare/solution.py
@@ -1,13 +1,19 @@
 class Solution:
     def backspaceCompare(self, S: str, T: str) -> bool:
-        st, tt = [], []
-        for s in S:
-            if s == '#':
-                if st: st.pop()
-            else: st.append(s)    
-        for t in T:
-            if t == '#':
-                if tt: tt.pop()
-            else: tt.append(t)    
-        print(st, tt)        
-        return ''.join(st) == ''.join(tt)
+        def skip(i: int, s: str) -> int:
+            cnt = 0
+            while i >= 0:
+                if s[i] == '#': cnt += 1
+                else:
+                    if cnt == 0: return i    
+                    cnt -= 1
+                i -= 1    
+            return i    
+        s, t, sc, tc = len(S)-1, len(T)-1, 0, 0
+        while s >= 0 or t >= 0:
+            s = skip(s, S)
+            t = skip(t, T)
+            if s == -1 and t == -1: return True
+            if s < 0 or t < 0 or S[s] != T[t]: return False
+            s, t = s-1, t-1
+        return True    
