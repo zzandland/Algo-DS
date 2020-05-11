bc6ecdc141355b576117a4e02fed5683914f4f09
Si Yong Kim <zzandland@gmail.com>
2020-05-11 15:18:43 +0000

    Sync LeetCode submission - Next Closest Time (python3)

--- problems/next_closest_time/solution.py
+++ problems/next_closest_time/solution.py
@@ -1,20 +1,19 @@
 class Solution:
     def nextClosestTime(self, time: str) -> str:
-        hr_d, hr_s, m_d, m_s = int(time[0]), int(time[1]), int(time[3]), int(time[4])
-        num_set = sorted({hr_d, hr_s, m_d, m_s})
-        i = num_set.index(m_s) + 1
-        if i < len(num_set):
-            return str(hr_d) + str(hr_s) + ':' + str(m_d) + str(num_set[i])
-        i = num_set.index(m_d) + 1
-        if i < len(num_set) and num_set[i] < 6:
-            return str(hr_d) + str(hr_s) + ':' + str(num_set[i]) + str(num_set[0])
-        i = num_set.index(hr_s) + 1
-        if i < len(num_set):
-            if hr_d < 2:
-                return str(hr_d) + str(num_set[i]) + ':' + str(num_set[0]) + str(num_set[0])
-            elif num_set[i] < 4:
-                return str(hr_d) + str(num_set[i]) + ':' + str(num_set[0]) + str(num_set[0])
-        i = num_set.index(hr_d) + 1
-        if i < len(num_set) and num_set[i] < 3:
-            return str(num_set[i]) + str(num_set[0]) + ':' + str(num_set[0]) + str(num_set[0])
-        return str(num_set[0]) + str(num_set[0]) + ':' + str(num_set[0]) + str(num_set[0])
+        h, m = time.split(':')
+        digits = [int(c) for c in list(time) if c.isdigit()]
+        sort = sorted(set(digits))
+        dic, mn = {e: i for i, e in enumerate(sort)}, sort[0]
+        nxt = dic[digits[3]]+1
+        if nxt < len(sort): return '{}:{}{}'.format(h, digits[2], sort[nxt])
+        nxt = dic[digits[2]]+1
+        if nxt < len(sort) and sort[nxt] < 6:
+            return '{}:{}{}'.format(h, sort[nxt], mn)
+        nxt = dic[digits[1]]+1
+        if nxt < len(sort):
+            if digits[0] < 2: return '{}{}:{}{}'.format(digits[0], sort[nxt], mn, mn)
+            if digits[0] == 2 and sort[nxt] <= 4:
+                return '{}{}:{}{}'.format(digits[0], sort[nxt], mn, mn)
+        nxt = dic[digits[0]]+1    
+        if nxt < len(sort) and sort[nxt] <= 2: return '{}{}:{}{}'.format(sort[nxt], mn, mn, mn)
+        return '{}{}:{}{}'.format(mn, mn, mn, mn)
