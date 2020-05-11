669a7845d9be3c73c7698f1b74743f4e40852c3b
Si Yong Kim <zzandland@gmail.com>
2020-05-11 23:30:49 +0000

    Sync LeetCode submission - K Closest Points to Origin (python3)

--- problems/k_closest_points_to_origin/solution.py
+++ problems/k_closest_points_to_origin/solution.py
@@ -1,3 +1,3 @@
 class Solution:
     def kClosest(self, points: List[List[int]], K: int) -> List[List[int]]:
-        return sorted(points, key=lambda x: x[0]*x[0]+x[1]*x[1])[:K]
+        return sorted(points, key=lambda x: x[0]**2 + x[1]**2)[:K]
