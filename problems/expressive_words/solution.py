5f28bec3031cbf6fc21c2c3e9a1678a91d909f9f
Si Yong Kim <zzandland@gmail.com>
2020-05-11 04:27:23 +0000

    Sync LeetCode submission - Expressive Words (python3)

--- problems/expressive_words/solution.py
+++ problems/expressive_words/solution.py
@@ -2,36 +2,29 @@ from typing import Tuple
 
 class Solution:
     def expressiveWords(self, S: str, words: List[str]) -> int:
-        if len(S) == 0 or len(words) == 0:
-            return 0
-        S_g = self.get_counts(S)    
+        if not S or not words: return 0
+        def compress(s: str) -> List[Tuple[str, int]]:
+            output, c1, cnt = [], s[0], 0
+            for c2 in s:
+                if c1 == c2: cnt += 1
+                else: 
+                    output.append((c1, cnt))
+                    cnt = 1
+                c1 = c2    
+            output.append((c1, cnt))    
+            return output    
+        sc = compress(S)
         output = 0
         for word in words:
-            w_g = self.get_counts(word)
-            if len(S_g) != len(w_g):
-                continue
-            match = True
-            for i, S_ in enumerate(S_g):
-                w_ = w_g[i]
-                if S_[0] != w_[0] or S_[1] < w_[1]:
-                    match = False
-                    break
-                if S_[1] == 1 or S_[1] == 2:
-                    if w_[1] != S_[1]:
-                        match = False
+            if not word: continue
+            wc = compress(word)
+            if len(sc) == len(wc):
+                gud = True
+                for i in range(len(sc)):
+                    sl, scnt = sc[i]
+                    wl, wcnt = wc[i]
+                    if sl != wl or wcnt > scnt or (scnt < 3 and wcnt != scnt): 
+                        gud = False
                         break
-            if match:
-                output += 1
-        return output        
-                            
-    def get_counts(self, S: str) -> List[Tuple[str, int]]:
-        output = []
-        prev, cnt = S[0], 1
-        for i in range(1, len(S)):
-            if S[i] == prev:
-                cnt += 1
-            else:
-                output.append((prev, cnt))    
-                prev, cnt = S[i], 1
-        output.append((prev, cnt))        
-        return output
+                if gud: output += 1
+        return output            
