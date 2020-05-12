6928263b33baf7735a3d88cb5c9802ae17cf3d88
Si Yong Kim <zzandland@gmail.com>
2020-05-12 18:58:57 +0000

    Sync LeetCode submission - Course Schedule II (python3)

--- problems/course_schedule_ii/solution.py
+++ problems/course_schedule_ii/solution.py
@@ -1,27 +1,18 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        adj = collections.defaultdict(list)
-        for prereq in prerequisites:
-            adj[prereq[0]].append(prereq[1])
-        visit = [-1] * numCourses
-        output = []
-        def helper(cur: int) -> bool:
-            if visit[cur] == 0:
-                return False
-            visit[cur] = 0
-            for ad in adj[cur]:
-                if visit[ad] != 1:
-                    if not helper(ad):
-                        return False
-            visit[cur] = 1        
-            output.append(cur)
+        graph, visited, st = [set() for _ in range(numCourses)], set(), []
+        for b, a in prerequisites: graph[a].add(b)
+        def dfs(n: int) -> bool:
+            if n in visited: return True
+            if n in tmp:  return False
+            tmp.add(n)
+            for nxt in graph[n]:
+                if not dfs(nxt): return False
+            st.append(n)
+            tmp.remove(n)
+            visited.add(n)
             return True
-        for i in range(numCourses):
-            if visit[i] == -1:
-                if not adj[i]:
-                    output.append(i)
-                    visit[i] = 1
-                else:
-                    if not helper(i):
-                        return []    
-        return output                    
+        for i in range(numCourses): 
+            tmp = set()
+            if not dfs(i): return []
+        return st[::-1]    
