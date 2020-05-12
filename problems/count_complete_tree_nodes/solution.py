e1663d1e91db28278337c4e6a12277767e52d852
Si Yong Kim <zzandland@gmail.com>
2020-05-12 23:49:05 +0000

    Sync LeetCode submission - Count Complete Tree Nodes (python3)

--- problems/count_complete_tree_nodes/solution.py
+++ problems/count_complete_tree_nodes/solution.py
@@ -1,21 +1,28 @@
 # Definition for a binary tree node.
 # class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def countNodes(self, root: TreeNode) -> int:
-        if root is None:
-            return 0
-        def getDepth(root: TreeNode) -> int:
-            if root is None:
-                return 0
-            return 1 + getDepth(root.left)
-        l_depth = getDepth(root.left)
-        r_depth = getDepth(root.right)
-        if l_depth == r_depth:
-            return pow(2, l_depth) + self.countNodes(root.right)
-        else:
-            return pow(2, r_depth) + self.countNodes(root.left)
+        h, n, res = 0, root, 0
+        while n and n.left:
+            res += 2**h
+            h, n = h+1, n.left
+        d = 2**h    
+        def down(t: int) -> TreeNode:
+            n, l, r = root, 0, d-1
+            while l < r:
+                m = l + (r-l)//2
+                if m >= t: r, n = m, n.left
+                else: l, n = m+1, n.right
+            return n        
+        l, r = 0, d-1
+        while l <= r:
+            m = l + (r-l)//2
+            n = down(m)
+            if n and r == l: break
+            if n: l = m+1
+            else: r = m-1 
+        return d + min(l, r)        
