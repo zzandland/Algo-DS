fb32e9c947e34c692c784d6611c7094636f2e8a4
Si Yong Kim <zzandland@gmail.com>
2020-05-12 18:26:15 +0000

    Sync LeetCode submission - Number of Islands (python3)

--- problems/number_of_islands/solution.py
+++ problems/number_of_islands/solution.py
@@ -1,16 +1,18 @@
+from collections import deque
+
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         if not grid: return 0
-        R, C = len(grid), len(grid[0])
-        def fn(y: int, x: int) -> None:
-            grid[y][x] = '0'
-            for r, c in ((1, 0), (-1, 0), (0, 1), (0, -1)):
-                ny, nx = y+r, x+c
-                if 0 <= ny < R and 0 <= nx < C and grid[ny][nx] == '1': fn(ny, nx)
-        output = 0            
-        for y in range(R):
-            for x in range(C):
-                if grid[y][x] == '1':
-                    output += 1
-                    fn(y, x)
-        return output            
+        R, C, res = len(grid), len(grid[0]), 0
+        for i in range(R):
+            for j in range(C):
+                if grid[i][j] == '1':
+                    grid[i][j], res, q = '0', res+1, deque([(i, j)])
+                    while q:
+                        y, x = q.popleft()
+                        for r, c in ((1, 0), (-1, 0), (0, 1), (0, -1)):
+                            ny, nx = y+r, x+c
+                            if 0 <= ny < R and 0 <= nx < C and grid[ny][nx] == '1':
+                                grid[ny][nx] = '0'
+                                q.append((ny, nx))
+        return res                        
