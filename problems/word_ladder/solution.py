fc7a0d2a18d3ad7d8145310364f8a1c850d53e59
Si Yong Kim <zzandland@gmail.com>
2020-05-12 04:03:56 +0000

    Sync LeetCode submission - Word Ladder (python3)

--- problems/word_ladder/solution.py
+++ problems/word_ladder/solution.py
@@ -1,33 +1,16 @@
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        wordSet = set(wordList)
-        if endWord not in wordSet:
-            return 0
-        wordDic = {}
-        for word in wordList:
-            for i in range(len(word)):
-                tmp = word[:i] + "*" + word[i+1:]
-                if tmp not in wordDic:
-                    wordDic[tmp] = []
-                wordDic[tmp].append(word)
-        q = (collections.deque(), collections.deque())        
-        q[0].append(beginWord)
-        q[1].append(endWord)    
-        wordSet.remove(endWord)
-        l = [0, 1]
-        while len(q[0]) > 0 or len(q[1]) > 0:
-            for i in range(2):
-                l[i] += 1
-                for j in range(len(q[i])):
-                    word = q[i].popleft()
-                    for k in range(len(word)):
-                        tmp = word[:k] + "*" + word[k+1:]
-                        if tmp in wordDic:
-                            for match in wordDic[tmp]:
-                                if match in q[(i + 1) % 2]:
-                                    return l[0] + l[1]
-                                if match in wordSet:
-                                    q[i].append(match)
-                                    wordSet.remove(match)
-                        wordDic.pop(tmp, None)            
-        return 0                
+        q1, q2, nq, words, cnt = set([beginWord]), set([endWord]), set(), set(wordList), 0
+        if endWord not in wordList: return cnt
+        while q1 and q2:
+            words -= q1
+            if len(q1) > len(q2): q1, q2 = q2, q1
+            cnt += 1
+            for w in q1:
+                for i in range(len(beginWord)):
+                    for c in 'qwertyuiopasdfghjklzxcvbnm':
+                        comb = w[:i]+c+w[i+1:]
+                        if comb in q2: return cnt+1
+                        if comb in words: nq.add(comb)
+            q1, nq = nq, set()
+        return 0
