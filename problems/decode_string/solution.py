88c7bac4a0e19af4fda3652e07964ed79c141673
Si Yong Kim <zzandland@gmail.com>
2020-05-13 00:39:48 +0000

    Sync LeetCode submission - Decode String (python3)

--- problems/decode_string/solution.py
+++ problems/decode_string/solution.py
@@ -1,23 +1,19 @@
-from typing import Tuple
+from collections import deque
 
 class Solution:
     def decodeString(self, s: str) -> str:
-        return self.helper(s, 0, 1)[0]
-        
-    def helper(self, s: str, i: int, k: int) -> Tuple[str, int]:
-        buf, output = [], ""
-        while i < len(s) and s[i] != ']':
-            if s[i].isdigit():
-                cnt = ""
-                while s[i].isdigit():
-                    cnt += s[i]
-                    i += 1
-                result, i = self.helper(s, i + 1, int(cnt))
-                buf.append(result)
-            else:
-                buf.append(s[i])    
-                i += 1
-        buf_str = ''.join(buf)
-        for _ in range(k):
-            output += buf_str
-        return (output, i + 1)    
+        q = deque(list(s))
+        def fn() -> str:
+            s = ''
+            while q:
+                c = q.popleft()
+                if c == ']': return s
+                elif c.isdigit():
+                    n = ''
+                    while c.isdigit():
+                        n += c
+                        c = q.popleft()
+                    s += (fn()*int(n))
+                else: s += c
+            return s        
+        return fn()
