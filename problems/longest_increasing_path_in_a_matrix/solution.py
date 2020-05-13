2065498ef663f46c761e88994abb7192487389ab
Si Yong Kim <zzandland@gmail.com>
2020-05-13 00:16:46 +0000

    Sync LeetCode submission - Longest Increasing Path in a Matrix (python3)

--- problems/longest_increasing_path_in_a_matrix/solution.py
+++ problems/longest_increasing_path_in_a_matrix/solution.py
@@ -1,19 +1,13 @@
 class Solution:
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
-        if not matrix:
-            return 0
-        w, l = len(matrix), len(matrix[0])
-        dp = [[0] * l for _ in range(w)]
-        def helper(r: int, c: int) -> int:
-            if not dp[r][c]:
-                max_len, curr = 1, matrix[r][c]
-                for y, x in ((r+1, c), (r-1, c), (r, c+1), (r, c-1)):
-                    if 0 <= y < w and 0 <= x < l and matrix[y][x] > curr:
-                        max_len = max(max_len, 1 + helper(y, x))
-                dp[r][c] = max_len            
-            return dp[r][c]    
-        output = 1
-        for i, row in enumerate(matrix):
-            for j, _ in enumerate(row):
-                output = max(output, helper(i, j))
-        return output        
+        if not matrix: return 0
+        R, C = len(matrix), len(matrix[0])
+        lst = sorted([(y, x) for y in range(R) for x in range(C)], key=lambda x: -matrix[x[0]][x[1]])
+        dp, mx = [[1]*C for _ in range(R)], 1
+        for i, j in lst:
+            for r, c in ((1, 0), (-1, 0), (0, 1), (0, -1)):
+                ni, nj = i+r, j+c
+                if 0 <= ni < R and 0 <= nj < C and matrix[ni][nj] > matrix[i][j]:
+                    dp[i][j] = max(dp[i][j], dp[ni][nj]+1)
+                mx = max(mx, dp[i][j])
+        return mx            
