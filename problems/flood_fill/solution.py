b323564c4ab4f2c8fececdeb51a0c9c7cb3e1a25
Si Yong Kim <zzandland@gmail.com>
2020-05-13 23:06:52 +0000

    Sync LeetCode submission - Flood Fill (python3)

--- problems/flood_fill/solution.py
+++ problems/flood_fill/solution.py
@@ -1,18 +1,15 @@
+from collections import deque
+
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
-        if len(image) == 0:
-            return image
-        height, width = len(image), len(image[0])
-        if not (0 <= sr < height and 0 <= sc < width and image[sr][sc] != newColor):
-            return image
-        q = collections.deque()
-        q.append((sr, sc))
-        origin_color = image[sr][sc]
-        image[sr][sc] = newColor
-        while len(q) > 0:
-            row, col = q.popleft()
-            for r, c in ((row + 1, col), (row - 1, col), (row, col + 1), (row, col - 1)):
-                if 0 <= r < height and 0 <= c < width and image[r][c] == origin_color:
-                    image[r][c] = newColor
-                    q.append((r, c))
+        if not image: return None
+        R, C = len(image), len(image[0])
+        q, prev = deque([(sr, sc)]), image[sr][sc]
+        if prev == newColor: return image
+        while q:
+            y, x = q.popleft()
+            image[y][x] = newColor
+            for r, c in ((1, 0), (-1, 0), (0, 1), (0, -1)):
+                ny, nx = y+r, x+c
+                if 0 <= ny < R and 0 <= nx < C and image[ny][nx] == prev: q.append((ny, nx))
         return image            
