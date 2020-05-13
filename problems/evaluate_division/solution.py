68d60d84b0fc47af6f3a42fd900d6e13108bf70b
Si Yong Kim <zzandland@gmail.com>
2020-05-13 01:58:42 +0000

    Sync LeetCode submission - Evaluate Division (python3)

--- problems/evaluate_division/solution.py
+++ problems/evaluate_division/solution.py
@@ -1,37 +1,27 @@
 from collections import defaultdict
-from fractions import Fraction as frac
 
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
-        dic = defaultdict(dict)
-        for [frm, to], val in zip(equations, values):
-            dic[frm][to] = frac(str(val))
-            dic[to][frm] = frac(str(1 / val))
-            
-        visited = set()
-        def traverse(to: str, n: str) -> int:
-            if to == n:
-                return 1
-            for nxt, val in dic[n].items():
-                if nxt not in visited:
-                    visited.add(nxt)
-                    cal = traverse(to, nxt)
-                    if cal != -1:
-                        return val * cal
-            return -1
-            
-        output = []    
-        for [frm, to] in queries:
-            if frm not in dic or to not in dic:
-                output.append(float(-1))
-            else:    
-                visited.add(frm)
-                cal = traverse(to, frm)
-                if cal == -1.0:
-                    output.append(float(-1))
-                else:    
-                    output.append(cal)
-                    dic[frm][to] = frac(str(cal))
-                    dic[to][frm] = frac(str(1 / cal))
-                visited.clear()
-        return output        
+        graph = defaultdict(dict)
+        for i, (a, b) in enumerate(equations):
+            graph[a][b] = values[i]
+            graph[b][a] = 1/values[i] if values[i] != 0 else -1
+        visited = set()    
+        output = []
+        def dfs(n: int, t: int) -> float:
+            if n == t: return 1
+            if n in visited: return -1
+            visited.add(n)
+            for nxt in graph[n]:
+                if graph[n][nxt] != -1:
+                    f = dfs(nxt, t)
+                    if f != -1: return f * graph[n][nxt]
+            return -1    
+        for a, b in queries:
+            if a in graph:
+                if b not in graph[a]:
+                    visited = set()
+                    graph[a][b] = dfs(a, b)
+                output.append(graph[a][b])    
+            else: output.append(-1)    
+        return output    
