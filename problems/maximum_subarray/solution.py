74d40d18aa2359bc31f608214d6ab3e72a2d4d0a
Si Yong Kim <zzandland@gmail.com>
2020-05-16 15:17:12 +0000

    Sync LeetCode submission - Maximum Subarray (python3)

--- problems/maximum_subarray/solution.py
+++ problems/maximum_subarray/solution.py
@@ -1,10 +1,10 @@
+import heapq
+
 class Solution:
-  def maxSubArray(self, nums: List[int]) -> int:
-    output, current = float('-inf'), 0
-    for num in nums:
-      if current < 0:
-        current = num
-      else:  
-        current += num  
-      output = max(output, current)  
-    return output   
+    def maxSubArray(self, nums: List[int]) -> int:
+        N, hp, mn, mx, run = len(nums), [], float('inf'), float('-inf'), 0
+        for num in nums:
+            run += num
+            mx = max(mx, run, run-mn)
+            mn = min(mn, run)
+        return mx    
