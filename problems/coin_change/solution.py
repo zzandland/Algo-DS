b8a414701e7c68cf18cf646f2a71a28a4654f924
Si Yong Kim <zzandland@gmail.com>
2020-05-17 00:28:21 +0000

    Sync LeetCode submission - Coin Change (python3)

--- problems/coin_change/solution.py
+++ problems/coin_change/solution.py
@@ -1,10 +1,7 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        if not coins: return -1
-        coins.sort()
-        N = len(coins)
-        dp = [0] + [float('inf')] * amount
+        dp = [0] + [float('inf')]*amount
         for coin in coins:
-            for j in range(coin, amount+1):
-                dp[j] = min(dp[j], dp[j-coin]+1)
-        return -1 if dp[-1] == float('inf') else dp[-1]
+            for i in range(coin, amount+1):
+                dp[i] = min(dp[i], dp[i-coin]+1)
+        return -1 if dp[-1] == float('inf') else dp[-1] 
