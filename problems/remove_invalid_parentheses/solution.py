1321f2bcadb5b04e6dd7e971651b3f3d68e8447c
Si Yong Kim <zzandland@gmail.com>
2020-05-17 23:23:43 +0000

    Sync LeetCode submission - Remove Invalid Parentheses (python3)

--- problems/remove_invalid_parentheses/solution.py
+++ problems/remove_invalid_parentheses/solution.py
@@ -1,20 +1,22 @@
 class Solution:
     def removeInvalidParentheses(self, s: str) -> List[str]:
         S, output, mx = len(s), set(), 0
-        def fn(i: int, cnt: int, substr: str) -> None:
+        def fn(i: int, c: int, run: str) -> List[str]:
             nonlocal mx, output
-            if cnt < 0:  return
-            if i == S: 
-                if cnt == 0:
-                    if len(substr) > mx: 
-                        output = set()
-                        mx = len(substr)
-                    if len(substr) == mx:    
-                        output.add(substr)
+            if c < 0: return
+            if i == S:
+                if c > 0: return
+                if len(run) > mx:
+                    mx = len(run)
+                    output = set()
+                if len(run) == mx: output.add(run)    
                 return
-            if s[i] not in ('(', ')'): return fn(i+1, cnt, substr+s[i])
-            nxt = cnt + 1 if s[i] == '(' else cnt - 1
-            fn(i+1, nxt, substr+s[i])
-            fn(i+1, cnt, substr)
+            if s[i] == '(':
+                fn(i+1, c+1, run+s[i])
+                fn(i+1, c, run)
+            elif s[i] == ')':
+                fn(i+1, c-1, run+s[i])
+                fn(i+1, c, run)
+            else: fn(i+1, c, run+s[i])
         fn(0, 0, '')
         return list(output)
