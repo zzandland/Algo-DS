6e724a247106102dcd56fdd28ce69d52116b3e7d
Si Yong Kim <zzandland@gmail.com>
2020-05-23 03:16:41 +0000

    Sync LeetCode submission - Valid Palindrome II (python3)

--- problems/valid_palindrome_ii/solution.py
+++ problems/valid_palindrome_ii/solution.py
@@ -1,15 +1,10 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
-        if len(s) < 3: return True
-        l, r, skipped = 0, len(s)-1, False
-        def isPal(l: int, r: int) -> bool:
+        def fn(skip: bool, l: int = 0, r: int = len(s)-1) -> bool:
             while l < r:
-                if s[l] != s[r]: return False
-                l, r = l+1, r-1
+                if s[l] != s[r]:
+                    if not skip: return fn(True, l+1, r) or fn(True, l, r-1)
+                    return False
+                l, r = l+1, r-1    
             return True    
-        while l < r:
-            if s[l] == s[r]:
-                l, r = l+1, r-1
-            else:
-                return isPal(l+1, r) or isPal(l, r-1)
-        return True        
+        return fn(False)
