c0ec242f891e144094c8a735ad8e7b26fd650a92
Si Yong Kim <zzandland@gmail.com>
2020-05-23 05:29:29 +0000

    Sync LeetCode submission - Flatten Binary Tree to Linked List (python3)

--- problems/flatten_binary_tree_to_linked_list/solution.py
+++ problems/flatten_binary_tree_to_linked_list/solution.py
@@ -9,14 +9,12 @@ class Solution:
         """
         Do not return anything, modify root in-place instead.
         """
-        def inorder(n: TreeNode) -> TreeNode:
+        def fn(n: TreeNode) -> TreeNode:
             if not n: return None
-            l, ln, r, rn = n.left, inorder(n.left), n.right, inorder(n.right)
-            if l:
-                n.right, n.left = l, None
+            l, r, ln, rn = n.left, n.right, fn(n.left) if n.left else n, fn(n.right) if n.right else n
+            if l: 
+                n.left, n.right = None, l
                 if r: ln.right = r
-            if r: return rn    
-            if l: return ln
-            return n
-        inorder(root)
-        return root
+                else: return ln
+            return rn
+        fn(root)
