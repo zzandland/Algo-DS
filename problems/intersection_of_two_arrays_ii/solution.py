7d780f092bf82eb9c8588f646c1ea5cc032df90d
Si Yong Kim <zzandland@gmail.com>
2020-05-28 15:42:16 +0000

    Sync LeetCode submission - Intersection of Two Arrays II (python3)

--- problems/intersection_of_two_arrays_ii/solution.py
+++ problems/intersection_of_two_arrays_ii/solution.py
@@ -1,13 +1,10 @@
+from collections import Counter
+
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
-      cache, output = {}, []
-      for num in nums1:
-        if num in cache:
-          cache[num] += 1
-        else:
-          cache[num] = 1  
-      for num in nums2:
-        if num in cache and cache[num] > 0:
-          output += [num]    
-          cache[num] -= 1
-      return output    
+        c1, c2, res = Counter(nums1), Counter(nums2), []
+        for num in c1:
+            if num in c2:
+                for i in range(min(c1[num], c2[num])):
+                    res.append(num)
+        return res            
