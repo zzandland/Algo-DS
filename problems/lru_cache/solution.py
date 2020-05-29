febeb89fe0dbdf5081aa39a9690c41548683d031
Si Yong Kim <zzandland@gmail.com>
2020-05-29 21:14:49 +0000

    Sync LeetCode submission - LRU Cache (python3)

--- problems/lru_cache/solution.py
+++ problems/lru_cache/solution.py
@@ -1,61 +1,20 @@
-class Node:
-    def __init__(self, key: int, val: int):
-        self.key = key
-        self.val = val
-        self.next = self.prev = None
-        
-class LRUQueue:
-    def __init__(self):
-        self.head = self.tail = None        
-        self.size = 0
-        
-    def enqueue(self, n: Node) -> None:
-        if self.head: n.next, self.head.prev = self.head, n
-        self.head = n
-        if not self.tail: self.tail = n
-        self.size += 1    
-            
-    def dequeue(self) -> Node:
-        t = self.tail
-        if self.tail != self.head:
-            self.tail = self.tail.prev
-            self.tail.next = None
-        else:
-            self.tail = self.head = None
-        t.next = t.prev = None
-        self.size -= 1    
-        return t
-        
-    def remove(self, n: Node) -> Node:
-        if self.tail == n: return self.dequeue()
-        self.size -= 1
-        if self.head == n: self.head = n.next
-        if n.prev: n.prev.next = n.next
-        if n.next: n.next.prev = n.prev    
-        n.next = n.prev = None
-        return n
-        
+from collections import OrderedDict
+
 class LRUCache:
     def __init__(self, capacity: int):
-        self.q = LRUQueue()        
-        self.dic = {}
-        self.capacity = capacity
-        
+        self.dic = OrderedDict()
+        self.cap = capacity
+
     def get(self, key: int) -> int:
         if key in self.dic: 
-            n = self.q.remove(self.dic[key])
-            self.q.enqueue(n)
-            return n.val
+            self.put(key, self.dic[key])
+            return self.dic[key]
         return -1
 
-    def put(self, key: int, val: int) -> None:
-        n = Node(key, val)
-        if key in self.dic: self.q.remove(self.dic[key])        
-        self.dic[key] = n
-        self.q.enqueue(n)
-        if self.capacity < self.q.size: 
-            d = self.q.dequeue()
-            del self.dic[d.key]
+    def put(self, key: int, value: int) -> None:
+        if key in self.dic: del self.dic[key]
+        self.dic[key] = value
+        if len(self.dic) > self.cap: self.dic.popitem(last=False)
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
