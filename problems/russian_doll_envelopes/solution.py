ae549ab392629d0ad23b0cf03f98c890f86c4771
Si Yong Kim <zzandland@gmail.com>
2020-06-01 19:52:56 +0000

    Sync LeetCode submission - Russian Doll Envelopes (python3)

--- problems/russian_doll_envelopes/solution.py
+++ problems/russian_doll_envelopes/solution.py
@@ -1,13 +1,19 @@
 class Solution:
     def maxEnvelopes(self, envelopes: List[List[int]]) -> int:
-        if not envelopes: return 0
-        N, mx = len(envelopes), 1
-        envelopes.sort()
-        dp = [1] * N
-        for i in range(1, N):
-            for j in range(i):
-                iw, ih = envelopes[i]
-                jw, jh = envelopes[j]
-                if iw > jw and ih > jh and dp[i] <= dp[j]: dp[i] += 1
-            mx = max(mx, dp[i])        
-        return mx    
+        dp = []
+        envelopes.sort(key=lambda x: (x[0], -x[1]))
+        for _, h in envelopes:
+            l, r = 0, len(dp)
+            while l < r:
+                m = l + (r-l)//2
+                if dp[m] == h:
+                    l = r = m
+                elif dp[m] > h:
+                    r = m
+                else:
+                    l = m+1
+            if l == len(dp):
+                dp.append(h)
+            else:
+                dp[l] = h
+        return len(dp)
