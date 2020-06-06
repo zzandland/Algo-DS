2ed4d95a699d68841fe9bf0a9bf7e9385e320d8f
Si Yong Kim <zzandland@gmail.com>
2020-06-06 15:45:56 +0000

    Sync LeetCode submission - Spiral Matrix II (python3)

--- problems/spiral_matrix_ii/solution.py
+++ problems/spiral_matrix_ii/solution.py
@@ -1,12 +1,20 @@
 class Solution:
     def generateMatrix(self, n: int) -> List[List[int]]:
-        grid, dir_ = [[None for _ in range(n)] for _ in range(n)], [(0, 1), (1, 0), (0, -1), (-1, 0)]
-        cur_dir = y = x = 0
-        for i in range(1, n*n+1):
-            grid[y][x] = i
-            ny, nx = y+dir_[cur_dir][0], x+dir_[cur_dir][1]
-            if not (0 <= ny < n and 0 <= nx < n) or grid[ny][nx] != None:
-                cur_dir = (cur_dir+1) % 4
-                ny, nx = y+dir_[cur_dir][0], x+dir_[cur_dir][1]
-            y, x = ny, nx    
-        return grid    
+        res = [[0]*n for _ in range(n)]
+        num, dir_, di = 1, [(0, 1), (1, 0), (0, -1), (-1, 0)], 0
+        y = x = turn = 0
+        py = px = -1
+        while turn < 2:
+            while 0 <= y < n and 0 <= x < n:
+                if res[y][x] != 0:
+                    break
+                turn = 0
+                res[y][x] = num
+                num += 1
+                py, px = y, x
+                y += dir_[di][0]
+                x += dir_[di][1]
+            turn += 1
+            di = (di+1) % 4
+            y, x = py + dir_[di][0], px + dir_[di][1]
+        return res
