e1c1ef2a09296efaa930e30fada5552f413ad73b
Si Yong Kim <zzandland@gmail.com>
2020-06-07 17:47:45 +0000

    Sync LeetCode submission - Cherry Pickup (python3)

--- problems/cherry_pickup/solution.py
+++ problems/cherry_pickup/solution.py
@@ -1,17 +1,22 @@
 class Solution:
     def cherryPickup(self, grid: List[List[int]]) -> int:
+        if not grid: return 0
         N = len(grid)
-        dp = {}
+        dp = [[[None]*N for _ in range(N)] for _ in range(N)]
         def fn(y1: int, x1: int, y2: int) -> int:
             x2 = y1 + x1 - y2
-            if (y1 >= N or x1 >= N or y2 >= N or x2 >= N or
-                grid[y1][x1] == -1 or grid[y2][x2] == -1): return float('-inf')
-            if y1 == x1 == N-1: return grid[y1][x1]
-            key = '{}:{}:{}'.format(y1, x1, y2)
-            if key not in dp:
-                dp[key] = grid[y1][x1] + (x1 != x2) * grid[y2][x2] + max(
+            if (y1 == N or x1 == N or y2 == N or x2 == N or
+                grid[y1][x1] == -1 or grid[y2][x2] == -1):
+                return float('-inf')
+            if y1 == y2 == x1 == x2 == N-1:
+                return grid[y1][x1]
+            if dp[y1][x1][y2] is None:
+                collect = grid[y1][x1] + grid[y2][x2]
+                if y1 == y2 and x1 == x2:
+                    collect -= grid[y2][x2]
+                dp[y1][x1][y2] = (collect + max(
                     fn(y1+1, x1, y2+1), fn(y1+1, x1, y2),
                     fn(y1, x1+1, y2+1), fn(y1, x1+1, y2)
-                )
-            return dp[key]    
-        return max(0, fn(0, 0, 0))
+                ))
+            return dp[y1][x1][y2]    
+        return max(0, fn(0, 0, 0))            
