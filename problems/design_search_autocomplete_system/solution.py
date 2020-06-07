3787bcfb9df84d97762a1c0396f2f294d582adf7
Si Yong Kim <zzandland@gmail.com>
2020-06-07 23:37:16 +0000

    Sync LeetCode submission - Design Search Autocomplete System (python3)

--- problems/design_search_autocomplete_system/solution.py
+++ problems/design_search_autocomplete_system/solution.py
@@ -1,28 +1,53 @@
-from collections import Counter
+from collections import defaultdict
+
+class Node:
+    def __init__(self):
+        self.next = defaultdict(Node)
+        self.top3 = []
 
 class AutocompleteSystem:
     def __init__(self, sentences: List[str], times: List[int]):
-        self.dic = {}
-        self.s = ''
-        for i, s in enumerate(sentences):
-            b = ''
-            for c in s:
-                b += c
-                if b not in self.dic: self.dic[b] = Counter()
-                self.dic[b][s] = times[i]
+        self.root = self.cur = Node()
+        self.id_ = 0
+        self.query = ''
+        self.query2id = {}
+        self.id2query = {}
+        self.id2cnt = {}
+        self.comp = lambda id_: (-self.id2cnt[id_], self.id2query[id_])
+        for s, t in zip(sentences, times):
+            self._update(s, t)
+        
+    def _query2id(self, query: str) -> int:
+        if query not in self.query2id:
+            self.query2id[query] = self.id_
+            self.id2query[self.id_] = query
+            self.id2cnt[self.id_] = 0
+            self.id_ += 1
+        return self.query2id[query]
+    
+    def _update(self, query: str, times: int = 1) -> None:
+        id_ = self._query2id(query)
+        self.id2cnt[id_] += times
+        n = self.root
+        for c in query:
+            n = n.next[c]
+            if id_ not in n.top3:
+                if len(n.top3) < 3:
+                    n.top3.append(id_)
+                else:
+                    minIdx = max(range(3), key=lambda i: self.comp(n.top3[i]))
+                    if self.comp(id_) <= self.comp(n.top3[minIdx]):
+                        n.top3[minIdx] = id_
 
     def input(self, c: str) -> List[str]:
-        if c == '#':
-            kw, b = self.s, ''
-            for c in self.s:
-                b += c
-                self.dic[b][kw] += 1
-            self.s = ''    
-            return []    
-        else:    
-            self.s += c
-            if self.s not in self.dic: self.dic[self.s] = Counter()
-            return [s for s, c in sorted(self.dic[self.s].items(), key=lambda x: (-x[1], x[0]))[:3]]
+        if c != '#':
+            self.query += c
+            self.cur = self.cur.next[c]
+            return [self.id2query[id_] for id_ in sorted(self.cur.top3, key=self.comp)]
+        else:
+            self._update(self.query)
+            self.query = ''
+            self.cur = self.root
 
 # Your AutocompleteSystem object will be instantiated and called as such:
 # obj = AutocompleteSystem(sentences, times)
