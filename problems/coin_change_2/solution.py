966d91f0f998ca171d5cea826a1ea3c6eaad160f
Si Yong Kim <zzandland@gmail.com>
2020-06-08 01:14:20 +0000

    Sync LeetCode submission - Coin Change 2 (python3)

--- problems/coin_change_2/solution.py
+++ problems/coin_change_2/solution.py
@@ -1,7 +1,10 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        dp = [1] + [0]*amount
-        for coin in coins:
-            for i in range(coin, amount+1):
-                dp[i] += dp[i-coin]
-        return dp[-1]        
+        dp = [1]+[0]*(amount)
+        for i, coin in enumerate(coins):
+            for j in range(coin, amount+1):
+                if i == 0:
+                    dp[j] = 1 if j % coin == 0 else 0
+                else:
+                    dp[j] += dp[j-coin]
+        return dp[-1]
