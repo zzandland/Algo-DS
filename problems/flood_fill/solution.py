55cc8df3f9830ae2f243fdc503828a879ea9702b
Si Yong Kim <zzandland@gmail.com>
2020-06-08 20:56:06 +0000

    Sync LeetCode submission - Flood Fill (python3)

--- problems/flood_fill/solution.py
+++ problems/flood_fill/solution.py
@@ -1,15 +1,20 @@
-from collections import deque
-
 class Solution:
-    def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
-        if not image: return None
-        R, C = len(image), len(image[0])
-        q, prev = deque([(sr, sc)]), image[sr][sc]
-        if prev == newColor: return image
-        while q:
-            y, x = q.popleft()
-            image[y][x] = newColor
-            for r, c in ((1, 0), (-1, 0), (0, 1), (0, -1)):
-                ny, nx = y+r, x+c
-                if 0 <= ny < R and 0 <= nx < C and image[ny][nx] == prev: q.append((ny, nx))
-        return image            
+    def floodFill(self, image: List[List[int]], 
+                  sr: int, sc: int, newColor: int) -> List[List[int]]:
+        if not image:
+            return []
+        R, C, dir_= len(image), len(image[0]), ((1, 0), (-1, 0), (0, 1), (0, -1))
+        seen, st, prev = [[False]*C for _ in range(R)], [], image[sr][sc]
+        if prev != newColor:
+            st.append((sr, sc))
+            seen[sr][sc] = True
+        while st:
+            r, c = st.pop()
+            image[r][c] = newColor
+            for y, x in dir_:
+                nr, nc = r+y, c+x
+                if (0 <= nr < R and 0 <= nc < C and
+                    not seen[nr][nc] and image[nr][nc] == prev):
+                    seen[nr][nc] = True
+                    st.append((nr, nc))
+        return image
