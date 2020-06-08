ed09093c251a6a00ee961a9e1d56e2fa6f69a170
Si Yong Kim <zzandland@gmail.com>
2020-06-08 21:07:20 +0000

    Sync LeetCode submission - 01 Matrix (python3)

--- problems/01_matrix/solution.py
+++ problems/01_matrix/solution.py
@@ -2,19 +2,25 @@ from collections import deque
 
 class Solution:
     def updateMatrix(self, matrix: List[List[int]]) -> List[List[int]]:
-        if matrix:
-            R, C, d, dir_ = len(matrix), len(matrix[0]), -1, [(1, 0), (-1, 0), (0, 1), (0, -1)]
-            q = deque([(y, x) for y in range(R) for x in range(C) if matrix[y][x] == 0])
-            v = [[matrix[y][x] == 0 for x in range(C)] for y in range(R)]
-            while q:
-                d+=1
-                l = len(q)
-                for _ in range(l):
-                    y, x = q.popleft()
-                    matrix[y][x] = d
-                    for r, c in dir_:
-                        ny, nx = y+r, x+c
-                        if 0 <= ny < R and 0 <= nx < C and not v[ny][nx] and matrix[ny][nx] == 1:
-                            v[ny][nx] = True
-                            q.append((ny, nx))
-        return matrix                
+        if not matrix:
+            return []
+        R, C = len(matrix), len(matrix[0])
+        dir_, cnt = ((1, 0), (-1, 0), (0, 1), (0, -1)), -1
+        seen, q = [[False]*C for _ in range(R)], deque()
+        for r in range(R):
+            for c in range(C):
+                if matrix[r][c] == 0:
+                    q.append((r, c))
+                    seen[r][c] = True
+        while q:
+            cnt += 1
+            l = len(q)
+            for _ in range(l):
+                r, c = q.popleft()
+                matrix[r][c] = cnt
+                for y, x in dir_:
+                    nr, nc = r+y, c+x
+                    if 0 <= nr < R and 0 <= nc < C and not seen[nr][nc]:
+                        seen[nr][nc] = True
+                        q.append((nr, nc))
+        return matrix
