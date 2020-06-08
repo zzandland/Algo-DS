3f9a0d6eea217b13411eddd923e2d28628a293e7
Si Yong Kim <zzandland@gmail.com>
2020-06-08 20:38:23 +0000

    Sync LeetCode submission - Decode String (python3)

--- problems/decode_string/solution.py
+++ problems/decode_string/solution.py
@@ -2,18 +2,19 @@ from collections import deque
 
 class Solution:
     def decodeString(self, s: str) -> str:
-        q = deque(list(s))
-        def fn() -> str:
-            s = ''
-            while q:
-                c = q.popleft()
-                if c == ']': return s
-                elif c.isdigit():
-                    n = ''
-                    while c.isdigit():
-                        n += c
-                        c = q.popleft()
-                    s += (fn()*int(n))
-                else: s += c
-            return s        
-        return fn()
+        q, st, buf = deque(s), [], ''
+        while q:
+            c = q.popleft()
+            if c.isdigit():
+                digit = ''
+                while c.isdigit():
+                    digit += c
+                    c = q.popleft()
+                st.append((int(digit), buf))
+                buf = ''
+            elif c == ']':
+                mul, p = st.pop()
+                buf = p + mul*buf
+            else:
+                buf += c
+        return buf         
