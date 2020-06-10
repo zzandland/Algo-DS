7025148af78d7fedbe9401524bf86222a3a866fc
Si Yong Kim <zzandland@gmail.com>
2020-06-10 00:42:13 +0000

    Sync LeetCode submission - Minimum Height Trees (python3)

--- problems/minimum_height_trees/solution.py
+++ problems/minimum_height_trees/solution.py
@@ -2,18 +2,23 @@ from collections import defaultdict
 
 class Solution:
     def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
-        if n == 1: return [0]
-        graph = [set() for _ in range(n)]
-        for a, b in edges:
-            graph[a].add(b)
-            graph[b].add(a)
-        leaves = [n for n, lst in enumerate(graph) if len(lst) == 1]
-        while n > 2:
-            n -= len(leaves)
-            tmp = []
-            for leaf in leaves:
-                to = graph[leaf].pop()
-                graph[to].remove(leaf)
-                if len(graph[to]) == 1: tmp.append(to)
-            leaves = tmp        
-        return leaves    
+        if n == 1:
+            return [0]
+        ind, adj, rem = [0]*n, defaultdict(set), set(range(n))
+        for u, v in edges:
+            ind[u] += 1
+            ind[v] += 1
+            adj[u].add(v)
+            adj[v].add(u)
+        q, nq = set([i for i in range(n) if ind[i] == 1]), set()
+        while len(rem) > 2:
+            rem -= q
+            for i in q:
+                for ni in list(adj[i]):
+                    ind[ni] -= 1
+                    if ind[ni] == 1:
+                        nq.add(ni)
+                        adj[i].remove(ni)
+                        adj[ni].remove(i)
+            q, nq = nq, set()
+        return q    
