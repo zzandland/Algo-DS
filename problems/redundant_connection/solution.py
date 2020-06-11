b09639848274436b5d23ffb4b283abb5f316f15e
Si Yong Kim <zzandland@gmail.com>
2020-06-11 18:13:57 +0000

    Sync LeetCode submission - Redundant Connection (python3)

--- problems/redundant_connection/solution.py
+++ problems/redundant_connection/solution.py
@@ -1,16 +1,22 @@
-from collections import defaultdict
+from collections import defaultdict, deque
 
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        parents = {}
+        mx = max([n for e in edges for n in e])
+        V = [i for i in range(mx+1)]
+        
         def find(n: int) -> int:
-            if n not in parents: parents[n] = n
-            if n != parents[n]: parents[n] = find(parents[n])
-            return parents[n]    
-        def union(a: int, b: int) -> None:
-            fa, fb = find(a), find(b)
-            parents[parents[a]] = b
+            if V[n] != n:
+                V[n] = find(V[n])
+            return V[n]
+        
+        def union(a: int, b: int) -> bool:
+            af, bf = find(a), find(b)
+            if af == bf:
+                return False
+            V[af] = bf
+            return True
+        
         for u, v in edges:
-            fu, fv = find(u), find(v)
-            if fu == fv: return u, v
-            union(u, v)
+            if not union(u, v):
+                return (u, v)
