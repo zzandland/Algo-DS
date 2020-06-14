16d8e7a5c8df087aecfd6c3a8d4f1ee1d9cbc434
Si Yong Kim <zzandland@gmail.com>
2020-06-14 19:43:22 +0000

    Sync LeetCode submission - Rotting Oranges (python3)

--- problems/rotting_oranges/solution.py
+++ problems/rotting_oranges/solution.py
@@ -1,26 +1,28 @@
-from collections import deque
-
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        if not grid: return -1
+        if not grid:
+            return 0
         R, C = len(grid), len(grid[0])
-        q, left = deque(), set()
-        for y, row in enumerate(grid):
-            for x, cell in enumerate(row):
-                if cell == 2: q.append((y, x))
-                if cell == 1: left.add((y, x))
-        t = rotCnt = 0
-        dir_ = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-        while left and q:
-            t += 1            
-            l, rotCnt = len(q), 0
-            for _ in range(l):
-                y, x = q.popleft()
-                for v, h in dir_:
-                    ny, nx = y+v, x+h
-                    if 0 <= ny < R and 0 <= nx < C and grid[ny][nx] == 1: 
-                        rotCnt += 1
-                        left.remove((ny, nx))
-                        grid[ny][nx] = 2
-                        q.append((ny, nx))
-        return -1 if left else t
+        q, r = [], 0
+        for y in range(R):
+             for x in range(C):
+                if grid[y][x] == 2:
+                    q.append([y, x])
+                elif grid[y][x] == 1:
+                    r += 1
+        if r == 0:
+            return 0
+        dir_ = ((1, 0), (-1, 0), (0, 1), (0, -1))
+        res = 0
+        while q:
+            if r == 0:
+                return res
+            nq = []
+            for ny, nx in [[y+r, x+c] for y, x in q for r, c in dir_]:
+                if 0 <= ny < R and 0 <= nx < C and grid[ny][nx] == 1:
+                    r -= 1
+                    grid[ny][nx] = 2
+                    nq.append([ny, nx])
+            res += 1
+            q = nq
+        return -1
