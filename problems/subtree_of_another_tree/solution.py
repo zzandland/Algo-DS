db0886eb805de1c19c5177d1cdb74d5161b510a8
Si Yong Kim <zzandland@gmail.com>
2020-06-15 03:30:10 +0000

    Sync LeetCode submission - Subtree of Another Tree (python3)

--- problems/subtree_of_another_tree/solution.py
+++ problems/subtree_of_another_tree/solution.py
@@ -1,18 +1,20 @@
 # Definition for a binary tree node.
 # class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def isSubtree(self, s: TreeNode, t: TreeNode) -> bool:
-        def hashify(n: TreeNode) -> str:
-            if not n: return '#'
-            return '[{},{},{}]'.format(n.val, hashify(n.left), hashify(n.right))
-        t_hashed = hashify(t)
-        def traverse(n: TreeNode) -> bool:
-            if not n: return False
-            if hashify(n) == t_hashed: return True
-            return traverse(n.left) or traverse(n.right)
-        return traverse(s)
+        def fn(a: TreeNode, b: TreeNode) -> bool:
+            if not a and not b:
+                return True
+            if not a or not b:
+                return False
+            if a.val == b.val:
+                if fn(a.left, b.left) and fn(a.right, b.right):
+                    return True
+            if b == t:
+                return fn(a.left, t) or fn(a.right, t)
+            return False
+        return fn(s, t)
