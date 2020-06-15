ec4a1522d17a6d9deebde576d2c834dc8952005f
Si Yong Kim <zzandland@gmail.com>
2020-06-15 12:41:40 +0000

    Sync LeetCode submission - Number of Islands (python3)

--- problems/number_of_islands/solution.py
+++ problems/number_of_islands/solution.py
@@ -2,17 +2,19 @@ from collections import deque
 
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid: return 0
-        R, C, res = len(grid), len(grid[0]), 0
-        for i in range(R):
-            for j in range(C):
-                if grid[i][j] == '1':
-                    grid[i][j], res, q = '0', res+1, deque([(i, j)])
-                    while q:
-                        y, x = q.popleft()
-                        for r, c in ((1, 0), (-1, 0), (0, 1), (0, -1)):
-                            ny, nx = y+r, x+c
-                            if 0 <= ny < R and 0 <= nx < C and grid[ny][nx] == '1':
-                                grid[ny][nx] = '0'
-                                q.append((ny, nx))
-        return res                        
+        if not grid:
+            return 0
+        R, C = len(grid), len(grid[0])
+        dir_, res = ((1, 0), (0, 1), (-1, 0), (0, -1)), 0
+        for y, x in [[y, x] for y in range(R) for x in range(C)]:
+            if grid[y][x] == '1':
+                res += 1
+                grid[y][x] = '0'
+                q = deque([[y, x]])
+                while q:
+                    r, c = q.popleft()
+                    for ny, nx in [[r+r2, c+c2] for r2, c2 in dir_]:
+                        if 0 <= ny < R and 0 <= nx < C and grid[ny][nx] == '1':
+                            grid[ny][nx] = '0'
+                            q.append([ny, nx])
+        return res
