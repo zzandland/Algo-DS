f0604ed9d8ea6dcfc7e92f1ecb28637513392718
Si Yong Kim <zzandland@gmail.com>
2020-06-19 03:09:03 +0000

    Sync LeetCode submission - Insert Interval (python3)

--- problems/insert_interval/solution.py
+++ problems/insert_interval/solution.py
@@ -1,18 +1,25 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        if not newInterval: return intervals
-        intervals.sort()
-        N, output, inserted = len(intervals), [], False
-        for i in range(N):
-            cur = intervals[i]
-            if cur[1] < newInterval[0]: output.append(cur)
-            elif cur[0] > newInterval[1]:
-                if not inserted:
-                    output.append(newInterval)
-                    inserted = True
-                output.append(cur)    
-            else: 
-                print(i, min(cur[0], newInterval[0]))
-                newInterval = [min(newInterval[0], cur[0]), max(newInterval[1], cur[1])]    
-        if not inserted: output.append(newInterval)        
-        return output
+        if not intervals:
+            return [newInterval]
+        N = len(intervals)
+        def bs(t: int) -> int:
+            l, r = 0, N-1
+            while l < r:
+                m = l + (r-l)//2
+                if intervals[m][1] <= t < intervals[m+1][0]:
+                    return m
+                if t > intervals[m][1]:
+                    l = m + 1
+                else:
+                    r = m
+            return l
+        a, b = bs(newInterval[0]), bs(newInterval[1])
+        if a == b == 0 and newInterval[1] < intervals[0][0]:
+            return [newInterval] + intervals
+        if a == b == N-1 and newInterval[0] > intervals[-1][1]:
+            return intervals + [newInterval]
+        if newInterval[0] > intervals[a][1]:
+            a += 1
+        merge = [min(intervals[a][0], newInterval[0]), max(intervals[b][1], newInterval[1])]
+        return intervals[:a] + [merge] + intervals[b+1:]
