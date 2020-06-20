f4b3ca1d126bb6a4855d4c3456f2e0e397aa71c5
Si Yong Kim <zzandland@gmail.com>
2020-06-20 04:53:01 +0000

    Sync LeetCode submission - Minimum Cost Tree From Leaf Values (python3)

--- problems/minimum_cost_tree_from_leaf_values/solution.py
+++ problems/minimum_cost_tree_from_leaf_values/solution.py
@@ -1,10 +1,11 @@
 class Solution:
     def mctFromLeafValues(self, arr: List[int]) -> int:
-        if len(arr) < 2: return sum(arr)
-        q, res = [n for n in arr], 0
-        while len(q) > 1:
-            N = len(q)
-            _, i = min([(q[i]+q[i+1], i) for i in range(N-1)], key=lambda x: x[0])
-            res += q[i]*q[i+1]
-            q = q[:i] + [(max(q[i], q[i+1]))] + q[i+2:]
-        return res    
+        res, st = 0, [float('inf')]
+        for n in arr:
+            while st[-1] <= n:
+                mn = st.pop()
+                res += mn * min(n, st[-1])
+            st.append(n)
+        while len(st) > 2:
+            res += st.pop() * st[-1]
+        return res
