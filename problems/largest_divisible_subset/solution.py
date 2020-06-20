00ecef20a2630ce74a761d6e518f48620a16509f
Si Yong Kim <zzandland@gmail.com>
2020-06-20 17:35:51 +0000

    Sync LeetCode submission - Largest Divisible Subset (python3)

--- problems/largest_divisible_subset/solution.py
+++ problems/largest_divisible_subset/solution.py
@@ -1,15 +1,19 @@
 class Solution:
     def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
+        if not nums:
+            return []
+        N = len(nums)
         nums.sort()
-        N, dp = len(nums), {}
-        def dq(i: int, l: int) -> List[int]:
-            if i == N:
-                return []
-            if (i, l) not in dp:
-                num = nums[i]
-                if l != 0 and num % l == 0 or l % num == 0:
-                    dp[i, l] = max([num] + dq(i+1, max(l, num)), dq(i+1, l), key=len)
-                else:
-                    dp[i, l] = dq(i+1, l)
-            return dp[i, l]
-        return dq(0, 0)
+        dp, mxi = [1]*N, N-1
+        for i in range(N-1, -1, -1):
+            for j in range(i+1, N):
+                if nums[j] % nums[i] == 0:
+                    dp[i] = max(dp[i], dp[j]+1)
+            mxi = max(mxi, i, key=lambda x: dp[x])
+        t, res = dp[mxi], []
+        while t > 0:
+            if dp[mxi] == t:
+                res.append(nums[mxi])
+                t -= 1
+            mxi += 1
+        return res
