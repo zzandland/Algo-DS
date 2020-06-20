5e9670edb7c75de9ce8c5861f75c7277584825c2
Si Yong Kim <zzandland@gmail.com>
2020-06-20 06:04:30 +0000

    Sync LeetCode submission - Search Suggestions System (python3)

--- problems/search_suggestions_system/solution.py
+++ problems/search_suggestions_system/solution.py
@@ -1,9 +1,22 @@
 from collections import defaultdict
 
+class Node:
+    def __init__(self):
+        self.next = defaultdict(Node)
+        self.wds = []
+
 class Solution:
     def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
-        t = defaultdict(list)
-        for product in products:
-            for i in range(1, len(product)+1):
-                t[product[:i]].append(product)
-        return [sorted(t[searchWord[:j]])[:3] for j in range(1, len(searchWord)+1)]
+        products.sort()
+        id2p, trie = {}, Node()
+        for i, p in enumerate(products):
+            n = trie
+            id2p[i] = p
+            for c in p:
+                n = n.next[c]
+                n.wds.append(i)
+        n, res = trie, []
+        for s in searchWord:
+            n = n.next[s]
+            res.append([id2p[id_] for id_ in n.wds[:3]])
+        return res
