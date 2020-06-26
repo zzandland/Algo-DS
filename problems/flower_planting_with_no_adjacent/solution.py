83602131f5ba049bf66282c60ed40f3697ba66b1
Si Yong Kim <zzandland@gmail.com>
2020-06-26 00:34:11 +0000

    Sync LeetCode submission - Flower Planting With No Adjacent (python3)

--- problems/flower_planting_with_no_adjacent/solution.py
+++ problems/flower_planting_with_no_adjacent/solution.py
@@ -1,15 +1,22 @@
-from collections import defaultdict, deque
+from collections import defaultdict
 
 class Solution:
     def gardenNoAdj(self, N: int, paths: List[List[int]]) -> List[int]:
-        adj, colors = defaultdict(list), [0]*(N+1)
-        for a, b in paths:
-            adj[a].append(b)
-            adj[b].append(a)
+        adj, seen, V = defaultdict(list), set(), [0]*(N+1)
+        for u, v in paths:
+            adj[u].append(v)
+            adj[v].append(u)
+        def dfs(n: int) -> None:
+            colors = {1, 2, 3, 4}
+            for nei in adj[n]:
+                if V[nei] in colors: colors.remove(V[nei])
+            V[n] = next(iter(colors))
+            for nei in adj[n]:
+                if nei not in seen:
+                    seen.add(nei)
+                    dfs(nei)
         for i in range(1, N+1):
-            cs = set([1,2,3,4])
-            for ni in adj[i]:
-                if colors[ni] in cs:
-                    cs.remove(colors[ni])
-            colors[i] = next(iter(cs))
-        return colors[1:]
+            if i not in seen:
+                seen.add(i)
+                dfs(i)
+        return V[1:]
