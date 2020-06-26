1cd03acda300238239f0fa7fb0a2eb14f9f5923e
Si Yong Kim <zzandland@gmail.com>
2020-06-26 05:55:32 +0000

    Sync LeetCode submission - Pow(x, n) (python3)

--- problems/pow(x,_n)/solution.py
+++ problems/pow(x,_n)/solution.py
@@ -1,12 +1,6 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        if n == 0: return 1    
-        pos, i, res = True, 0, 1
-        if n < 0: pos, n = False, n*-1
-        while n > 0:
-            i, tmp = 1, x
-            while i*2 <= n:
-                tmp *= tmp
-                i *= 2
-            n, res = n-i, res*tmp
-        return res if pos else 1/res
+        if n == 0: return 1
+        t = self.myPow(x, n//2 if n > 0 else ceil(n/2));
+        if n % 2: return 1/x*t*t if n < 0 else x*t*t
+        return t*t
