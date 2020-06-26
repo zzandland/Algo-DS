f7b736124680ff8f968a725a113e7b5cbb71c7dd
Si Yong Kim <zzandland@gmail.com>
2020-06-26 05:43:48 +0000

    Sync LeetCode submission - Unique Binary Search Trees II (python3)

--- problems/unique_binary_search_trees_ii/solution.py
+++ problems/unique_binary_search_trees_ii/solution.py
@@ -1,5 +1,3 @@
-from collections import deque
-
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -7,30 +5,27 @@ from collections import deque
 #         self.left = left
 #         self.right = right
 class Solution:
-    def copy(self, n: TreeNode) -> TreeNode:
-        if not n:
-            return None
-        res = TreeNode(n.val)
-        res.left = self.copy(n.left)
-        res.right = self.copy(n.right)
-        return res
-    
+    def copy(self, n: TreeNode) -> TreeNode:    
+        if not n: return n
+        cp = TreeNode(n.val)
+        cp.left = self.copy(n.left)
+        cp.right = self.copy(n.right)
+        return cp
+
     def generateTrees(self, n: int) -> List[TreeNode]:
-        if n == 0:
-            return []
+        if n == 0: return []
         dp = {}
-        def dq(l: int, r: int) -> List[TreeNode]:
-            if l > r:
-                return [None]
+        def fn(l: int, r: int) -> List[TreeNode]:
+            if l == r: return [None]
             if (l, r) not in dp:
-                dp[l, r] = []
-                for i in range(l, r+1):
-                    n = TreeNode(i)
-                    left, right = dq(l, i-1), dq(i+1, r)
-                    for ln in left:
-                        n.left = ln
-                        for rn in right:
-                            n.right = rn
-                            dp[l, r].append(self.copy(n))
+                res = []
+                for j in range(l, r):
+                    tmp = TreeNode(j+1)
+                    for ln in fn(l, j):
+                        tmp.left = ln
+                        for rn in fn(j+1, r):
+                            tmp.right = rn
+                            res.append(self.copy(tmp))
+                dp[l, r] = res
             return dp[l, r]
-        return dq(1, n)
+        return fn(0, n)
