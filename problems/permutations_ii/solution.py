e7f5f1f800ce4815580f2ce93abce0314b176000
Si Yong Kim <zzandland@gmail.com>
2020-06-27 22:11:07 +0000

    Sync LeetCode submission - Permutations II (python3)

--- problems/permutations_ii/solution.py
+++ problems/permutations_ii/solution.py
@@ -1,11 +1,13 @@
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-        res = [[]]
-        for n in sorted(nums):
-            tmp = []
-            for p in res:
-                for i in range(len(p)+1):
-                    tmp.append(p[:i]+[n]+p[i:])
-                    if i < len(p) and p[i] == n: break
-            res = tmp            
-        return res
+        if not nums: return []
+        nums.sort()
+        q = [[]]
+        for i, n in enumerate(nums):
+            nq = []
+            for p in q:
+                for j in range(len(p)+1):
+                    if j > 0 and n == p[j-1]: break
+                    nq.append(p[:j]+[n]+p[j:])
+            q = nq
+        return q
