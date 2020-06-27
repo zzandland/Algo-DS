e8ee6cb013100eb6bef60f17392d3cf5af661212
Si Yong Kim <zzandland@gmail.com>
2020-06-27 03:44:26 +0000

    Sync LeetCode submission - Find Peak Element (python3)

--- problems/find_peak_element/solution.py
+++ problems/find_peak_element/solution.py
@@ -1,11 +1,10 @@
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
-        N = len(nums)
-        l, r = 0, N-1
-        while l+1 < r:
+        nums = [float('-inf')] + nums + [float('-inf')]
+        l, r = 0, len(nums)-1
+        while l + 1 < r:
             m = l + (r-l)//2
-            if nums[m-1] < nums[m] and nums[m+1] < nums[m]: return m
-            if nums[m-1] < nums[m] < nums[m+1]: l = m
+            if nums[m-1] < nums[m] and nums[m+1] < nums[m]: return m-1
+            if nums[m-1] < nums[m]: l = m
             else: r = m
-        return l if nums[l] > nums[r] else r
-        print(l, r, m)        
+                
