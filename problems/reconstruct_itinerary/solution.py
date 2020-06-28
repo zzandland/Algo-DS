c4445b885608244f01f03693ba170e10b1d98926
Si Yong Kim <zzandland@gmail.com>
2020-06-28 15:43:43 +0000

    Sync LeetCode submission - Reconstruct Itinerary (python3)

--- problems/reconstruct_itinerary/solution.py
+++ problems/reconstruct_itinerary/solution.py
@@ -2,14 +2,22 @@ from collections import defaultdict
 
 class Solution:
     def findItinerary(self, tickets: List[List[str]]) -> List[str]:
-        dic = collections.defaultdict(list)
-        for to,fro in sorted(tickets)[::-1]:
-            dic[to].append(fro)
-        print(dic)    
-        route = []
-        def visit(airport):
-            while dic[airport]:
-                visit(dic[airport].pop())
-            route.append(airport)
-        visit('JFK')
-        return route[::-1]
+        adj, seen = defaultdict(set), defaultdict(int)
+        for u, v in tickets:
+            adj[u].add(v)
+            seen[u, v] += 1
+        found = False
+        def dfs(u: int, cnt: int) -> List[str]:
+            nonlocal found
+            if cnt == 0:
+                found = True
+                return [u]
+            res = []
+            for v in sorted(adj[u]):
+                if seen[u, v] > 0:
+                    seen[u, v] -= 1
+                    tmp = [u] + dfs(v, cnt-1)
+                    if found: return tmp
+                    seen[u, v] += 1
+            return []
+        return dfs('JFK', len(tickets))
