e504a4edadbedf5b38c11f14f1ccedba07188ce1
Si Yong Kim <zzandland@gmail.com>
2020-06-29 18:38:09 +0000

    Sync LeetCode submission - Strobogrammatic Number (python3)

--- problems/strobogrammatic_number/solution.py
+++ problems/strobogrammatic_number/solution.py
@@ -1,15 +1,11 @@
 class Solution:
     def isStrobogrammatic(self, num: str) -> bool:
-        if not num: return True
+        stro = {'0': '0', '1': '1', '6': '9', '8': '8', '9': '6'}
         l, r = 0, len(num)-1
-        nt = ('2', '3', '4', '5', '7')
-        while l < r:
-            if num[l] in nt or num[r] in nt: return False
-            if num[l] == '6' and num[r] != '9': return False
-            if num[l] == '9' and num[r] != '6': return False
-            if num[l] == '0' and num[r] != '0': return False
-            if num[l] == '1' and num[r] != '1': return False
-            if num[l] == '8' and num[r] != '8': return False
-            l, r = l+1, r-1
-        if l == r: return num[l] in ('0', '1', '8')    
+        while l <= r:
+            lc, rc = num[l], num[r]
+            if lc not in stro or rc not in stro: return False
+            if stro[lc] != rc or stro[rc] != lc: return False
+            l += 1
+            r -= 1
         return True
