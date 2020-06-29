9d130c8546588156b1cdac4e589c610cc09b2a47
Si Yong Kim <zzandland@gmail.com>
2020-06-29 13:02:15 +0000

    Sync LeetCode submission - Unique Paths (python3)

--- problems/unique_paths/solution.py
+++ problems/unique_paths/solution.py
@@ -1,8 +1,8 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        if n > m: m, n = n, m
-        dp = [1] + [0]*(m-1)
-        for _ in range(n):
-            for j in range(1, m):
-                dp[j] += dp[j-1]
-        return dp[-1]        
+        dp = [[0]*(n+1) for _ in range(m+1)]
+        dp[0][1] = 1
+        for y in range(1, m+1):
+            for x in range(1, n+1):
+                dp[y][x] = dp[y-1][x] + dp[y][x-1]
+        return dp[m][n]
