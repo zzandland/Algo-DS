db5a57ff14bcd66946b99556b457b21e4492cf43
Si Yong Kim <zzandland@gmail.com>
2020-06-30 03:22:59 +0000

    Sync LeetCode submission - Moving Average from Data Stream (python3)

--- problems/moving_average_from_data_stream/solution.py
+++ problems/moving_average_from_data_stream/solution.py
@@ -7,13 +7,14 @@ class MovingAverage:
         Initialize your data structure here.
         """
         self.q = deque()
-        self.sum = 0
         self.size = size
+        self.sum = 0
 
     def next(self, val: int) -> float:
-        if len(self.q) == self.size: self.sum -= self.q.popleft()
         self.q.append(val)
         self.sum += val
+        if len(self.q) > self.size:
+            self.sum -= self.q.popleft()
         return self.sum / len(self.q)
 
 # Your MovingAverage object will be instantiated and called as such:
