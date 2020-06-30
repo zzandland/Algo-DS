e7ea9ad59928a3dd4676d4c194064f1a281c2e11
Si Yong Kim <zzandland@gmail.com>
2020-06-30 19:44:38 +0000

    Sync LeetCode submission - Word Search II (python3)

--- problems/word_search_ii/solution.py
+++ problems/word_search_ii/solution.py
@@ -1,33 +1,27 @@
-from collections import defaultdict
-
 class Solution:
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
-        R, C = len(board), len(board[0])
-        dic, n = {}, None
+        trie = {}
         for w in words:
-            n = dic
-            for i in range(len(w)):
-                n.setdefault(w[i], {})
-                n = n[w[i]]
-            n['*'] = 1    
-        output = []
-        def dfs(y: int, x: int, s: str, n: object) -> None:
-            if s == 'bend': print(n)
-            if '*' in n: 
+            n = trie
+            for c in w:
+                if c not in n: n[c] = {}
+                n = n[c]
+            n['*'] = w
+        M, N = len(board), len(board[0])
+        dir_ = ((1, 0), (-1, 0), (0, 1), (0, -1))
+        def dfs(n: dict, y: int, x: int) -> None:
+            if '*' in n:
+                res.append(n['*'])
                 del n['*']
-                output.append(s)
-            for r, c in ((1, 0), (-1, 0), (0, 1), (0, -1)):
-                ny, nx = y+r, x+c    
-                if 0 <= ny < R and 0 <= nx < C and (ny, nx) not in visited:
-                    ch = board[ny][nx]
-                    if ch in n: 
-                        visited.add((ny, nx))
-                        dfs(ny, nx, s+ch, n[ch])
-                        visited.remove((ny, nx))
-        for y in range(R):
-            for x in range(C):
-                ch = board[y][x]
-                if ch in dic:
-                    visited = set([(y, x)])
-                    dfs(y, x, ch, dic[ch])
-        return output
+            for ny, nx in [(y+r, x+c) for r, c in dir_]:
+                if 0 <= ny < M and 0 <= nx < N and board[ny][nx] in n and (ny, nx) not in seen:
+                    seen.add((ny, nx))
+                    dfs(n[board[ny][nx]], ny, nx)
+                    seen.remove((ny, nx))
+        res = []
+        for y in range(M):
+            for x in range(N):
+                if board[y][x] in trie:
+                    seen = set([(y, x)])
+                    dfs(trie[board[y][x]], y, x)
+        return sorted(res)
