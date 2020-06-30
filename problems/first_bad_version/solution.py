383f0fc8352343e930bcc29ea5d2ba63d319fa28
Si Yong Kim <zzandland@gmail.com>
2020-06-30 19:47:09 +0000

    Sync LeetCode submission - First Bad Version (python3)

--- problems/first_bad_version/solution.py
+++ problems/first_bad_version/solution.py
@@ -14,5 +14,5 @@ class Solution:
             m = l + (r-l)//2
             if not isBadVersion(m) and isBadVersion(m+1): return m+1
             if not isBadVersion(m): l = m+1
-            else: r = m    
-        return l        
+            else: r = m
+        return l
