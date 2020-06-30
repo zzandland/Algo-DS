cabfdbf35f3d5d15aa55c1dba5fe877ceaaf5755
Si Yong Kim <zzandland@gmail.com>
2020-06-30 00:39:52 +0000

    Sync LeetCode submission - Merge Intervals (python3)

--- problems/merge_intervals/solution.py
+++ problems/merge_intervals/solution.py
@@ -1,14 +1,11 @@
-import heapq
-
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         if not intervals: return []
-        heapq.heapify(intervals)
-        res = []
-        while intervals:
-            i, j = heapq.heappop(intervals)
-            if not res:
-                res.append([i, j])
-            elif i <= res[-1][1]: res[-1][1] = max(j, res[-1][1])
-            else: res.append([i, j])
-        return res        
+        intervals.sort()
+        res = [intervals[0]]
+        for i in range(1, len(intervals)):
+            s1, e1 = res[-1]
+            s2, e2 = intervals[i]
+            if e1 >= s2: res[-1] = [min(s1, s2), max(e1, e2)]
+            else: res.append([s2, e2])
+        return res
