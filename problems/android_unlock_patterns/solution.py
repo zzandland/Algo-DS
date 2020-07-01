a93c60248fc7a5bc010a5e44008ed32a31c84958
Si Yong Kim <zzandland@gmail.com>
2020-07-01 17:23:04 +0000

    Sync LeetCode submission - Android Unlock Patterns (python3)

--- problems/android_unlock_patterns/solution.py
+++ problems/android_unlock_patterns/solution.py
@@ -1,36 +1,49 @@
 class Solution:
     def numberOfPatterns(self, m: int, n: int) -> int:
-        visited, res = set(), 0
-        fives = [0, 9, 8, 7, 6, 5, 4, 3, 2, 1]
-        def travel(x: int) -> set:
-            if x in [1, 3, 7, 9]: res = set([2, 4, 5, 6, 8])
-            elif x in [2, 8]: res = set([1, 3, 4, 5, 6, 7, 9])
-            elif x in [4, 6]: res = set([1, 2, 3, 5, 7, 8, 9])
-            else: res = set([1, 2, 3, 4, 6, 7, 8, 9])
-            if 5 in visited: res.add(fives[x])    
-            if x == 1:
-                if 2 in visited: res.add(3)
-                if 4 in visited: res.add(7)    
-            elif x == 3:
-                if 2 in visited: res.add(1) 
-                if 6 in visited: res.add(9)
-            elif x == 7:
-                if 4 in visited: res.add(1) 
-                if 8 in visited: res.add(9)
-            elif x == 9:
-                if 6 in visited: res.add(3)        
-                if 8 in visited: res.add(7)
-            return res - visited
-        def dfs(x: int, l: int) -> None:
-            nonlocal res
-            if l >= m: res += 1
-            if l == n: return
-            for nxt in travel(x):
-                visited.add(nxt)
-                dfs(nxt, l+1)
-                visited.remove(nxt)
+        def genAvail(i: int) -> List[int]:
+            res = set()
+            if i == 1:
+                if 2 in seen: res.add(3)
+                if 5 in seen: res.add(9)
+                if 4 in seen: res.add(7)
+            elif i == 2:
+                if 5 in seen: res.add(8)
+                res |= set([1, 4, 7, 5, 9, 6, 3])
+            elif i == 3:
+                if 2 in seen: res.add(1)
+                if 5 in seen: res.add(7)
+                if 6 in seen: res.add(9)
+            elif i == 4:    
+                if 5 in seen: res.add(6)
+                res |= set([1, 2, 3, 5, 7, 8, 9])
+            elif i == 5:
+                res |= set([1, 2, 3, 4, 6, 7, 8, 9])
+            elif i == 6:
+                if 5 in seen: res.add(4)
+                res |= set([3, 2, 1, 5, 7, 8, 9])
+            elif i == 7:
+                if 4 in seen: res.add(1)
+                if 5 in seen: res.add(3)
+                if 8 in seen: res.add(9)
+            elif i == 8:
+                if 5 in seen: res.add(2)
+                res |= set([7,4,1,5,3,6,9])
+            elif i == 9:
+                if 6 in seen: res.add(3)
+                if 5 in seen: res.add(1)
+                if 8 in seen: res.add(7)
+            if i in (1, 3, 7, 9): res |= set([2, 5, 4, 8, 6])
+            return res - seen
+        def dfs(i: int, k: int) -> int:
+            if k == n: return 1
+            res = 0
+            for ni in genAvail(i):
+                seen.add(ni)
+                res += dfs(ni, k+1)
+                seen.remove(ni)
+            return int(m <= k) + res
+        out = 0
         for i in range(1, 10):
-            visited.add(i)
-            dfs(i, 1)
-            visited.remove(i)
-        return res    
+            seen = set([i])
+            out += dfs(i, 1)            
+        return out
