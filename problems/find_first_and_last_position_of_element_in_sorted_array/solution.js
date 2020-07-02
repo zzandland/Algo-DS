62ee3460b8f48bdb8be03055f44a34c5a0ec0afc
Si Yong Kim <zzandland@gmail.com>
2020-07-02 23:51:10 +0000

    Sync LeetCode submission - Find First and Last Position of Element in Sorted Array (javascript)

--- problems/find_first_and_last_position_of_element_in_sorted_array/solution.js
+++ problems/find_first_and_last_position_of_element_in_sorted_array/solution.js
@@ -4,23 +4,17 @@
  * @return {number[]}
  */
 var searchRange = function(nums, target) {
-  if (nums === null || nums.length === 0) return [-1, -1];
-  let left = 0;
-  let right = nums.length - 1;
-  while (right > left + 1) {
-    let mid = left + Math.floor((right - left) / 2);
-    if (nums[mid] === target) {
-      let i, j;
-      i = j = mid;
-      while (nums[i] === target) { i++; }
-      while (nums[j] === target) { j--; }
-      return [j + 1, i - 1];
+    var N = nums.length;
+    var bs = function(t) {
+        let l = 0, r = N;
+        while (l < r) {
+            var m = l + Math.floor((r-l) / 2);
+            if (nums[m] < t) l = m+1;
+            else r = m;
+        }
+        return l;
     }
-    if (nums[mid] > target) right = mid;
-    else left = mid;  
-  }
-  if (nums[left] === target && nums[right] === target) return [left, right];
-  if (nums[left] === target) return [left, left];
-  if (nums[right] === target) return [right, right];
-  return [-1, -1];
+    l = bs(target);
+    if (l == N || nums[l] != target) return [-1, -1];
+    return [l, bs(target+1)-1];
 };
