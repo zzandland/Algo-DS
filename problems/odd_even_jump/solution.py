5f07718e25c7f63920f7c9eb6b65e8e8550ef2f3
Si Yong Kim <zzandland@gmail.com>
2020-07-02 01:35:44 +0000

    Sync LeetCode submission - Odd Even Jump (python3)

--- problems/odd_even_jump/solution.py
+++ problems/odd_even_jump/solution.py
@@ -1,22 +1,26 @@
 class Solution:
     def oddEvenJumps(self, A: List[int]) -> int:
-        if not A: return 0
-        L = len(A)
-        odds, evens = [False]*L, [False]*L
+        N = len(A)
+        odds, evens = [False]*N, [False]*N
         odds[-1] = evens[-1] = True
-        def make(N: List[int]) -> List[int]:
-            output = [None]*L
-            s = []
-            for i in N:
-                while s and s[-1] < i:
-                    output[s.pop()] = i
-                s.append(i)
-            return output    
-        B = sorted(range(L), key=lambda i: A[i])
-        oddNext = make(B)
-        B.sort(key=lambda i: -A[i])
-        evenNext = make(B)
-        for i in range(L-2, -1, -1):
-            if oddNext[i] != None: odds[i] = evens[oddNext[i]]
-            if evenNext[i] != None: evens[i] = odds[evenNext[i]]
-        return sum(odds)
+        oddNext, evenNext = [-1]*N, [-1]*N
+        B = sorted(range(N), key=lambda x: A[x])
+        st = []
+        for n in B:
+            while st and st[-1] < n:
+                oddNext[st.pop()] = n
+            st.append(n)
+        st = []    
+        B.sort(key=lambda x: -A[x])
+        for n in B:
+            while st and st[-1] < n:
+                evenNext[st.pop()] = n
+            st.append(n)
+        res = 1
+        for i in range(N-1, -1, -1):
+            if oddNext[i] != -1:
+                odds[i] = evens[oddNext[i]]
+                if odds[i]: res += 1
+            if evenNext[i] != -1:
+                evens[i] = odds[evenNext[i]]
+        return res
