1c73279e613de259555d527cea2bed49a9990cfa
Si Yong Kim <zzandland@gmail.com>
2020-07-03 15:07:43 +0000

    Sync LeetCode submission - Evaluate Division (python3)

--- problems/evaluate_division/solution.py
+++ problems/evaluate_division/solution.py
@@ -2,26 +2,32 @@ from collections import defaultdict
 
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
-        graph = defaultdict(dict)
-        for i, (a, b) in enumerate(equations):
-            graph[a][b] = values[i]
-            graph[b][a] = 1/values[i] if values[i] != 0 else -1
-        visited = set()    
-        output = []
-        def dfs(n: int, t: int) -> float:
+        adj = defaultdict(list)
+        def div(a: int, b: int) -> float:
+            if b == 0: return 0
+            return a / b
+        for (u, v), val in zip(equations, values):
+            adj[u].append((v, val))
+            adj[v].append((u, div(1, val)))
+        res = []
+        def dfs(n: str, t: str) -> float:
             if n == t: return 1
-            if n in visited: return -1
-            visited.add(n)
-            for nxt in graph[n]:
-                if graph[n][nxt] != -1:
-                    f = dfs(nxt, t)
-                    if f != -1: return f * graph[n][nxt]
-            return -1    
-        for a, b in queries:
-            if a in graph:
-                if b not in graph[a]:
-                    visited = set()
-                    graph[a][b] = dfs(a, b)
-                output.append(graph[a][b])    
-            else: output.append(-1)    
-        return output    
+            if n in seen: return float('inf')
+            seen.add(n)
+            for nn, v in adj[n]:
+                tmp = dfs(nn, t)
+                if tmp != float('inf'): return v * tmp
+            return float('inf')
+        for u, v in queries:
+            if u not in adj:
+                res.append(-1.0)
+            else:
+                seen = set()
+                val = dfs(u, v)
+                if val == float('inf'):
+                    res.append(-1)
+                else:
+                    res. append(val)
+                    adj[u].append((v, val))
+                    adj[v].append((u, div(1, val)))
+        return res
