65e97e1afc74849b77cdc4461472dcee9083185e
Si Yong Kim <zzandland@gmail.com>
2020-07-05 00:23:42 +0000

    Sync LeetCode submission - Course Schedule IV (python3)

--- problems/course_schedule_iv/solution.py
+++ problems/course_schedule_iv/solution.py
@@ -1,13 +1,10 @@
-from collections import defaultdict
-
 class Solution:
     def checkIfPrerequisite(self, n: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
-        adj = [[set() for _ in range(n)] for _ in range(n)]
+        dp = [[False for _1 in range(n)] for _2 in range(n)]
         for u, v in prerequisites:
-            adj[u][v].add(u)
-        for x in range(n):
-            for y in range(n):
-                for z in range(n):
-                    if adj[y][x] and adj[x][z]:
-                        adj[y][z] = adj[y][x] | adj[x][z]
-        return [u in adj[u][v] for u, v in queries]
+            dp[u][v] = True
+        for i in range(n):
+            for j in range(n):
+                for k in range(n):
+                    dp[j][k] = dp[j][k] or (dp[j][i] and dp[i][k])
+        return [dp[u][v] for u, v in queries]
