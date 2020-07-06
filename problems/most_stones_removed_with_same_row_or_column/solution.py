93046c167f53875a6f9b62d1ec026e2dadf3032a
Si Yong Kim <zzandland@gmail.com>
2020-07-06 02:29:45 +0000

    Sync LeetCode submission - Most Stones Removed with Same Row or Column (python3)

--- problems/most_stones_removed_with_same_row_or_column/solution.py
+++ problems/most_stones_removed_with_same_row_or_column/solution.py
@@ -1,12 +1,23 @@
+from collections import defaultdict
+
 class Solution:
     def removeStones(self, stones: List[List[int]]) -> int:
-        UF = {}
-        def find(n: int) -> int:
-            UF.setdefault(n, n)
-            if UF[n] != n: UF[n] = find(UF[n])
-            return UF[n]
-        def union(a: int, b: int) -> None:
-            UF[find(a)] = find(b)
+        rows, cols = defaultdict(list), defaultdict(list)
         for y, x in stones:
-            union(y, x + 10000)    
-        return len(stones) - len({find(x) for x in UF})   
+            rows[y].append(x)
+            cols[x].append(y)
+        def dfs(y: int, x: int) -> int:
+            if (y, x) in seen: return 0
+            seen.add((y, x))
+            res = 1
+            for nx in rows[y]:
+                res += dfs(y, nx)
+            for ny in cols[x]:
+                res += dfs(ny, x)
+            return res
+        seen = set()
+        res = 0
+        for y, x in stones:
+            if (y, x) not in seen:
+                res += dfs(y, x) - 1
+        return res
