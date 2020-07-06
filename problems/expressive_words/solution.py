e69479d4ba491d3bf9dde1dae6e83151732c7e42
Si Yong Kim <zzandland@gmail.com>
2020-07-06 02:20:52 +0000

    Sync LeetCode submission - Expressive Words (python3)

--- problems/expressive_words/solution.py
+++ problems/expressive_words/solution.py
@@ -1,30 +1,27 @@
-from typing import Tuple
-
 class Solution:
     def expressiveWords(self, S: str, words: List[str]) -> int:
-        if not S or not words: return 0
-        def compress(s: str) -> List[Tuple[str, int]]:
-            output, c1, cnt = [], s[0], 0
-            for c2 in s:
-                if c1 == c2: cnt += 1
-                else: 
-                    output.append((c1, cnt))
-                    cnt = 1
-                c1 = c2    
-            output.append((c1, cnt))    
-            return output    
-        sc = compress(S)
-        output = 0
-        for word in words:
-            if not word: continue
-            wc = compress(word)
-            if len(sc) == len(wc):
-                gud = True
-                for i in range(len(sc)):
-                    sl, scnt = sc[i]
-                    wl, wcnt = wc[i]
-                    if sl != wl or wcnt > scnt or (scnt < 3 and wcnt != scnt): 
-                        gud = False
-                        break
-                if gud: output += 1
-        return output            
+        def gen(s: str):
+            res = []
+            let, cnt = S[0], 1
+            for i in range(1, len(s)):
+                c = s[i]
+                if c != let:
+                    res.append((let, cnt))
+                    let, cnt = c, 1
+                else:
+                    cnt += 1
+            res.append((let, cnt))
+            return res
+        target = gen(S)
+        res = 0
+        for w in words:
+            wd = gen(w)
+            if len(wd) == len(target):
+                for a, b in zip(target, wd):
+                    tc, tf = a
+                    wc, wf = b
+                    if tc != wc or tf < wf: break
+                    if tf == 2 and tf > wf: break
+                else:
+                    res += 1
+        return res
