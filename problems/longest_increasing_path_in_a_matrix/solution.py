abc3aefd2456d1a30a8420901ba51456eaeb7ac8
Si Yong Kim <zzandland@gmail.com>
2020-07-06 02:35:56 +0000

    Sync LeetCode submission - Longest Increasing Path in a Matrix (python3)

--- problems/longest_increasing_path_in_a_matrix/solution.py
+++ problems/longest_increasing_path_in_a_matrix/solution.py
@@ -1,13 +1,14 @@
 class Solution:
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
         if not matrix: return 0
-        R, C = len(matrix), len(matrix[0])
-        lst = sorted([(y, x) for y in range(R) for x in range(C)], key=lambda x: -matrix[x[0]][x[1]])
-        dp, mx = [[1]*C for _ in range(R)], 1
-        for i, j in lst:
-            for r, c in ((1, 0), (-1, 0), (0, 1), (0, -1)):
-                ni, nj = i+r, j+c
-                if 0 <= ni < R and 0 <= nj < C and matrix[ni][nj] > matrix[i][j]:
-                    dp[i][j] = max(dp[i][j], dp[ni][nj]+1)
-                mx = max(mx, dp[i][j])
-        return mx            
+        M, N = len(matrix), len(matrix[0])
+        dp = [[0]*N for _ in range(M)]
+        def dfs(y: int, x: int) -> int:
+            if not dp[y][x]:
+                res = 0
+                for ny, nx in [(y + r, x + c) for r, c in ((1, 0), (-1, 0), (0, 1), (0, -1))]:
+                    if 0 <= ny < M and 0 <= nx < N and matrix[ny][nx] > matrix[y][x]:
+                        res = max(res, dfs(ny, nx))
+                dp[y][x] = res + 1
+            return dp[y][x]
+        return max([dfs(y, x) for y, x in [(y, x) for y in range(M) for x in range(N)]])
