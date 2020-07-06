9e3d9d0c6c85203a72416cbdd07f11f2087f36cc
Si Yong Kim <zzandland@gmail.com>
2020-07-06 13:42:32 +0000

    Sync LeetCode submission - Plus One (python3)

--- problems/plus_one/solution.py
+++ problems/plus_one/solution.py
@@ -1,10 +1,12 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        lft, idx = 0, len(digits)-1
-        digits[idx] += 1
-        while idx > 0 and digits[idx] == 10:
-            digits[idx] = 0
-            idx -= 1
-            digits[idx] += 1
-        if digits[0] == 10: return [1, 0] + digits[1:]
+        digits[-1] += 1
+        for i in range(len(digits)-1, 0, -1):
+            if digits[i] == 10:
+                digits[i] = 0
+                digits[i-1] += 1
+            else: break
+        if digits[0] == 10:
+            digits[0] = 0
+            digits = [1] + digits
         return digits
