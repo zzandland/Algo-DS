1848178cd748215205096e4b707c49672a1370b1
Si Yong Kim <zzandland@gmail.com>
2020-07-11 15:16:13 +0000

    Sync LeetCode submission - Perfect Squares (python3)

--- problems/perfect_squares/solution.py
+++ problems/perfect_squares/solution.py
@@ -1,11 +1,12 @@
 class Solution:
     def numSquares(self, n: int) -> int:
-        sqs, idx = [], 1
-        while idx*idx <= n:
-            sqs.append(idx*idx)
-            idx += 1
-        dp = [0] + [float('inf')]*(n)
+        sqs = []
+        i = 2
+        while i*i <= n:
+            sqs.append(i*i)
+            i += 1
+        dp = list(range(n+1))
         for sq in sqs:
             for i in range(sq, n+1):
-                dp[i] = min(dp[i], 1 + dp[i-sq])
-        return dp[n]
+                dp[i] = min(dp[i], dp[i - sq] + 1)
+        return dp[-1]
