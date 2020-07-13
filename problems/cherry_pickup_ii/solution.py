60e080217673ae0bbb7d5444b7a9a33b848d231a
Si Yong Kim <zzandland@gmail.com>
2020-07-13 17:11:05 +0000

    Sync LeetCode submission - Cherry Pickup II (python3)

--- problems/cherry_pickup_ii/solution.py
+++ problems/cherry_pickup_ii/solution.py
@@ -1,19 +1,15 @@
 class Solution:
     def cherryPickup(self, grid: List[List[int]]) -> int:
-        if not grid:
-            return 0
-        R, C = len(grid), len(grid[0])
-        dp = {}
-        def dq(c1: int, c2: int, y: int) -> int:
-            if y == R or not (0 <= c1 < C) or not (0 <= c2 < C):
-                return 0
-            key = '{}:{}:{}'.format(c1, c2, y)
-            if key not in dp:
-                take = grid[y][c1] + grid[y][c2] if c1 != c2 else grid[y][c1]
-                dp[key] = take + max(
-                    dq(c1-1, c2-1, y+1), dq(c1, c2-1, y+1), dq(c1+1, c2-1, y+1),
-                    dq(c1-1, c2, y+1), dq(c1, c2, y+1), dq(c1+1, c2, y+1),
-                    dq(c1-1, c2+1, y+1), dq(c1, c2+1, y+1), dq(c1+1, c2+1, y+1)
-                )
-            return dp[key]
-        return dq(0, C-1, 0)
+        if not grid: return 0
+        M, N = len(grid), len(grid[0])
+        dp = [[0]*(N+2) for _ in range(N+2)]
+        
+        for y in range(M-1, -1, -1):
+            ndp = [[0]*(N+2) for _ in range(N+2)]
+            for x1 in range(1, N+1):
+                for x2 in range(x1+1, N+1):
+                    prev = max(dp[px1][px2] for px1, px2 in [(x1+c1, x2+c2) for c1 in (-1, 0, 1) for c2 in (-1, 0, 1)])
+                    ndp[x1][x2] = grid[y][x1-1] + grid[y][x2-1] + prev
+            dp = ndp
+            
+        return dp[1][N]
