2fa183f0ede2415525dac1750852239d147a3959
Si Yong Kim <zzandland@gmail.com>
2020-07-13 15:34:34 +0000

    Sync LeetCode submission - Cherry Pickup (python3)

--- problems/cherry_pickup/solution.py
+++ problems/cherry_pickup/solution.py
@@ -1,22 +1,23 @@
 class Solution:
     def cherryPickup(self, grid: List[List[int]]) -> int:
         if not grid: return 0
-        N = len(grid)
-        dp = [[[None]*N for _ in range(N)] for _ in range(N)]
-        def fn(y1: int, x1: int, y2: int) -> int:
+        M, N = len(grid), len(grid[0])
+        dp = {}
+        
+        def dq(y1: int, y2: int, x1: int) -> int:
             x2 = y1 + x1 - y2
-            if (y1 == N or x1 == N or y2 == N or x2 == N or
-                grid[y1][x1] == -1 or grid[y2][x2] == -1):
-                return float('-inf')
-            if y1 == y2 == x1 == x2 == N-1:
-                return grid[y1][x1]
-            if dp[y1][x1][y2] is None:
-                collect = grid[y1][x1] + grid[y2][x2]
-                if y1 == y2 and x1 == x2:
-                    collect -= grid[y2][x2]
-                dp[y1][x1][y2] = (collect + max(
-                    fn(y1+1, x1, y2+1), fn(y1+1, x1, y2),
-                    fn(y1, x1+1, y2+1), fn(y1, x1+1, y2)
-                ))
-            return dp[y1][x1][y2]    
-        return max(0, fn(0, 0, 0))            
+            if y1 == M-1 and y2 == M-1 and x1 == N-1 and x2 == N-1: return grid[y1][x1]
+            if y1 == M or y2 == M or x1 == N or x2 == N: return float('-inf')
+            if grid[y1][x1] == -1 or grid[y2][x2] == -1: return float('-inf')
+            if (y1, y2, x1) not in dp:
+                res = grid[y1][x1] + grid[y2][x2]
+                if res > 0 and y1 == y2: res -= 1
+                dp[y1, y2, x1] = res + max(
+                    dq(y1 + 1, y2 + 1, x1),
+                    dq(y1 + 1, y2, x1),
+                    dq(y1, y2 + 1, x1 + 1),
+                    dq(y1, y2, x1 + 1)
+                )
+            return dp[y1, y2, x1]
+        
+        return max(0, dq(0, 0, 0))
