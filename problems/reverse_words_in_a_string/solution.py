6626aeb1bfa52dad03c0f9a346c9ef6ac8406a61
Si Yong Kim <zzandland@gmail.com>
2020-07-15 13:43:26 +0000

    Sync LeetCode submission - Reverse Words in a String (python3)

--- problems/reverse_words_in_a_string/solution.py
+++ problems/reverse_words_in_a_string/solution.py
@@ -1,6 +1,3 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
-        words = s.split()
-        for i in range(len(words) // 2):
-            words[i], words[-1 - i] = words[-1 - i], words[i]
-        return ' '.join(words).strip()
+        return ' '.join(filter(bool, s.split()[::-1]))
