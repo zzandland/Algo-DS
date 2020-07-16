6c14dd294f296966ffc8f3108d7a98fa98391e4a
Si Yong Kim <zzandland@gmail.com>
2020-07-16 04:15:43 +0000

    Sync LeetCode submission - Range Sum Query 2D - Immutable (python3)

--- problems/range_sum_query_2d_-_immutable/solution.py
+++ problems/range_sum_query_2d_-_immutable/solution.py
@@ -1,12 +1,15 @@
 class NumMatrix:
 
     def __init__(self, matrix: List[List[int]]):
-        if matrix:
-            R, C = len(matrix)+1, len(matrix[0])+1
-            self.dp = [[0]*C for _ in range(R)]
-            for y in range(R-1):
-                for x in range(C-1):
-                    self.dp[y+1][x+1] = self.dp[y][x+1] + self.dp[y+1][x] - self.dp[y][x] + matrix[y][x]
+        if not matrix: return
+        m, n = len(matrix), len(matrix[0])
+        dp = [[0]*(n+1) for _ in range(m+1)]
+        
+        for y in range(1, m+1):
+            for x in range(1, n+1):
+                dp[y][x] = matrix[y-1][x-1] + dp[y-1][x] + dp[y][x-1] - dp[y-1][x-1]
+                
+        self.dp = dp
 
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
         return self.dp[row2+1][col2+1] - self.dp[row2+1][col1] - self.dp[row1][col2+1] + self.dp[row1][col1]
