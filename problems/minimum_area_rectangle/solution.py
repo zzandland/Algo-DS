da1f467955423154f31c23c06bf471217b949354
Si Yong Kim <zzandland@gmail.com>
2020-07-17 15:36:58 +0000

    Sync LeetCode submission - Minimum Area Rectangle (python3)

--- problems/minimum_area_rectangle/solution.py
+++ problems/minimum_area_rectangle/solution.py
@@ -1,14 +1,15 @@
-from collections import defaultdict
-
 class Solution:
     def minAreaRect(self, points: List[List[int]]) -> int:
-        cols, lasty, mn = defaultdict(list), {}, float('inf')
-        for y, x in points: cols[y].append(x)
-        for y, row in sorted(cols.items()):
-            row.sort()
-            for i, x2 in enumerate(row):
-                for j in range(i):
-                    x1 = row[j]
-                    if (x1, x2) in lasty: mn = min(mn, (x2-x1) * (y-lasty[(x1, x2)]))
-                    lasty[(x1, x2)] = y    
-        return mn if mn != float('inf') else 0
+        # store all points into a set O(n)
+        S = set(map(tuple, points))
+        L = list(S)
+        res = float('inf')
+        # loop each point O(n) -> O(n^2)
+        for i, (y1, x1) in enumerate(L):
+            # loop points upto ith index O(n)
+            for j in range(i):
+                y2, x2 = L[j]
+                # if the other coords are in set cal min area
+                if y1 != y2 and x1 != x2 and (y1, x2) in S and (y2, x1) in S:
+                    res = min(res, abs(y2-y1) * abs(x2-x1))
+        return res if res != float('inf') else 0
