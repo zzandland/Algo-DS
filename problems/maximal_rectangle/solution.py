7a5a830fcc2e8a6f961ccbf084aef7f6a3f68883
Si Yong Kim <zzandland@gmail.com>
2020-07-17 01:36:36 +0000

    Sync LeetCode submission - Maximal Rectangle (python3)

--- problems/maximal_rectangle/solution.py
+++ problems/maximal_rectangle/solution.py
@@ -1,20 +1,39 @@
 class Solution:
     def maximalRectangle(self, matrix: List[List[str]]) -> int:
         if not matrix: return 0
-        N, M = len(matrix), len(matrix[0])
-        dp = [[0]*M for _ in range(N+1)]
-        for y in range(1, N+1):
-            for x in range(M):
-                if matrix[y-1][x] == '1': dp[y][x] = dp[y-1][x] + 1
+        M, N = len(matrix), len(matrix[0])
+        h, l, r = [0]*N, [0]*N, [N-1]*N
         res = 0
-        for y in range(1, N+1):
-            row, st = dp[y], []
-            left, right = [-1]*M, [M]*M
-            for i, n in enumerate(row):
-                while st and st[-1][0] > n:
-                    right[st.pop()[1]] = i
-                if st: left[i] = st[-1][1]
-                st.append((n, i))
-            for i, n in enumerate(row):
-                res = max(res, (right[i]-left[i]-1) * n)
+        
+        # for each row O(m)
+        # O(3mn)
+        for y in range(M):
+            
+            # calculate maximum height from the cell O(n)
+            for x in range(N):
+                if matrix[y][x] == '1': h[x] += 1
+                else: h[x] = 0
+                    
+            # calculate left index limit that makes a rectangle O(n)
+            cur_left_bound = 0
+            for x in range(N):
+                if matrix[y][x] == '1':
+                    l[x] = max(l[x], cur_left_bound)
+                else:
+                    l[x] = 0
+                    cur_left_bound = x + 1
+                    
+            # calculate right index limit that makes a rectangle O(n)
+            cur_right_bound = N-1
+            for x in range(N-1, -1, -1):
+                if matrix[y][x] == '1':
+                    r[x] = min(r[x], cur_right_bound)
+                else:
+                    r[x] = N-1
+                    cur_right_bound = x - 1
+                    
+            # calculate maximum area from the cell O(n)
+            for x in range(N):
+                res = max(res, (r[x] - l[x] + 1) * h[x])
+                
         return res
