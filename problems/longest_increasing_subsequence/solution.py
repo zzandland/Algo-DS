8671924b41a27707f2c66b09a43c54eaa2d96131
Si Yong Kim <zzandland@gmail.com>
2020-07-18 14:54:06 +0000

    Sync LeetCode submission - Longest Increasing Subsequence (python3)

--- problems/longest_increasing_subsequence/solution.py
+++ problems/longest_increasing_subsequence/solution.py
@@ -1,20 +1,15 @@
+import bisect
+
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        N, res = len(nums), 0
-        dp = [float('-inf')]*N
-        for num in nums:
-            l, r = 0, res-1
+        N = len(nums)
+        dp = []
+        for i in range(N):
+            l, r = 0, len(dp)
             while l < r:
                 m = l + (r-l)//2
-                if dp[m] == num:
-                    l = r = m
-                elif dp[m] > num:
-                    r = m
-                else:
-                    l = m+1
-            if num > dp[l]:
-                dp[res] = num
-                res += 1
-            else:
-                dp[l] = num
-        return res
+                if dp[m] < nums[i]: l = m+1
+                else: r = m
+            if l == len(dp): dp.append(nums[i])
+            else: dp[l] = nums[i]
+        return len(dp)
