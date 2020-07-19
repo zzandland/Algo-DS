635e923af72b667ad31ccf5bef09c130d728b72a
Si Yong Kim <zzandland@gmail.com>
2020-07-19 14:06:24 +0000

    Sync LeetCode submission - Add Binary (python3)

--- problems/add_binary/solution.py
+++ problems/add_binary/solution.py
@@ -1,12 +1,18 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        i, j, lft, res = len(a)-1, len(b)-1, 0, []
+        res = []
+        i, j = len(a)-1, len(b)-1
+        lft = 0
         while i >= 0 or j >= 0:
-            av = int(a[i]) if i >= 0 else 0
-            bv = int(b[j]) if j >= 0 else 0
-            tmp = av + bv + lft
-            lft = tmp // 2
-            res.append(str(tmp % 2))
-            i, j = i-1, j-1
-        if lft == 1: res.append('1')
+            ab = int(a[i]) if i >= 0 else 0
+            bb = int(b[j]) if j >= 0 else 0
+            val = ab + bb + lft
+            if val >= 2:
+                val -= 2
+                lft = 1
+            else: lft = 0
+            res.append(str(val))
+            i -= 1
+            j -= 1
+        if lft: res.append(str(lft))
         return ''.join(res[::-1])
