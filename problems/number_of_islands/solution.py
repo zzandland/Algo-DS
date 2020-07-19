a10d74dae669559b2d3bec352f2f687efd75a927
Si Yong Kim <zzandland@gmail.com>
2020-07-19 03:52:13 +0000

    Sync LeetCode submission - Number of Islands (python3)

--- problems/number_of_islands/solution.py
+++ problems/number_of_islands/solution.py
@@ -1,20 +1,15 @@
-from collections import deque
-
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid:
-            return 0
-        R, C = len(grid), len(grid[0])
-        dir_, res = ((1, 0), (0, 1), (-1, 0), (0, -1)), 0
-        for y, x in [[y, x] for y in range(R) for x in range(C)]:
+        if not grid: return 0
+        M, N = len(grid), len(grid[0])
+        dir_ = ((1, 0), (-1, 0), (0, 1), (0, -1))
+        def dfs(y: int, x: int) -> None:
+            grid[y][x] = '0'
+            for ny, nx in ((y+r, x+c) for r, c in dir_):
+                if 0 <= ny < M and 0 <= nx < N and grid[ny][nx] == '1':  dfs(ny, nx)
+        res = 0
+        for y, x in ((y, x) for y in range(M) for x in range(N)):
             if grid[y][x] == '1':
                 res += 1
-                grid[y][x] = '0'
-                q = deque([[y, x]])
-                while q:
-                    r, c = q.popleft()
-                    for ny, nx in [[r+r2, c+c2] for r2, c2 in dir_]:
-                        if 0 <= ny < R and 0 <= nx < C and grid[ny][nx] == '1':
-                            grid[ny][nx] = '0'
-                            q.append([ny, nx])
+                dfs(y, x)
         return res
