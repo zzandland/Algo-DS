97fd03f675dd54083a52ec6cc5572837f2bec3c6
Si Yong Kim <zzandland@gmail.com>
2020-07-21 20:07:37 +0000

    Sync LeetCode submission - Word Search (python3)

--- problems/word_search/solution.py
+++ problems/word_search/solution.py
@@ -1,27 +1,18 @@
 class Solution:
-    def exist(self, grid: List[List[str]], match: str) -> bool:
-        if len(grid) == 0:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        M, N, L = len(board), len(board[0]), len(word)
+        seen = set()
+        # dfs and matching chars on board with the word
+        # set dynamically grows and shrinks to handle visiting agian
+        def dfs(y: int, x: int, i: int) -> bool:
+            if (y, x) in seen or board[y][x] != word[i]: return False
+            if i == L-1: return True
+            seen.add((y, x))
+            for ny, nx in ((y+r, x+c) for r, c in ((1, 0), (-1, 0), (0, 1), (0, -1))):
+                if 0 <= ny < M and 0 <= nx < N and dfs(ny, nx, i+1): return True
+            seen.remove((y, x))
             return False
-        if not match:
-            return False
-        visited = [[False for _ in range(len(grid[0]))] for _ in range(len(grid))]
         
-        def recurse(r: int, c: int, i: int) -> bool:
-            if i == len(match):
-                return True
-            for row, col in ((r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1)):
-                if 0 <= row < len(grid) and 0 <= col < len(grid[0]):
-                    if grid[row][col] == match[i] and not visited[row][col]:
-                        visited[row][col] = True
-                        if recurse(row, col, i + 1):
-                            return True
-                        visited[row][col] = False
-            return False            
-        for i, row in enumerate(grid):
-            for j, col in enumerate(row):
-                if col == match[0]:
-                    visited[i][j] = True
-                    if recurse(i, j, 1):                        
-                        return True
-                    visited[i][j] = False
-        return False            
+        for y, x in ((y, x) for y in range(M) for x in range(N)):
+            if dfs(y, x, 0): return True
+        return False
