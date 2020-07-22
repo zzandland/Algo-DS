47a726234cfcaf373e3483f2db9fde01f69d86fd
Si Yong Kim <zzandland@gmail.com>
2020-07-22 13:39:55 +0000

    Sync LeetCode submission - Binary Tree Zigzag Level Order Traversal (python3)

--- problems/binary_tree_zigzag_level_order_traversal/solution.py
+++ problems/binary_tree_zigzag_level_order_traversal/solution.py
@@ -1,30 +1,24 @@
 # Definition for a binary tree node.
 # class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def zigzagLevelOrder(self, root: TreeNode) -> List[List[int]]:
-        output = []
-        if root is None:
-            return output
-        q = collections.deque()
-        q.append(root)
-        reverse = False
-        while len(q) > 0:
-            k = len(q)
-            level = []
-            for i in range(k):
-                node = q.popleft()
-                level.append(node.val)
-                if node.left is not None:
-                    q.append(node.left)
-                if node.right is not None:    
-                    q.append(node.right)
-            if reverse is True:
-                level.reverse()
-            output.append(level)
-            reverse = not reverse    
-        return output        
+        if not root: return []
+        q = [root]
+        res = []
+        lv = 0
+        while q:
+            nq = []
+            tmp = []
+            for n in q:
+                tmp.append(n.val)
+                if n.left: nq.append(n.left)
+                if n.right: nq.append(n.right)
+            q = nq
+            if lv & 1 == 1: res.append(tmp[::-1])
+            else: res.append(tmp)
+            lv += 1
+        return res
