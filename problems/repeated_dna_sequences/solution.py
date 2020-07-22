c2b51c7e2101e1296a8260786b6d798185abef31
Si Yong Kim <zzandland@gmail.com>
2020-07-22 15:22:02 +0000

    Sync LeetCode submission - Repeated DNA Sequences (python3)

--- problems/repeated_dna_sequences/solution.py
+++ problems/repeated_dna_sequences/solution.py
@@ -1,14 +1,20 @@
 class Solution:
     def findRepeatedDnaSequences(self, s: str) -> List[str]:
+        # bitmap -> only possible combs are A, T, C, G: assign a bit to them
+        # A -> 00 T -> 01 C -> 10 G -> 11
+        # iterate and add code at index
+        bm = 0
         dic = {'A': 0, 'T': 1, 'C': 2, 'G': 3}
-        bitmask, seen, res = 0, set(), set()
-        for i in range(len(s)+1):
+        seen = set()
+        res = set()
+        for i in range(len(s)):
+            # if i is greater than 10, we need to erase the higher bits
             if i >= 10:
-                if bitmask in seen:
-                    res.add(s[i-10:i])
-                seen.add(bitmask)    
-            if i < len(s):
-                bitmask <<= 2
-                bitmask |= dic[s[i]]
-                bitmask &= ~(3 << 2*10)
-        return res
+                bm &= ~(3 << 20)
+                if bm in seen: res.add(s[i-10:i])
+                seen.add(bm)
+            bm <<= 2
+            bm |= dic[s[i]]
+        bm &= ~(3 << 20)
+        if bm in seen: res.add(s[i-9:i+1])
+        return list(res)
