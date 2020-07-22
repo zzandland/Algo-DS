bbfaa2741c2478f801566dce5d1606b980a45e46
Si Yong Kim <zzandland@gmail.com>
2020-07-22 04:43:14 +0000

    Sync LeetCode submission - Filling Bookcase Shelves (python3)

--- problems/filling_bookcase_shelves/solution.py
+++ problems/filling_bookcase_shelves/solution.py
@@ -1,13 +1,15 @@
 class Solution:
     def minHeightShelves(self, books: List[List[int]], shelf_width: int) -> int:
-        N, dp = len(books), {}
-        def dq(i: int, cur: int, mh: int) -> int:
-            if i == N: return mh
-            w, h = books[i]
-            if (i, cur) not in dp:
-                if cur+w > shelf_width:
-                    dp[i, cur] = mh + dq(i+1, w, h)
-                else:
-                    dp[i, cur] = min(dq(i+1, cur+w, max(mh, h)), mh + dq(i+1, w, h))
-            return dp[i, cur]
-        return dq(0, 0, 0)
+        N = len(books)
+        dp = [0]
+        for i in range(N):
+            w, j = books[i][0], i
+            while j >= 0 and w <= shelf_width:
+                j -= 1
+                w += books[j][0]
+            tmp = float('inf')
+            for k in range(j+1, i+1):
+                tmp = min(tmp, dp[k] + max(books[x][1] for x in range(k, i+1)))
+            dp.append(tmp)
+        return dp[-1]
+ 
