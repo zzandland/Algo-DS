8104bc133b7cfdeef690182093554dd6bfaf51d0
Si Yong Kim <zzandland@gmail.com>
2020-07-23 18:33:21 +0000

    Sync LeetCode submission - Reverse Linked List (python3)

--- problems/reverse_linked_list/solution.py
+++ problems/reverse_linked_list/solution.py
@@ -6,7 +6,7 @@
 class Solution:
     def reverseList(self, head: ListNode) -> ListNode:
         if not head or not head.next: return head
-        tmp = self.reverseList(head.next)
+        p = self.reverseList(head.next)
         head.next.next = head
         head.next = None
-        return tmp
+        return p
