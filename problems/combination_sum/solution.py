668f1e457c9d7e12ff006aca73ed54abd23db55a
Si Yong Kim <zzandland@gmail.com>
2020-07-23 16:30:14 +0000

    Sync LeetCode submission - Combination Sum (python3)

--- problems/combination_sum/solution.py
+++ problems/combination_sum/solution.py
@@ -1,14 +1,7 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        N, res, run = len(candidates), [], []
-        def fn(i: int, s: int) -> None:
-            if i == N or s > target: return
-            if s == target:
-                res.append(run[:])
-                return
-            for j in range(i, N):
-                run.append(candidates[j])
-                fn(j, s + candidates[j])
-                run.pop()
-        fn(0, 0)
-        return res
+        def dfs(i: int, run: int, tmp: int) -> List[int]:
+            if i == len(candidates) or run > target: return []
+            if run == target: return [tmp[:]]
+            return dfs(i, run + candidates[i], tmp + [candidates[i]]) + dfs(i+1, run, tmp)
+        return dfs(0, 0, [])
