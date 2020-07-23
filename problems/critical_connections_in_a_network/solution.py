477f68402936196bfb2d4099c0f2aa3feaf4f310
Si Yong Kim <zzandland@gmail.com>
2020-07-23 07:03:14 +0000

    Sync LeetCode submission - Critical Connections in a Network (python3)

--- problems/critical_connections_in_a_network/solution.py
+++ problems/critical_connections_in_a_network/solution.py
@@ -2,22 +2,31 @@ from collections import defaultdict
 
 class Solution:
     def criticalConnections(self, n: int, connections: List[List[int]]) -> List[List[int]]:
+        # adj list O(con)
         adj = defaultdict(list)
-        for a, b in connections:
-            adj[a].append(b)
-            adj[b].append(a)
-        visit, low, seen, res, t = [None]*n, [None]*n, set(), [], 0
-        def dfs(c: int, p: int) -> None:
-            if c in seen: return
-            nonlocal t
-            seen.add(c)
-            visit[c] = low[c] = t
-            t += 1
-            for nn in adj[c]:
-                if nn != p:
-                    dfs(nn, c)
-                    low[c] = min(low[c], low[nn])
-                    if visit[c] < low[nn]: res.append([c, nn])
-            return low[c]    
+        for u, v in connections:
+            adj[u].append(v)
+            adj[v].append(u)
+        
+        # strongly connected components O(V+E)
+        id_ = 0
+        A = [None]*n
+        B = [0]*n
+        def dfs(i: int, p: int) -> int:
+            nonlocal id_
+            if A[i] != None:
+                B[i] = min(B[i], B[p])
+                return B[i]
+            A[i] = B[i] = id_
+            id_ += 1
+            for ni in adj[i]:
+                if ni != p: 
+                    bni = dfs(ni, i)
+                    B[i] = min(B[i], bni)
+            return B[i]
         dfs(0, -1)
-        return res
+        res = set()
+        for i in range(n):
+            for ni in adj[i]:
+                if B[i] != B[ni]: res.add(tuple(sorted([i, ni])))
+        return list(res)
