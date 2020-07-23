efa1dddfe84d8406be13b9a1f2bd888db99c723f
Si Yong Kim <zzandland@gmail.com>
2020-07-23 18:10:22 +0000

    Sync LeetCode submission - Subsets (python3)

--- problems/subsets/solution.py
+++ problems/subsets/solution.py
@@ -1,13 +1,6 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        N = len(nums)
-        tmp, res = [], []
-        def fn(i: int) -> None:
-            res.append(tmp[:])
-            if i == N: return
-            for j in range(i, N):
-                tmp.append(nums[j])
-                fn(j+1)
-                tmp.pop()
-        fn(0)
+        res = [[]]
+        if nums:
+            res += [[nums[i]] + rest for i in range(len(nums)) for rest in self.subsets(nums[i+1:])]
         return res
