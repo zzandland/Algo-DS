cb12b9e6ff959e410bfaa244c6eb6cb4771f5969
Si Yong Kim <zzandland@gmail.com>
2020-07-24 00:24:11 +0000

    Sync LeetCode submission - Palindrome Partitioning (python3)

--- problems/palindrome_partitioning/solution.py
+++ problems/palindrome_partitioning/solution.py
@@ -1,20 +1,21 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-        N = len(s)
-        run, res, dp = [], [], [[None]*N for _ in range(N)]
-        def checkPal(l: int, r: int) -> bool:
-            if l >= r: return True
-            if dp[l][r] == None:
-                dp[l][r] = s[l] == s[r] and checkPal(l+1, r-1)
-            return dp[l][r]
-        def recurse(i: int) -> None:
-            if i == N:
-                res.append(run[:])
-                return
-            for j in range(i, N):
-                if checkPal(i, j):
-                    run.append(s[i:j+1])
-                    recurse(j+1)
-                    run.pop()
-        recurse(0)
-        return res
+        S = len(s)
+        dp = [[False]*S for _ in range(S)]
+        for i in range(S-1, -1, -1):
+            dp[i][i] = True
+            for j in range(i+1, S):
+                if s[i] == s[j]:
+                    dp[i][j] = dp[i+1][j-1] or j-i == 1
+                    
+        def dfs(i: int, tmp: List[str]) -> List[List[str]]:
+            if i == S: return [tmp[:]]
+            res = []
+            for j in range(i, S):
+                if dp[i][j]:
+                    tmp.append(s[i:j+1])
+                    res += dfs(j+1, tmp)
+                    tmp.pop()
+            return res
+        
+        return dfs(0, [])
