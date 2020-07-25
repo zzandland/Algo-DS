f8a9b0ce330e3b110c6905ce733e15acdd9dec34
Si Yong Kim <zzandland@gmail.com>
2020-07-25 23:03:36 +0000

    Sync LeetCode submission - Critical Connections in a Network (python3)

--- problems/critical_connections_in_a_network/solution.py
+++ problems/critical_connections_in_a_network/solution.py
@@ -2,31 +2,26 @@ from collections import defaultdict
 
 class Solution:
     def criticalConnections(self, n: int, connections: List[List[int]]) -> List[List[int]]:
-        # adj list O(con)
-        adj = defaultdict(list)
+        # adj list of edges O(connections)
+        adj = defaultdict(set)
         for u, v in connections:
-            adj[u].append(v)
-            adj[v].append(u)
+            adj[u].add(v)
+            adj[v].add(u)
         
-        # strongly connected components O(V+E)
-        id_ = 0
-        A = [None]*n
-        B = [0]*n
-        def dfs(i: int, p: int) -> int:
-            nonlocal id_
-            if A[i] != None:
-                B[i] = min(B[i], B[p])
-                return B[i]
-            A[i] = B[i] = id_
-            id_ += 1
+        # dfs to label group that each node belongs to O(V+E)
+        label = [-1]*n
+        group = [-1]*n
+        cnt = 0
+        res = []
+        def dfs(i: int, p: int) -> None:
+            nonlocal cnt
+            label[i] = group[i] = cnt
+            cnt += 1
             for ni in adj[i]:
-                if ni != p: 
-                    bni = dfs(ni, i)
-                    B[i] = min(B[i], bni)
-            return B[i]
+                if ni != p:
+                    if label[ni] == -1: dfs(ni, i)
+                    group[i] = min(group[i], group[ni])
+                    if label[i] < group[ni]: res.append([i, ni])
         dfs(0, -1)
-        res = set()
-        for i in range(n):
-            for ni in adj[i]:
-                if B[i] != B[ni]: res.add(tuple(sorted([i, ni])))
-        return list(res)
+        
+        return res
