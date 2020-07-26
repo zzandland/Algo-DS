5b1c2a3f0b27f72beda62da7e06794426e1c3a96
Si Yong Kim <zzandland@gmail.com>
2020-07-26 19:38:56 +0000

    Sync LeetCode submission - Minimum Cost to Connect Sticks (python3)

--- problems/minimum_cost_to_connect_sticks/solution.py
+++ problems/minimum_cost_to_connect_sticks/solution.py
@@ -1,11 +1,14 @@
-import heapq
+from heapq import heappush, heappop, heapify
 
 class Solution:
     def connectSticks(self, sticks: List[int]) -> int:
-        heapq.heapify(sticks)
-        output = 0
+        # heapify sticks O(sticks) 
+        heapify(sticks)
+        
+        # take out two smallest sticks and stick them O(log sticks)
+        cost = 0
         while len(sticks) > 1:
-            sum_ = sum([heapq.heappop(sticks) for _ in range(2)])
-            output += sum_
-            heapq.heappush(sticks, sum_)
-        return output    
+            a, b = heappop(sticks), heappop(sticks)
+            cost += a + b
+            heappush(sticks, a + b)
+        return cost
