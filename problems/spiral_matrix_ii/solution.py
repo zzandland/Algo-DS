0f9d95e898d3900454362e7bab14e3a38731b8ee
Si Yong Kim <zzandland@gmail.com>
2020-07-26 21:36:02 +0000

    Sync LeetCode submission - Spiral Matrix II (python3)

--- problems/spiral_matrix_ii/solution.py
+++ problems/spiral_matrix_ii/solution.py
@@ -1,13 +1,15 @@
 class Solution:
     def generateMatrix(self, n: int) -> List[List[int]]:
         dir_, di = ((0, 1), (1, 0), (0, -1), (-1, 0)), 0
-        res = [[0]*n for _ in range(n)]
+        matrix = [[0]*n for _ in range(n)]
         y = x = 0
         for i in range(1, n*n+1):
-            res[y][x] = i
-            ny, nx = y + dir_[di][0], x + dir_[di][1]
-            if not (0 <= ny < n and 0 <= nx < n and res[ny][nx] == 0):
+            matrix[y][x] = i
+            r, c = dir_[di]
+            ny, nx = y+r, x+c
+            if not (0 <= ny < n and 0 <= nx < n) or matrix[ny][nx] != 0:
                 di = (di+1) % 4
-                ny, nx = y + dir_[di][0], x + dir_[di][1]
+                r, c = dir_[di]
+                ny, nx = y+r, x+c
             y, x = ny, nx
-        return res
+        return matrix
