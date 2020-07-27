e314a61df6d850fa276c06a2c9621680b5eee76d
Si Yong Kim <zzandland@gmail.com>
2020-07-27 00:02:33 +0000

    Sync LeetCode submission - Most Common Word (python3)

--- problems/most_common_word/solution.py
+++ problems/most_common_word/solution.py
@@ -1,14 +1,14 @@
 from collections import Counter
-import re
-import heapq
 
 class Solution:
     def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
-        cnt, ban, hp = Counter(), set(banned), []
-        for wd in re.split("[!?',;.'\s]", paragraph):
-            wdL = wd.lower()
-            if wdL not in banned and wdL != '':
-                cnt[wdL] += 1
-        for w, c in cnt.items():
-            heapq.heappush(hp, (-c, w))
-        return heapq.heappop(hp)[1]
+        # filter out non letter chars O(paragraph)
+        normalized = ''.join([c.lower() if c.isalnum() else ' ' for c in paragraph])
+        
+        banned_st = set(banned)
+        cnt = Counter()
+        for word in normalized.split():
+            if word and word not in banned_st:
+                cnt[word] += 1
+                
+        return max(cnt.items(), key=operator.itemgetter(1))[0]
