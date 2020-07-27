92bd6b47b4f21272cd6928caaa3331a34d96f9ac
Si Yong Kim <zzandland@gmail.com>
2020-07-27 02:46:03 +0000

    Sync LeetCode submission - Connecting Cities With Minimum Cost (python3)

--- problems/connecting_cities_with_minimum_cost/solution.py
+++ problems/connecting_cities_with_minimum_cost/solution.py
@@ -1,19 +1,37 @@
-from heapq import *
+from heapq import heappush, heappop, heapify
 from collections import defaultdict
 
 class Solution:
     def minimumCost(self, N: int, connections: List[List[int]]) -> int:
-        adj = defaultdict(list)
-        for u, v, w in connections:
-            adj[u].append((w, v))
-            adj[v].append((w, u))
-        seen, q, res = set(), [(0, 1)], 0
-        while q and len(seen) < N:
-            w, u = heappop(q)
-            if u not in seen:
-                seen.add(u)
-                res += w
-                for nw, v in adj[u]:
-                    if v not in seen:
-                        heappush(q, (nw, v))
-        return -1 if len(seen) < N else res
+        # kruskal's algorithm O(con log con) -> disjoint set operations
+        # are mostly O(1) with compaction and union by rank
+        parents = [n for n in range(N+1)]
+        ranks = [1]*(N+1)
+        
+        # disjoint set find O(log biggest set -> amortized 1)
+        def find(u: int) -> int:
+            while u != parents[u]:
+                parents[u] = parents[parents[u]]
+                u = parents[u]
+            return u
+        
+        # disjoint set union O(1)
+        def union(u: int, v: int) -> bool:
+            pu, pv = find(u), find(v)
+            if pu == pv: return False
+            if ranks[pv] > ranks[pu]: pu, pv = pv, pu
+            parents[pv] = pu
+            ranks[pu] += ranks[pv]
+            return True
+        
+        # sort the connections by cost O(con log con)
+        connections.sort(key=operator.itemgetter(2))
+        
+        # go through all connections while doing disjoing-set operations O(V log E)
+        cost = 0
+        for u, v, c in connections:
+            # if union is successful, add the cost
+            if union(u, v): cost += c
+        
+        # if there's only one group return cost
+        return cost if len({find(n) for n in parents[1:]}) == 1 else -1
