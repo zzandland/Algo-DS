5a7cadb2aa47d462d744fc2e9f1d4da9b47f529e
Si Yong Kim <zzandland@gmail.com>
2020-07-26 23:21:43 -0400

    Solve min cost to repair edges in both kruskal and prim for amazon OA in python

--- Python/Amazon_2020_03/min_cost_to_repair_edges.py
+++ Python/Amazon_2020_03/min_cost_to_repair_edges.py
@@ -61,7 +61,108 @@ edgesToRepair3 = [[1, 5, 110], [2, 4, 84], [3, 4, 79]]
             #  union(a, b)
     #  return output
 
-def prims()
+from typing import List
+from collections import defaultdict
+from heapq import heapify, heappush, heappop
+
+def prim(N: int, edges: List[List[int]], edgesToRepair: List[List[int]]) -> int:
+    '''
+    >>> prim(n1, edges1, edgesToRepair1)
+    20
+    >>> prim(n2, edges2, edgesToRepair2)
+    410
+    >>> prim(n3, edges3, edgesToRepair3)
+    79
+    '''
+    # prim's algorithm O((V+E) log V)
+
+    # create adj list O(edges)
+    adj = defaultdict(list)
+
+    # for edges to repair add with the given costs
+    # save visited edges to prevent redundancy in next step
+    seenEdges = set()
+    for u, v, c in edgesToRepair:
+        adj[u].append((c, v))
+        adj[v].append((c, u))
+        seenEdges.add(tuple(sorted([u, v])))
+
+    # for edges that don't need maintenance add with 0 cost
+    for u, v in edges:
+        if tuple(sorted([u, v])) not in seenEdges:
+            adj[u].append((0, v))
+            adj[v].append((0, u))
+
+    # start from vertex 1, link vertices with min cost edges until all vertices
+    # are connected O((V+1) log V)
+    seen = set([1])
+    pq = adj[1]
+    heapify(pq)
+    cost = 0
+
+    while pq:
+        c, n = heappop(pq)
+        if n in seen: continue
+        seen.add(n)
+        cost += c
+        if len(seen) == N: return cost
+        for nc, nn in adj[n]:
+            heappush(pq, (nc, nn))
+
+    # if number of visited nodes never reached N, we could never connect all nodes
+    return -1
+
+def kruskal(N: int, edges: List[List[int]], edgesToRepair: List[List[int]]) -> int:
+    '''
+    >>> kruskal(n1, edges1, edgesToRepair1)
+    20
+    >>> kruskal(n2, edges2, edgesToRepair2)
+    410
+    >>> kruskal(n3, edges3, edgesToRepair3)
+    79
+    '''
+    # kruskal's algorithm O(E log(V))
+
+    # union edges with edgesToRepair in a way that edges don't need maintenance gets 0 cost
+    # O(edges + edgesToRepair)
+    repair_set = set()
+    edge_union = []
+    for u, v, c in edgesToRepair:
+        edge_union.append([c, u, v])
+        repair_set.add(tuple(sorted([u, v])))
+
+    for u, v in edges:
+        if tuple(sorted([u, v])) not in repair_set: edge_union.append([0, u, v])
+
+    # sort the edges by cost O(edge log edge)
+    edge_union.sort()
+
+    parents = [i for i in range(N+1)]
+    ranks = [1]*(N+1)
+
+    # disjoint set find operation O(log biggest set) -> amortized O(1) due to compaction
+    def find(u: int) -> int:
+        while parents[u] != u:
+            parents[u] = parents[parents[u]]
+            u = parents[u]
+        return u
+
+    # disjoint union operation O(1) -> union by rank
+    def union(u: int, v: int) -> bool:
+        pu, pv = find(u), find(v)
+        if pu == pv: return False
+        if ranks[pv] > ranks[pu]: pu, pv = pv, pu
+        parents[pv] = pu
+        ranks[pu] += ranks[pv]
+        return True
+
+    # iterate through the edges and add costs when two disjoint sets are unioned O(edge)
+    cost = 0
+    for c, u, v in edge_union:
+        if union(u, v): cost += c
+
+    # only if there's one disjoint set at the end we could make a minimum spanning tree
+    return cost if len({find(u) for u in parents[1:]}) == 1 else -1
 
 if __name__ == '__main__':
     import doctest
