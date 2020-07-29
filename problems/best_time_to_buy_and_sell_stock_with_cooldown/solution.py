6fc3836ca97e05a6be1a7f41b0854389ec16b078
Si Yong Kim <zzandland@gmail.com>
2020-07-29 13:51:14 +0000

    Sync LeetCode submission - Best Time to Buy and Sell Stock with Cooldown (python3)

--- problems/best_time_to_buy_and_sell_stock_with_cooldown/solution.py
+++ problems/best_time_to_buy_and_sell_stock_with_cooldown/solution.py
@@ -1,23 +1,15 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        """
-        define a state machine:
-        1) we can only reach the state sold from selling the stock, thus previous
-        state can be only sell (held + price of stock)
-        2) we can reach the state held from either holding onto the stock or just
-        buying the stock, thus previous actions are rest or buy (reset - price of stock)
-        3) we can reach the state reset from either reset (no stock in hand) or just
-        sold off stock, thus previous actions are rest
-        """
-        N = len(prices)
-        # states: sold, held, reset
-        sold = float('-inf') # at start we could not have just sold
-        held = float('-inf') # at start we can't be holding onto a stock
-        reset = 0
-        res = 0
-        # O(n)
+        # state machine
+        # free state from taking break (free) or just sold a stock (sold)
+        # sold state from selling (held+price)
+        # held state from taking break (free) or buying after cooldown (free-price)
+        
+        free, sold, held = 0, float('-inf'), float('-inf')
+        
         for price in prices:
-            held = max(held, reset - price)
-            reset = max(reset, sold)
-            sold = held + price
-        return max(sold, reset)
+            held = max(held, free-price)
+            free = max(free, sold)
+            sold = held+price
+            
+        return max(free, sold)
