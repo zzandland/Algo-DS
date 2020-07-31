9cb935f26a4138da701f89ca57679cf93796cc0d
Si Yong Kim <zzandland@gmail.com>
2020-07-31 18:29:35 +0000

    Sync LeetCode submission - Search Suggestions System (python3)

--- problems/search_suggestions_system/solution.py
+++ problems/search_suggestions_system/solution.py
@@ -1,26 +1,23 @@
-from heapq import heappush, nsmallest
 from collections import defaultdict
+from heapq import heappush, nsmallest
 
 class Trie:
     def __init__(self):
+        self.products = []
         self.next = defaultdict(Trie)
-        self.words = []
 
 class Solution:
     def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
-        # create trie of all characters in products O(products * longest word product)
         trie = Trie()
-        for product in products:
+        for prod in products:
             n = trie
-            for c in product:
-                heappush(n.words, product)
+            for c in prod:
+                n.products.append(prod)
                 n = n.next[c]
-            heappush(n.words, product)
-            
+            n.products.append(prod)
         res = []
         n = trie
         for c in searchWord:
             n = n.next[c]
-            res.append(nsmallest(3, n.words))
-            
+            res.append(nsmallest(3, n.products))
         return res
