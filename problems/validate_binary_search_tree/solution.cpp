a741a9198eec7810ee05f55bead824652ad99107
Si Yong Kim <zzandland@gmail.com>
2020-08-01 19:48:28 +0000

    Sync LeetCode submission - Validate Binary Search Tree (cpp)

---
+++ problems/validate_binary_search_tree/solution.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        return helper(root, LONG_MIN, LONG_MAX);
+    }
+    
+    bool helper(TreeNode* n, long l, long r) {
+        if (!n) return true;
+        return l < n->val && n->val < r && helper(n->left, l, n->val) && helper(n->right, n->val, r);
+    }
+};
