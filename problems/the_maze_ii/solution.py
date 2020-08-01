ec7c4a5b3ae1ff8755333718f0fc395ec9853673
Si Yong Kim <zzandland@gmail.com>
2020-08-01 21:11:38 +0000

    Sync LeetCode submission - The Maze II (python3)

--- problems/the_maze_ii/solution.py
+++ problems/the_maze_ii/solution.py
@@ -2,31 +2,26 @@ from heapq import heappush, heappop
 
 class Solution:
     def shortestDistance(self, maze: List[List[int]], start: List[int], destination: List[int]) -> int:
-        if not maze: return 0 
+        if not maze: return -1
         M, N = len(maze), len(maze[0])
-        
         dir_ = ((-1, 0), (0, 1), (1, 0), (0, -1))
-        q = [(0, start[0], start[1])]
-        seen = {}
-        res = []
-        
-        def roll(y: int, x: int, dir: int) -> List[int]: 
-            r, c = dir_[dir]
+        def roll(y: int, x: int, d: int) -> List[int]:
+            r, c = dir_[d]
             ny, nx = y+r, x+c
-            d = 0
+            cnt = 0
             while 0 <= ny < M and 0 <= nx < N and maze[ny][nx] != 1:
+                cnt += 1
                 y, x = ny, nx
                 ny, nx = y+r, x+c
-                d += 1
-            return [d, y, x]
-        
+            return [cnt, y, x]
+        q = [(0, start[0], start[1])]
+        seen = {tuple(start): 0}
         while q:
             d, y, x = heappop(q)
-            if [y, x] == destination:
-                return d
-            if (y, x) not in seen or d < seen[y, x]:
-                seen[y, x] = d
-                for dir in range(4):
-                    nd, ny, nx = roll(y, x, dir)
+            if [y, x] == destination: return d
+            for i in range(4):
+                nd, ny, nx = roll(y, x, i)
+                if (ny, nx) not in seen or seen[ny, nx] > d+nd:
+                    seen[ny, nx] = d+nd
                     heappush(q, (d+nd, ny, nx))
         return -1
