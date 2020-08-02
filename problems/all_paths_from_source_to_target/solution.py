af4a1d034c12243bb5c66915c4b26129204d83f2
Si Yong Kim <zzandland@gmail.com>
2020-08-02 21:01:33 +0000

    Sync LeetCode submission - All Paths From Source to Target (python3)

--- problems/all_paths_from_source_to_target/solution.py
+++ problems/all_paths_from_source_to_target/solution.py
@@ -1,15 +1,14 @@
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
-        seen = set([0])
-        def dfs(n: int, tmp: List[int]) -> List[List[int]]:
-            if n == len(graph) - 1: return [tmp[:]]
+        def dfs(n: int, tmp: List[int], seen: set) -> List[List[int]]:
+            if n == len(graph)-1: return [tmp[:] + [n]]
+            if n in seen: return []
+            seen.add(n)
+            tmp.append(n)
             res = []
             for nn in graph[n]:
-                if nn not in seen:
-                    seen.add(nn)
-                    tmp.append(nn)
-                    res += dfs(nn, tmp)
-                    tmp.pop()
-                    seen.remove(nn)
+                res += dfs(nn, tmp, seen)
+            seen.remove(n)
+            tmp.pop()
             return res
-        return dfs(0, [0])
+        return dfs(0, [], set())
