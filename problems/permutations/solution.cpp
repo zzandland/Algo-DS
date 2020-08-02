c2b47c717df5232e31563f6dbc3350d2d7ba16fa
Si Yong Kim <zzandland@gmail.com>
2020-08-02 16:32:40 +0000

    Sync LeetCode submission - Permutations (cpp)

--- problems/permutations/solution.cpp
+++ problems/permutations/solution.cpp
@@ -1,31 +1,19 @@
 class Solution {
 public:
-  vector<vector<int>> permute(vector<int>& nums) {
-    if (nums.size() == 0) {
-      vector<vector<int>> out;
-      out.push_back(vector<int>());
-      return out;
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> q;
+        q.push_back(vector<int>());
+        for (auto num: nums) {
+            vector<vector<int>> nq;
+            for (auto perm: q) {
+                for (int i = 0; i <= perm.size(); ++i) {
+                    vector<int> cpy = perm;
+                    cpy.insert(cpy.begin()+i, num);
+                    nq.push_back(cpy);
+                }
+            }
+            q = nq;
+        }
+        return q;
     }
-    return recurse(nums);
-  }
-  
-  vector<vector<int>> recurse(vector<int>& nums) {
-    vector<vector<int>> output;
-    if (nums.size() == 1) {
-      vector<int> single = {nums[0]};
-      output.push_back(single);
-      return output;
-    }
-    int out = nums[nums.size()-1];
-    nums.pop_back();
-    vector<vector<int>> prev = recurse(nums);
-    for (int i = 0; i < prev.size(); ++i) {
-      for (int j = 0; j <= prev[i].size(); ++j) {
-        vector<int> tmp = prev[i];
-        tmp.insert(tmp.begin()+j, out);
-        output.push_back(tmp);
-      }
-    }
-    return output;
-  }
 };
