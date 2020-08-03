8af945613df5e47d94e21dd4bcbb2748e2a0785b
Si Yong Kim <zzandland@gmail.com>
2020-08-03 16:06:17 +0000

    Sync LeetCode submission - Flood Fill (python3)

--- problems/flood_fill/solution.py
+++ problems/flood_fill/solution.py
@@ -1,20 +1,15 @@
 class Solution:
-    def floodFill(self, image: List[List[int]], 
-                  sr: int, sc: int, newColor: int) -> List[List[int]]:
-        if not image:
-            return []
-        R, C, dir_= len(image), len(image[0]), ((1, 0), (-1, 0), (0, 1), (0, -1))
-        seen, st, prev = [[False]*C for _ in range(R)], [], image[sr][sc]
-        if prev != newColor:
-            st.append((sr, sc))
-            seen[sr][sc] = True
-        while st:
-            r, c = st.pop()
-            image[r][c] = newColor
-            for y, x in dir_:
-                nr, nc = r+y, c+x
-                if (0 <= nr < R and 0 <= nc < C and
-                    not seen[nr][nc] and image[nr][nc] == prev):
-                    seen[nr][nc] = True
-                    st.append((nr, nc))
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
+        if not image: return None
+        m, n = len(image), len(image[0])
+        directions = ((1, 0), (-1, 0), (0, 1), (0, -1))
+        def dfs(y: int, x: int, c: int) -> None:
+            for ny, nx in ((y+r, x+c) for r, c in directions):
+                if 0 <= ny < m and 0 <= nx < n and image[ny][nx] == c:
+                    image[ny][nx] = newColor
+                    dfs(ny, nx, c)
+        if image[sr][sc] != newColor:
+            o = image[sr][sc]
+            image[sr][sc] = newColor
+            dfs(sr, sc, o)
         return image
