727a40f5f019bb4f96cd84f8fedeb11aa46d1b29
Si Yong Kim <zzandland@gmail.com>
2020-08-08 04:07:41 +0000

    Sync LeetCode submission - Next Greater Element II (python3)

--- problems/next_greater_element_ii/solution.py
+++ problems/next_greater_element_ii/solution.py
@@ -1,9 +1,11 @@
 class Solution:
     def nextGreaterElements(self, nums: List[int]) -> List[int]:
         N = len(nums)
-        st, nxt = [], [-1]*N*2
-        for i, n in enumerate(nums+nums):
-            while st and st[-1][0] < n:
-                nxt[st.pop()[1]] = n
-            st.append((n, i))        
-        return [nxt[i] for i, n in enumerate(nums)]
+        nums += nums
+        st = []
+        nxt = [-1]*N*2
+        for i, n in enumerate(nums):
+            while st and st[-1][1] < n:
+                nxt[st.pop()[0]] = n
+            st.append((i, n))
+        return nxt[:N]
