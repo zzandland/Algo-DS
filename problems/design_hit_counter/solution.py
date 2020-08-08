40a3c47aa23083183cdfa2762c5130b5c6c5c7a0
Si Yong Kim <zzandland@gmail.com>
2020-08-08 04:24:54 +0000

    Sync LeetCode submission - Design Hit Counter (python3)

--- problems/design_hit_counter/solution.py
+++ problems/design_hit_counter/solution.py
@@ -1,31 +1,29 @@
 class HitCounter:
+
     def __init__(self):
         """
         Initialize your data structure here.
         """
-        self.q = {}
+        self.hits = Counter({0:0})
+        self.total = 0
+        self.time = [0]
 
     def hit(self, timestamp: int) -> None:
         """
         Record a hit.
         @param timestamp - The current timestamp (in seconds granularity).
         """
-        t = timestamp // 60
-        if t not in self.q: self.q[t] = [0]*61
-        self.q[t][timestamp%60] += 1
+        self.total += 1
+        if timestamp not in self.hits: self.time.append(timestamp)
+        self.hits[timestamp] = self.total
 
     def getHits(self, timestamp: int) -> int:
         """
         Return the number of hits in the past 5 minutes.
         @param timestamp - The current timestamp (in seconds granularity).
         """
-        t, output = timestamp // 60, 0
-        j = t - 5
-        for i in range(j, t+1):
-            if i in self.q and i >= 0:
-                if i == j:  output += sum(self.q[i][timestamp%60+1:])
-                else: output += sum(self.q[i])    
-        return output            
+        t = self.time[bisect.bisect(self.time, max(0, timestamp - 300))-1]
+        return self.total - self.hits[t]
 
 # Your HitCounter object will be instantiated and called as such:
 # obj = HitCounter()
