a4771e95b3aaf22a6cccce36dffee36db5b99427
Si Yong Kim <zzandland@gmail.com>
2020-08-08 03:59:22 +0000

    Sync LeetCode submission - Reorder Data in Log Files (python3)

--- problems/reorder_data_in_log_files/solution.py
+++ problems/reorder_data_in_log_files/solution.py
@@ -1,8 +1,8 @@
 class Solution:
     def reorderLogFiles(self, logs: List[str]) -> List[str]:
-        def key(log: str):
-            identifier, rest = log.split(' ', 1)
-            if rest[0].isdigit(): return (1,)
-            else: return (0, rest, identifier)
-            
-        return sorted(logs, key=key)
+        def comp(s: str):
+            id, rest = s.split(' ', 1)
+            if rest[0].isdigit(): return (1, )
+            return (0, rest, id)
+        logs.sort(key=comp)
+        return logs
