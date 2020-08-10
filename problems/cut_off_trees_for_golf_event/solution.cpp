166e71fb1ffe6ecf33f0a2a8ed4812d5ae7fc437
Si Yong Kim <zzandland@gmail.com>
2020-08-10 19:13:21 +0000

    Sync LeetCode submission - Cut Off Trees for Golf Event (cpp)

--- problems/cut_off_trees_for_golf_event/solution.cpp
+++ problems/cut_off_trees_for_golf_event/solution.cpp
@@ -1,55 +1,58 @@
 class Solution {
 public:
+    const vector<int> dir = {-1, 0, 1, 0, -1};
+    
     int cutOffTree(vector<vector<int>>& forest) {
-        if (forest.empty() || forest[0].empty()) return 0;
-        int m = forest.size(), n = forest[0].size();
-        vector<vector<int>> trees;
-        // get all the tree positions and sort based on height
-        // trees[i][0] is height. The default comparison of vector compare first element before other elements.
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (forest[i][j] > 1) trees.push_back({forest[i][j], i, j});
+        if (!forest.size()) return -1;
+        
+        vector<pair<int, int>> trees;
+        for (int y = 0; y < forest.size(); ++y) {
+            for (int x = 0; x < forest[y].size(); ++x) {
+                if (forest[y][x] > 1) trees.push_back({y, x});
             }
         }
-        sort(trees.begin(), trees.end());
-        int ans = 0;
-        // accumulate all the paths
-        for (int i = 0, cur_row = 0, cur_col = 0; i < trees.size(); i++) {
-            int step = next_step(forest, cur_row, cur_col, trees[i][1], trees[i][2]);
-            // if next tree cannot be reached, step = -1;
-            if (step == -1) return -1;
-            ans += step;
-            cur_row = trees[i][1];
-            cur_col = trees[i][2];
+        
+        sort(trees.begin(), trees.end(), [&](auto& a, auto& b) {
+            return forest[a.first][a.second] < forest[b.first][b.second];
+        });
+        
+        int res = 0, y = 0, x = 0;
+        for (auto [ty, tx]: trees) {
+            int d = getDist(y, x, ty, tx, forest);
+            if (d < 0) return -1;
+            res += d;
+            y = ty;
+            x = tx;
         }
-        return ans;
+        
+        return res;
     }
-private:
-    // BFS to find shortest path to next tree; if cannot reach next tree, return -1
-    int next_step(vector<vector<int>>& forest, int sr, int sc, int er, int ec) {
-        if (sr == er && sc == ec) return 0;
+    
+    int getDist(int sy, int sx, int ty, int tx, vector<vector<int>>& forest) {
         int m = forest.size(), n = forest[0].size();
-        queue<pair<int, int>> myq;
-        myq.push({sr, sc}); 
-        vector<vector<int>> visited(m, vector<int>(n, 0));
-        visited[sr][sc] = 1;
-        int step = 0;
-        vector<int> dir = {-1, 0, 1, 0, -1};
-        while (!myq.empty()) {
-            step++;
-            int sz = myq.size();
-            for (int i = 0; i < sz; i++) {
-                int row = myq.front().first, col = myq.front().second;
-                myq.pop();
-                for (int i = 0; i < 4; i++) {
-                    int r = row + dir[i], c = col + dir[i+1];
-                    if (r < 0 || r >= m || c < 0 || c >= n || visited[r][c] == 1 || forest[r][c] == 0) continue;
-                    if (r == er && c == ec) return step;
-                    visited[r][c] = 1;
-                    myq.push({r, c});
+        vector<vector<int>> seen(m, vector<int>(n, 0));
+        queue<pair<int, int>> q;
+        seen[sy][sx] = 1;
+        q.emplace(sy, sx);
+        int res = 0;
+        
+        while (!q.empty()) {
+            int len = q.size();
+            for (int j = 0; j < len; ++j) {
+                auto [y, x] = q.front();
+                q.pop();
+                if (y == ty && x == tx) return res;
+                for (int i = 1; i < dir.size(); ++i) {
+                    int ny = y + dir[i-1], nx = x + dir[i];
+                    if (0 <= ny && ny < m && 0 <= nx && nx < n && forest[ny][nx] > 0 && !seen[ny][nx]) {
+                        seen[ny][nx] = 1;
+                        q.emplace(ny, nx);
+                    }
                 }
             }
+            ++res;
         }
+        
         return -1;
     }
 };
