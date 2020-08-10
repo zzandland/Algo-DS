40ba7c00faf1cbc1667a6fd26951b48f913b2768
Si Yong Kim <zzandland@gmail.com>
2020-08-10 00:50:11 +0000

    Sync LeetCode submission - Most Common Word (python3)

--- problems/most_common_word/solution.py
+++ problems/most_common_word/solution.py
@@ -1,14 +1,7 @@
-from collections import Counter
-
 class Solution:
     def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
-        # filter out non letter chars O(paragraph)
-        normalized = ''.join([c.lower() if c.isalnum() else ' ' for c in paragraph])
-        
-        banned_st = set(banned)
-        cnt = Counter()
-        for word in normalized.split():
-            if word and word not in banned_st:
-                cnt[word] += 1
-                
-        return max(cnt.items(), key=operator.itemgetter(1))[0]
+        ban = set(banned)
+        freq = Counter()
+        for w in re.sub('\W', ' ', paragraph.lower()).split(' '):
+            if w and w not in ban: freq[w] += 1
+        return max(freq.items(), key=operator.itemgetter(1))[0]
