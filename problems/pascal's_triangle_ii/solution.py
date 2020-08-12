c2efc109c351ca9125faf0a6fa7d8ab04077a834
Si Yong Kim <zzandland@gmail.com>
2020-08-12 15:23:16 +0000

    Sync LeetCode submission - Pascal's Triangle II (python3)

--- problems/pascal's_triangle_ii/solution.py
+++ problems/pascal's_triangle_ii/solution.py
@@ -1,6 +1,11 @@
 class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
-        def fn(arr: List[int], k: int) -> List[int]:
-            if k == 0: return arr
-            return fn([0] + [arr[i] + arr[i+1] for i in range(0, len(arr)-1)] + [0], k-1)
-        return fn([0, 1, 0], rowIndex)[1:-1]
+        row = [0, 1, 0]
+        while rowIndex > 0:
+            nrow = [0]
+            for a, b in zip(row, row[1:]):
+                nrow.append(a+b)
+            nrow.append(0)
+            row = nrow
+            rowIndex -= 1
+        return row[1:-1]
