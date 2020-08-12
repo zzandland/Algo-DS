1e98c92123def1e6688f636eebeba71921fda827
Si Yong Kim <zzandland@gmail.com>
2020-08-12 15:49:57 +0000

    Sync LeetCode submission - Partition Labels (python3)

--- problems/partition_labels/solution.py
+++ problems/partition_labels/solution.py
@@ -1,19 +1,13 @@
-from collections import defaultdict
-
 class Solution:
     def partitionLabels(self, S: str) -> List[int]:
-        # get the lastest index of each character O(S)
         idx = {}
-        for i, c in enumerate(S):
-            idx[c] = i
+        for i, c in enumerate(S): idx[c] = i
             
-        # greedily increase window until left pointer
-        # catches up with right pointer O(S)
-        res = []
         l = r = 0
+        res = []
         for i, c in enumerate(S):
             r = max(r, idx[c])
-            if r == len(S)-1: return res + [r-l+1]
             if i == r:
                 res.append(r-l+1)
                 l = i+1
+        return res
