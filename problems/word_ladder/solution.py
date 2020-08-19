51152b57fcc1866138f39e1f8d65449837e90683
Si Yong Kim <zzandland@gmail.com>
2020-08-19 22:04:31 +0000

    Sync LeetCode submission - Word Ladder (python3)

--- problems/word_ladder/solution.py
+++ problems/word_ladder/solution.py
@@ -1,16 +1,20 @@
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        q1, q2, nq, words, cnt = set([beginWord]), set([endWord]), set(), set(wordList), 0
-        if endWord not in wordList: return cnt
-        while q1 and q2:
-            words -= q1
-            if len(q1) > len(q2): q1, q2 = q2, q1
-            cnt += 1
-            for w in q1:
-                for i in range(len(beginWord)):
-                    for c in 'qwertyuiopasdfghjklzxcvbnm':
-                        comb = w[:i]+c+w[i+1:]
-                        if comb in q2: return cnt+1
-                        if comb in words: nq.add(comb)
-            q1, nq = nq, set()
+        ws = set(wordList)
+        ln = len(endWord)
+        if endWord not in ws: return 0
+        q = set([beginWord])
+        res = 0
+        while q:
+            ws -= q
+            res += 1
+            nq = set()
+            for w in q:
+                if w == endWord: return res
+                for i in range(ln):
+                    for c in string.ascii_lowercase:
+                        nw = w[:i] + c + w[i+1:]
+                        if nw in ws:
+                            nq.add(nw)
+            q = nq
         return 0
