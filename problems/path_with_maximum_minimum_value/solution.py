eda0af5da931f7f9e71384ab0481814bd87d00a6
Si Yong Kim <zzandland@gmail.com>
2020-08-20 02:05:03 +0000

    Sync LeetCode submission - Path With Maximum Minimum Value (python3)

--- problems/path_with_maximum_minimum_value/solution.py
+++ problems/path_with_maximum_minimum_value/solution.py
@@ -1,20 +1,18 @@
+from heapq import heappush, heappop
+
 class Solution:
     def maximumMinimumPath(self, A: List[List[int]]) -> int:
-        R, C = len(A), len(A[0])
-        uf, seen, dir_ = [i for i in range(R * C)], [[False for _ in range(C)] for _ in range(R)], [(1, 0), (-1, 0), (0, 1), (0, -1)]
-        def find(x: int) -> int:
-            if x != uf[x]:
-                uf[x] = find(uf[x])
-            return uf[x]
-        def union(x: int, y: int) -> None:
-            ux, uy = find(x), find(y)
-            if ux != uy:
-                uf[ux] = uy
-        for y, x in sorted([(r, c) for r in range(R) for c in range(C)], key=lambda x: A[x[0]][x[1]], reverse=True):
-            seen[y][x] = True
-            for ver, hor in dir_:
-                ny, nx = y+ver, x+hor
-                if 0 <= ny < R and 0 <= nx < C and seen[ny][nx]:
-                    union(ny*C+nx, y*C+x)
-                if find(0) == find(R*C-1):
-                    return A[y][x]
+        if not A: return 0
+        M, N = len(A), len(A[0])
+        dir_ = (-1, 0, 1, 0, -1)
+        q = [(-A[0][0], 0, 0)]
+        seen = set([0])
+        
+        while q:
+            v, y, x = heappop(q)
+            if y == M-1 and x == N-1: return -v
+            for ny, nx in ((y+r, x+c) for r, c in zip(dir_, dir_[1:])):
+                if 0 <= ny < M and 0 <= nx < N and (ny*N+nx) not in seen:
+                    seen.add(ny*N+nx)
+                    heappush(q, (max(v, -A[ny][nx]), ny, nx))
+        return -1
