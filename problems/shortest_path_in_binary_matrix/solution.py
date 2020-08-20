dec10c40eae6354814ff6a8e828f6300b387fa3e
Si Yong Kim <zzandland@gmail.com>
2020-08-20 00:11:31 +0000

    Sync LeetCode submission - Shortest Path in Binary Matrix (python3)

--- problems/shortest_path_in_binary_matrix/solution.py
+++ problems/shortest_path_in_binary_matrix/solution.py
@@ -1,22 +1,18 @@
+from heapq import heappush, heappop
+
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-        if not grid:
-            return 0
-        N = len(grid)
-        if N == 1 and grid[0][0] == 0:
-            return 1
-        if grid[0][0] == 1:
-            return -1
-        dir_ = ((-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1))
-        q, grid[0][0], res = [(0, 0)], 1, 1
+        if not grid or grid[0][0] == 1: return -1
+        M, N = len(grid), len(grid[0])
+        q = [(1, 0, 0)]
+        dir_ = ((-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1))
+        seen = {(0, 0): 1}
         while q:
-            nq = []
-            res += 1
-            for ny, nx in [[y+r, x+c] for y, x in q for r, c in dir_]:
-                if 0 <= ny < N and 0 <= nx < N and grid[ny][nx] == 0:
-                    if ny == N-1 and nx == N-1:
-                        return res
-                    grid[ny][nx] = 1
-                    nq.append((ny, nx))
-            q = nq
+            d, y, x = heappop(q)
+            if y == M-1 and x == N-1: return d
+            for ny, nx in (((y+r, x+c) for r, c in dir_)):
+                if 0 <= ny < M and 0 <= nx < N and grid[ny][nx] == 0:
+                    if (ny, nx) not in seen or seen[ny, nx] > d+1:
+                        seen[ny, nx] = d+1
+                        heappush(q, (d+1, ny, nx))
         return -1
