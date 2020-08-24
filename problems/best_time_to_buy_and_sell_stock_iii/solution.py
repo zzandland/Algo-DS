5008ad86120deb5f8a73d3ae2f3930252328011c
Si Yong Kim <zzandland@gmail.com>
2020-08-24 21:05:52 +0000

    Sync LeetCode submission - Best Time to Buy and Sell Stock III (python3)

--- problems/best_time_to_buy_and_sell_stock_iii/solution.py
+++ problems/best_time_to_buy_and_sell_stock_iii/solution.py
@@ -1,12 +1,12 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        if not prices: return 0
-        N, K = len(prices), 2
-        # dp array of n cols and k rows
-        dp = [[0]*(N+1) for _ in range(K+1)]
-        for k in range(1, K+1):
-            mx = -prices[0]
-            for i in range(2, N+1):
-                mx = max(mx, dp[k-1][i-2] - prices[i-2])
-                dp[k][i] = max(dp[k-1][i], dp[k][i-1], prices[i-1] + mx)
-        return dp[-1][-1]
+        t1_cost = t2_cost = float('inf')
+        t1_profit = t2_profit = 0
+        
+        for p in prices:
+            t1_cost = min(t1_cost, p)
+            t1_profit = max(t1_profit, p - t1_cost)
+            t2_cost = min(t2_cost, p - t1_profit)
+            t2_profit = max(t2_profit, p - t2_cost)
+            
+        return t2_profit
