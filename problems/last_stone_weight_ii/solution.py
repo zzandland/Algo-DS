91e0febcf3d4045752db94a22f6d227ffa8a61ef
Si Yong Kim <zzandland@gmail.com>
2020-08-24 19:02:34 +0000

    Sync LeetCode submission - Last Stone Weight II (python3)

--- problems/last_stone_weight_ii/solution.py
+++ problems/last_stone_weight_ii/solution.py
@@ -1,10 +1,6 @@
 class Solution:
     def lastStoneWeightII(self, stones: List[int]) -> int:
-        q = set([0])
+        dp = {0}
         for s in stones:
-            nq = set()
-            for n in q:
-                nq.add(n+s)
-                nq.add(n-s)
-            q = nq
-        return min([n for n in nq if n >= 0])
+            dp = {i+s for i in dp} | {i-s for i in dp}
+        return min([abs(i) for i in dp])
