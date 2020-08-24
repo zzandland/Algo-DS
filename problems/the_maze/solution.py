d5ded422aadedcc8ef14dcfea978f42a8c52cce8
Si Yong Kim <zzandland@gmail.com>
2020-08-24 16:54:11 +0000

    Sync LeetCode submission - The Maze (python3)

--- problems/the_maze/solution.py
+++ problems/the_maze/solution.py
@@ -2,28 +2,23 @@ from collections import deque
 
 class Solution:
     def hasPath(self, maze: List[List[int]], start: List[int], destination: List[int]) -> bool:
-        if not maze: return 0 
+        if not maze: return False
         M, N = len(maze), len(maze[0])
-        dir_ = [(-1, 0), (0, 1), (1, 0), (0, -1)]
-        
-        def roll(y: int, x: int, dir: int) -> List[int]:
-            d = 0
-            r, c = dir_[dir]
-            ny, nx = y+r, x+c
+        dir_ = {0: (-1, 0), 1: (0, 1), 2: (1, 0), 3: (0, -1)}
+        def move(y: int, x: int, d: int) -> List[int]:
+            ny, nx = y + dir_[d][0], x + dir_[d][1]
             while 0 <= ny < M and 0 <= nx < N and maze[ny][nx] != 1:
                 y, x = ny, nx
-                ny, nx = y+r, x+c
+                ny, nx = y + dir_[d][0], x + dir_[d][1]
             return [y, x]
-            
         q = deque([start])
-        seen = set()
-        
+        seen = set([tuple(start)])
         while q:
             y, x = q.popleft()
             if [y, x] == destination: return True
-            if (y, x) not in seen:
-                seen.add((y, x))
-                for dir in range(4):
-                    q.append(roll(y, x, dir))
-                    
+            for i in range(4):
+                ny, nx = move(y, x, i)
+                if (y != ny or x != nx) and (ny, nx) not in seen:
+                    q.append((ny, nx))
+                    seen.add((ny, nx))
         return False
