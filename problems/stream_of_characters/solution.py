1488e2d56b95925f5c5ecbfedcce4a59aa3b857d
Si Yong Kim <zzandland@gmail.com>
2020-08-24 03:00:33 +0000

    Sync LeetCode submission - Stream of Characters (python3)

--- problems/stream_of_characters/solution.py
+++ problems/stream_of_characters/solution.py
@@ -1,32 +1,24 @@
 from collections import deque
 
-class Trie:
-    def __init__(self):
-        self.next = {}
-        self.end = False
-
 class StreamChecker:
+
     def __init__(self, words: List[str]):
-        self.root = Trie()
-        self.buffer = ''
-        self.maxlen = 0
-        for w in map(lambda w: w[::-1], words):
+        self.root = {}
+        self.buf = deque()
+        for w in words:
             n = self.root
-            self.maxlen = max(self.maxlen, len(w))
-            for c in w:
-                n.next.setdefault(c, Trie())
-                n = n.next[c]
-            n.end = True
+            for c in w[::-1]:
+                n.setdefault(c, {})
+                n = n[c]
+            n['*'] = {}
 
     def query(self, letter: str) -> bool:
-        self.buffer = (letter + self.buffer)[:self.maxlen]
-        n = self.root
-        for c in self.buffer:
-            if c in n.next:
-                n = n.next[c]
-                if n.end: return True
-            else: break
-        return False
+        self.buf.appendleft(letter)
+        run = self.root
+        for c in self.buf:
+            if c not in run: return False
+            run = run[c]
+            if '*' in run: return True
 
 # Your StreamChecker object will be instantiated and called as such:
 # obj = StreamChecker(words)
