172462f0238e23c857eaddef1bd55c6c6f830d2d
Si Yong Kim <zzandland@gmail.com>
2020-08-25 15:59:28 +0000

    Sync LeetCode submission - Maximum Number of Non-Overlapping Substrings (python3)

--- problems/maximum_number_of_non-overlapping_substrings/solution.py
+++ problems/maximum_number_of_non-overlapping_substrings/solution.py
@@ -1,50 +1,23 @@
 from collections import defaultdict
-import bisect
 
 class Solution:
     def maxNumOfSubstrings(self, s: str) -> List[str]:
-        # get indexes of all characters O(n)
-        dic = defaultdict(list)
+        idx = defaultdict(list)
         for i, c in enumerate(s):
-            dic[c].append(i)
+            idx[c].append(i)
             
-        if all(map(lambda x: len(x) == 1, dic.values())): return [c for c in s]
-            
-        S = len(s)
         substrs = []
+        for i in range(len(s)):
+            l, r = i, idx[s[i]][-1]
+            while l < len(s):
+                if idx[s[l]][0] < i: break
+                r = max(r, idx[s[l]][-1])
+                if l == r: substrs.append((i, r))
+                l += 1
         
-        # greedily expand interval starting from each c O(n^2)
-        for i in range(S):
-            r = i
-            for j in range(i, S):
-                c = s[j]
-                if i > dic[c][0]: break
-                # greedily expand r
-                r = max(r, dic[c][-1])
-                if j == r: substrs.append((i, r))
+        res = [substrs[0]]
+        for a, b in substrs[1:]:
+            if res[-1][1] < a: res.append((a, b))
+            elif res[-1][1] > b: res[-1] = (a, b)
                 
-        # 0/1 knapsack backtrack O(!intervals)
-        tmp = []
-        cnt = 0
-        rescnt = float('inf')
-        res = []
-        def dfs(i: int) -> None:
-            nonlocal tmp, cnt, res, rescnt
-            if i == len(substrs):
-                if len(res) <= len(tmp) and cnt < rescnt:
-                    rescnt = cnt
-                    res = tmp[:]
-                return
-            l, r = substrs[i]
-            idx = bisect.bisect_left(tmp, (l,))
-            if (not tmp or
-                (tmp and idx == len(tmp) and tmp[idx-1][1] < l) or
-                (idx < len(tmp)-1 and tmp[idx][1] < l and r < tmp[idx+1][0])):
-                tmp.insert(idx, (l, r))
-                cnt += (r-l+1)
-                dfs(i+1)
-                tmp.pop(idx)
-                cnt -= (r-l+1)
-            dfs(i+1)
-        dfs(0)
-        return map(lambda x: s[x[0]:x[1]+1], res)
+        return [s[a:b+1] for a, b in res]
