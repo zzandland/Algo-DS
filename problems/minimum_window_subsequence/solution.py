057ea43cdc7801a745bdd69f57ced9649e2e2ab5
Si Yong Kim <zzandland@gmail.com>
2020-08-26 19:16:10 +0000

    Sync LeetCode submission - Minimum Window Subsequence (python3)

--- problems/minimum_window_subsequence/solution.py
+++ problems/minimum_window_subsequence/solution.py
@@ -1,21 +1,17 @@
-from collections import defaultdict
-import bisect
-
 class Solution:
     def minWindow(self, S: str, T: str) -> str:
-        idx = defaultdict(list)
-        for i, c in enumerate(S):
-            idx[c].append(i)
-        res = ''
-        for i in idx[T[0]]:
-            j = i
-            for c in T[1:]:
-                if c not in idx: return ''
-                k = bisect.bisect_left(idx[c], j+1)
-                if k == len(idx[c]): break
-                j = idx[c][k]
-            else:
-                if res == '': res = S[i:j+1]
-                else: res = min(res, S[i:j+1], key=len)
-                if res == T: return res
-        return res
+        start = -1
+        ln = float('inf')
+        s = t = 0
+        while s < len(S):
+            if S[s] == T[t]:
+                t += 1
+                if t == len(T):
+                    end = s+1
+                    t -= 1
+                    while t >= 0:
+                        if S[s] == T[t]: t -= 1
+                        s -= 1
+                    if end - s < ln: start, ln = s+1, end - s
+            s += 1
+        return '' if start == -1 else S[start:start+ln-1]
