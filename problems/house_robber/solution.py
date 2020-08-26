786b63ba205af39935ae8a87a2e64bc34190e8cc
Si Yong Kim <zzandland@gmail.com>
2020-08-26 15:26:17 +0000

    Sync LeetCode submission - House Robber (python3)

--- problems/house_robber/solution.py
+++ problems/house_robber/solution.py
@@ -1,12 +1,6 @@
 class Solution:
-  def rob(self, nums: List[int]) -> int:
-    if len(nums) == 0:
-      return 0
-    if len(nums) == 1:
-      return nums[0]
-    if len(nums) == 2:
-      return max(nums[0], nums[1])
-    dp = [nums[0], max(nums[0], nums[1])]
-    for i in range(2, len(nums)):
-      dp.append(max(dp[i - 2] + nums[i], dp[i - 1]))
-    return dp[-1]
+    def rob(self, nums: List[int]) -> int:
+        dp = [0, 0, 0]
+        for v in nums:
+            dp.append(max(dp[-3], dp[-2]) + v)
+        return max(dp[-2], dp[-1])
