9d046fa254dece899f7782c72bfadc33a3452b89
Si Yong Kim <zzandland@gmail.com>
2020-08-26 19:55:50 +0000

    Sync LeetCode submission - Decode Ways (python3)

--- problems/decode_ways/solution.py
+++ problems/decode_ways/solution.py
@@ -1,15 +1,12 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        S = len(s)
-        if not s or s[0] == '0': return 0
-        if S < 2: return S
-        c1, c2 = 1, 1
-        for i in range(1, S):
-            val = int(s[i-1]+s[i])
-            if s[i] == '0':
-                if val > 26: return 0
-                else: c3, c2 = c1, 0
-            elif val < 27: c3 = c1+c2
-            else: c3 = c2    
-            c1, c2 = c2, c3    
-        return c3    
+        if not s: return 0
+        dp = {}
+        def dfs(i: int) -> int:
+            if i == len(s): return 1
+            if i not in dp:
+                res = 0
+                if i + 1 < len(s) and int(s[i:i+2]) <= 26: res += dfs(i+2)
+                dp[i] = res + dfs(i+1) if s[i] != '0' else 0
+            return dp[i]
+        return dfs(0)
