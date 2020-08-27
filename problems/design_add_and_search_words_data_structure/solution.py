bca969fc78159a821aa1b23cf31d21e9f55fa423
Si Yong Kim <zzandland@gmail.com>
2020-08-27 23:40:15 +0000

    Sync LeetCode submission - Design Add and Search Words Data Structure (python3)

--- problems/design_add_and_search_words_data_structure/solution.py
+++ problems/design_add_and_search_words_data_structure/solution.py
@@ -1,33 +1,41 @@
+from collections import defaultdict
+
+class Trie:
+    
+    def __init__(self):
+        self.next = {}
+        self.end = False
+
 class WordDictionary:
 
     def __init__(self):
         """
         Initialize your data structure here.
         """
-        self.trie = {}
+        self.root = Trie()
 
     def addWord(self, word: str) -> None:
         """
         Adds a word into the data structure.
         """
-        n = self.trie
+        n = self.root
         for c in word:
-            n.setdefault(c, {})
-            n = n[c]
-        n['*'] = word;
+            n.next.setdefault(c, Trie())
+            n = n.next[c]
+        n.end = True
 
     def search(self, word: str) -> bool:
         """
         Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter.
         """
-        N = len(word)
-        def dfs(i: int, n: dict) -> bool:
-            if isinstance(n, str): return False
-            if i == N: return '*' in n
-            if word[i] == '.': return any([dfs(i+1, nc) for nc in n.values()])
-            if word[i] not in n: return False
-            return dfs(i+1, n[word[i]])
-        return dfs(0, self.trie)
+        def dfs(i: int, n: Trie) -> bool:
+            if i == len(word): return n.end
+            if word[i] == '.':
+                for c in n.next:
+                    if dfs(i+1, n.next[c]): return True
+            elif word[i] in n.next: return dfs(i+1, n.next[word[i]])
+            return False
+        return dfs(0, self.root)
 
 # Your WordDictionary object will be instantiated and called as such:
 # obj = WordDictionary()
