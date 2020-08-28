9ff6df4de29e7fb0a6c8478ab2bbbfc4b1691852
Si Yong Kim <zzandland@gmail.com>
2020-08-28 15:58:51 +0000

    Sync LeetCode submission - Merge Intervals (python3)

--- problems/merge_intervals/solution.py
+++ problems/merge_intervals/solution.py
@@ -3,9 +3,7 @@ class Solution:
         if not intervals: return []
         intervals.sort()
         res = [intervals[0]]
-        for i in range(1, len(intervals)):
-            s1, e1 = res[-1]
-            s2, e2 = intervals[i]
-            if e1 >= s2: res[-1] = [min(s1, s2), max(e1, e2)]
-            else: res.append([s2, e2])
+        for u, v in intervals[1:]:
+            if res[-1][1] < u: res.append([u, v])
+            else: res[-1][1] = max(res[-1][1], v)
         return res
