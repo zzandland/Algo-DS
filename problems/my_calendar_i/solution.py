53abf497f959111a294fa0cabf3ded0a9a2257cf
Si Yong Kim <zzandland@gmail.com>
2020-08-28 16:46:01 +0000

    Sync LeetCode submission - My Calendar I (python3)

--- problems/my_calendar_i/solution.py
+++ problems/my_calendar_i/solution.py
@@ -1,31 +1,18 @@
-class Node:
-    def __init__(self, start: int, end: int):
-        self.start = start
-        self.end = end
-        self.left = self.right = None
-        
-    def insert(self, node) -> bool:
-        if node.start >= self.end:
-            if not self.right:
-                self.right = node
-                return True
-            return self.right.insert(node)
-        if node.end <= self.start:
-            if not self.left:
-                self.left = node
-                return True
-            return self.left.insert(node)
-        return False
-
 class MyCalendar:
+
     def __init__(self):
-        self.root = None
+        self.changes = []
 
     def book(self, start: int, end: int) -> bool:
-        if not self.root:
-            self.root = Node(start, end)
-            return True
-        return self.root.insert(Node(start, end))
+        tmp = self.changes[:]
+        bisect.insort_left(tmp, (start, 1))
+        bisect.insort_left(tmp, (end, -1))
+        curr = 0
+        for time, change in tmp:
+            curr += change
+            if curr > 1: return False
+        self.changes = tmp
+        return True
 
 # Your MyCalendar object will be instantiated and called as such:
 # obj = MyCalendar()
