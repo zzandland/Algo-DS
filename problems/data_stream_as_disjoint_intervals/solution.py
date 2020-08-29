00c1853af5d20cbee8de12bdbb1619fcba933c0d
Si Yong Kim <zzandland@gmail.com>
2020-08-29 14:15:16 +0000

    Sync LeetCode submission - Data Stream as Disjoint Intervals (python3)

--- problems/data_stream_as_disjoint_intervals/solution.py
+++ problems/data_stream_as_disjoint_intervals/solution.py
@@ -1,34 +1,36 @@
-import bisect
-
 class SummaryRanges:
 
     def __init__(self):
         """
         Initialize your data structure here.
         """
-        self.intervals = []
+        self.parents = {}
+        self.intervals = {}
+        
+    def find(self, val: int) -> int:
+        if val not in self.parents: return -1
+        if val != self.parents[val]:
+            self.parents[val] = self.find(self.parents[val])
+        return self.parents[val]
+    
+    def union(self, x: int, y: int) -> None:
+        fx, fy = self.find(x), self.find(y)
+        if fx == -1 or fy == -1: return
+        self.parents[fy] = fx
+        xa, xb = self.intervals[fx]
+        ya, yb = self.intervals[fy]
+        del self.intervals[fy]
+        self.intervals[fx] = [min(xa, ya), max(xb, yb)]
 
     def addNum(self, val: int) -> None:
-        N = len(self.intervals)
-        idx = bisect.bisect_left([i for _, i in self.intervals], val)
-        if not self.intervals:
-            self.intervals.append([val, val])
-        elif (0 < idx < N and self.intervals[idx-1][1] < val and
-            val < self.intervals[idx][0]):
-            self.intervals.insert(idx, [val, val])
-        elif idx == 0 and val < self.intervals[idx][0]:
-            self.intervals.insert(0, [val, val])
-        elif idx == N and self.intervals[idx-1][1] < val:
-            self.intervals.append([val, val])
-        while 0 < idx < len(self.intervals) and self.intervals[idx-1][1] + 1 == self.intervals[idx][0]:
-            self.intervals[idx-1][1] = self.intervals[idx][1]
-            self.intervals.pop(idx)
-        while 0 <= idx < len(self.intervals)-1 and self.intervals[idx][1] + 1 == self.intervals[idx+1][0]:
-            self.intervals[idx][1] = self.intervals[idx+1][1]
-            self.intervals.pop(idx+1)
+        if val in self.parents: return
+        self.parents[val] = val
+        self.intervals[val] = [val, val]
+        self.union(val, val-1)
+        self.union(val, val+1)
 
     def getIntervals(self) -> List[List[int]]:
-        return self.intervals
+        return sorted(self.intervals.values())
 
 # Your SummaryRanges object will be instantiated and called as such:
 # obj = SummaryRanges()
