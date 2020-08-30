94bdc9f78330c6f782cffddf86cb37f5e5b3fbcf
Si Yong Kim <zzandland@gmail.com>
2020-08-30 03:32:49 +0000

    Sync LeetCode submission - Length of Longest Fibonacci Subsequence (python3)

--- problems/length_of_longest_fibonacci_subsequence/solution.py
+++ problems/length_of_longest_fibonacci_subsequence/solution.py
@@ -1,14 +1,14 @@
+from collections import defaultdict
+
 class Solution:
     def lenLongestFibSubseq(self, A: List[int]) -> int:
         N = len(A)
-        index = {n: i for i, n in enumerate(A)}
-        dp = [[2]*N for _ in range(N)]
-        
+        idx = {n: i for i, n in enumerate(A)}
+        dp = defaultdict(lambda: 2)
         res = 0
-        for k, z in enumerate(A):
-            for j in range(k):
-                i = index.get(z - A[j], -1)
-                if i != -1 and i < j:
-                    dp[j][k] = dp[i][j] + 1
-                    res = max(res, dp[j][k])
-        return res if res >= 3 else 0
+        for i in range(N):
+            for j in range(i):
+                if A[i] - A[j] < A[j] and A[i] - A[j] in idx:
+                    dp[A[j], A[i]] = dp[A[i]-A[j], A[j]] + 1
+                    res = max(res, dp[A[j], A[i]])
+        return res if res > 2 else 0
