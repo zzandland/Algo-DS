430e2092f36493f84d9bbe9e8afc053e1d57cab4
Si Yong Kim <zzandland@gmail.com>
2020-08-31 20:26:07 +0000

    Sync LeetCode submission - Unique Paths II (python3)

--- problems/unique_paths_ii/solution.py
+++ problems/unique_paths_ii/solution.py
@@ -1,11 +1,14 @@
 class Solution:
-    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
-        if not obstacleGrid: return 0
-        M, N = len(obstacleGrid), len(obstacleGrid[0])
+    def uniquePathsWithObstacles(self, A: List[List[int]]) -> int:
+        if not A: return 0
+        M, N = len(A), len(A[0])
+        
         dp = [[0]*(N+1) for _ in range(M+1)]
-        dp[1][0] = 1
-        for y in range(1, M+1):
-            for x in range(1, N+1):
-                if obstacleGrid[y-1][x-1] == 0:
-                    dp[y][x] = dp[y-1][x] + dp[y][x-1]
+        dp[0][1] = 1
+        
+        for y in range(M):
+            for x in range(N):
+                if A[y][x] == 0:
+                    dp[y+1][x+1] = dp[y][x+1] + dp[y+1][x]
+                    
         return dp[-1][-1]
