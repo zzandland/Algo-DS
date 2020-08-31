4af1e3fe95372baa84d678a8271d46bb3ab50890
Si Yong Kim <zzandland@gmail.com>
2020-08-31 20:19:38 +0000

    Sync LeetCode submission - Find And Replace in String (python3)

--- problems/find_and_replace_in_string/solution.py
+++ problems/find_and_replace_in_string/solution.py
@@ -1,13 +1,12 @@
 class Solution:
     def findReplaceString(self, S: str, indexes: List[int], sources: List[str], targets: List[str]) -> str:
-        res, i, dic = [], 0, {i: (s, t) for i, s, t in zip(indexes, sources, targets)}
-        while i < len(S):
-            if i in dic:
-                s, t = dic[i]
-                if S[i:i+len(s)] == s: res.append(t)
-                else: res.append(S[i:i+len(s)])
-                i += len(s)
-            else:    
-                res.append(S[i])
-                i += 1
-        return ''.join(res)
+        res = []
+        idx = {i: (s, t) for i, s, t in zip(indexes, sources, targets)}
+        p = 0
+        for i, (s, t) in sorted(idx.items()):
+            if p < i: res.append(S[p:i])
+            tmp = S[i:i+len(s)]
+            if tmp == s: res.append(t)
+            else: res.append(tmp)
+            p = i + len(s)
+        return ''.join(res + [S[p:]])
