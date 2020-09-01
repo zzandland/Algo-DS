c37af7ae364fde2ca8f3e4c28b2a681285947322
Si Yong Kim <zzandland@gmail.com>
2020-09-01 13:36:41 +0000

    Sync LeetCode submission - Largest Time for Given Digits (python3)

--- problems/largest_time_for_given_digits/solution.py
+++ problems/largest_time_for_given_digits/solution.py
@@ -1,21 +1,13 @@
 class Solution:
     def largestTimeFromDigits(self, A: List[int]) -> str:
-        A.sort()
-        p = [str(A[0])]
-        for i in range(1, len(A)):
-            c, np = str(A[i]), []
-            for perm in p:
-                for j in range(len(perm)+1):
-                    if j > 0 and perm[j-1] == c:
-                        break
-                    np.append(perm[:j]+c+perm[j:])
-            p = np
-        def key(s: str) -> int:
-            s = list(map(int, list(s)))
-            if s[0] > 2 or s[2] > 5 or s[0] == 2 and s[1] > 3:
-                return 0
-            return s[0]*600 + s[1]*60 + s[2]*10 + s[3]
-        mx = max(p, key=key)
-        if not key(mx) and mx != '0000':
-            return ''
-        return "%s:%s" % (mx[:2], mx[2:])
+        q = ['']
+        for d in A:
+            nq = []
+            for n in q:
+                for i in range(len(n)+1):
+                    nq.append(n[:i] + str(d) + n[i:])
+            q = nq
+        filtered = list(filter(lambda x: int(x[:2]) < 24 and int(x[2:]) < 60, q))
+        if not filtered: return ''
+        mx = max(filtered, key=lambda x: int(x[:2]) * 60 + int(x[2:]))
+        return mx[:2] + ':' + mx[2:]
