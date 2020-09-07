3d2b8978c346d932578f91880630a566ae4f160b
Si Yong Kim <zzandland@gmail.com>
2020-09-07 23:14:21 +0000

    Sync LeetCode submission - Contains Duplicate III (python3)

--- problems/contains_duplicate_iii/solution.py
+++ problems/contains_duplicate_iii/solution.py
@@ -1,46 +1,54 @@
 class Node:
+    
     def __init__(self, val: int):
         self.val = val
         self.left = self.right = None
 
 class BST:
+    
     def __init__(self):
         self.root = None
         
-    def insert(self, val: int, root: Node) -> Node:
-        if not root: return Node(val)
-        if root.val > val: root.left = self.insert(val, root.left)
-        else: root.right = self.insert(val, root.right)
-        return root
+    def add(self, val: int) -> None:
+        def dfs(n: Node, val: int) -> Node:
+            if not n: return Node(val)
+            if val < n.val: n.left = dfs(n.left, val)
+            else: n.right = dfs(n.right, val)
+            return n
+        self.root = dfs(self.root, val)
+        
+    def remove(self, val: int) -> None:
+        def dfs(n: Node, val: int) -> Node:
+            if not n: return None
+            if n.val == val:
+                if not n.left: return n.right
+                if not n.right: return n.left
+                right = n.right
+                while right.left:
+                    right = right.left
+                n.val = right.val
+                n.right = dfs(n.right, right.val)
+            elif val < n.val: n.left = dfs(n.left, val)
+            else: n.right = dfs(n.right, val)
+            return n
+        self.root = dfs(self.root, val)
     
-    def query(self, val: int, k: int) -> bool:
+    def find(self, val: int, t: int) -> bool:
         n = self.root
         while n:
-            if abs(val - n.val) <= k: return True
-            if n.val > val: n = n.left
+            if abs(n.val - val) <= t: return True
+            if val < n.val: n = n.left
             else: n = n.right
         return False
-    
-    def delete(self, val: int, root: Node) -> Node:
-        if not root: return None
-        if root.val == val:
-            if root.left and root.right:
-                tmp = root.right
-                while tmp.left: tmp = tmp.left
-                root.val = tmp.val
-                root.right = self.delete(tmp.val, root.right)
-            elif not root.left: return root.right
-            else: return root.left
-        else:
-            if root.val > val: root.left = self.delete(val, root.left)
-            else: root.right = self.delete(val, root.right)
-        return root
 
 class Solution:
-    def containsNearbyAlmostDuplicate(self, nums: List[int], k: int, t: int) -> bool:
+    def containsNearbyAlmostDuplicate(self, nums: [int], k: int, t: int) -> bool:
+        if k < 0: return False
         bst = BST()
+        
         for i, n in enumerate(nums):
-            if i-k > 0: bst.root = bst.delete(nums[i-k-1], bst.root)
-            if bst.query(n, t): return True
-            bst.root = bst.insert(n, bst.root)
+            if i > k: bst.remove(nums[i-k-1])
+            if bst.find(n, t):
+                return True
+            bst.add(n)
         return False
