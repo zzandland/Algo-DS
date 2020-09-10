e010921dca617dbf81f86ca2feea096f3206e663
Si Yong Kim <zzandland@gmail.com>
2020-09-10 15:48:55 +0000

    Sync LeetCode submission - Maximum Profit in Job Scheduling (python3)

--- problems/maximum_profit_in_job_scheduling/solution.py
+++ problems/maximum_profit_in_job_scheduling/solution.py
@@ -1,12 +1,10 @@
-import functools
+import bisect
 
 class Solution:
     def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
-        N = len(startTime)
-        tmp = sorted(zip(startTime, endTime, profit), key=lambda x: x[1])
-        dp = [(0, 0)]
-        
-        for s, e, p in tmp:
-            idx = bisect.bisect_left(dp, (s+1,))-1
-            if dp[idx][1] + p > dp[-1][1]: dp.append((e, dp[idx][1] + p))
-        return dp[-1][1]
+        arr = sorted(zip(startTime, endTime, profit), key=operator.itemgetter(1))
+        q = [(0, 0)]
+        for st, et, p in arr:
+            idx = bisect.bisect_left(q, (st+1,))-1
+            if q[idx][1] + p > q[-1][1]: q.append((et, q[idx][1] + p))
+        return q[-1][1]
