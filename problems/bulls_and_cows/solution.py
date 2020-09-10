4ebaca736e759f35378eed413f008e9da1de295d
Si Yong Kim <zzandland@gmail.com>
2020-09-10 13:54:10 +0000

    Sync LeetCode submission - Bulls and Cows (python3)

--- problems/bulls_and_cows/solution.py
+++ problems/bulls_and_cows/solution.py
@@ -2,17 +2,20 @@ from collections import Counter
 
 class Solution:
     def getHint(self, secret: str, guess: str) -> str:
-        bulls = cows = 0
-        sl, gl = [], []
-        for a, b in zip(secret, guess):
-            if a == b: bulls += 1
+        freq = Counter(secret)
+        a = b = 0
+        missed = []
+        for s, g in zip(secret, guess):
+            if s == g:
+                if freq[g] > 0:
+                    a += 1
+                    freq[g] -= 1
             else:
-                sl.append(a)
-                gl.append(b)
-        nums = Counter(sl)
-        for c in gl:
-            if c in nums:
-                nums[c] -= 1
-                cows += 1
-                if nums[c] == 0: del nums[c]
-        return '%dA%dB' % (bulls, cows)
+                missed.append(g)
+                    
+        for g in missed:
+            if freq[g] > 0:
+                b += 1
+                freq[g] -= 1
+                
+        return '%dA%dB' % (a, b)
