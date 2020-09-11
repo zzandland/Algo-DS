2d7d648b304cbb2c070d94607fcc95ab9819a4d2
Si Yong Kim <zzandland@gmail.com>
2020-09-11 05:20:48 +0000

    Sync LeetCode submission - Jump Game III (python3)

--- problems/jump_game_iii/solution.py
+++ problems/jump_game_iii/solution.py
@@ -1,8 +1,18 @@
+from collections import deque
+
 class Solution:
     def canReach(self, arr: List[int], start: int) -> bool:
-        def fn(i: int) -> bool:
-            if not (0 <= i < len(arr)) or arr[i] < 0: return False
-            if arr[i] == 0: return True
-            arr[i] = -arr[i]
-            return fn(i + arr[i]) or fn(i - arr[i])
-        return fn(start)
+        q = deque([start])
+        seen = {start}
+        
+        while q:
+            idx = q.popleft()
+            if arr[idx] == 0: return True
+            l, r = idx - arr[idx], idx + arr[idx]
+            if l >= 0 and l not in seen:
+                seen.add(l)
+                q.append(l)
+            if r < len(arr) and r not in seen:
+                seen.add(r)
+                q.append(r)
+        return False
