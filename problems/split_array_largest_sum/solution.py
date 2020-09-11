a6d902aa747ada92513b5fe75c546ddb0afe85d3
Si Yong Kim <zzandland@gmail.com>
2020-09-11 19:07:32 +0000

    Sync LeetCode submission - Split Array Largest Sum (python3)

--- problems/split_array_largest_sum/solution.py
+++ problems/split_array_largest_sum/solution.py
@@ -1,15 +1,17 @@
 class Solution:
-    def splitArray(self, nums: List[int], m: int) -> int:
-        def split(n: int) -> int:
-            cnt, run = 1, 0
-            for num in nums:
-                if num + run <= n: run += num
-                else:
-                    run, cnt = num, cnt+1
-            return cnt    
+    def splitArray(self, nums: List[int], M: int) -> int:
+        def check(t: int) -> int:
+            cnt = 1
+            sm = 0
+            for n in nums:
+                if sm + n > t:
+                    cnt += 1
+                    sm = n
+                else: sm += n
+            return cnt
         l, r = max(nums), sum(nums)
         while l < r:
-            mid = l + (r-l)//2
-            if split(mid) <= m: r = mid
-            else: l = mid+1
-        return l        
+            m = l + (r-l)//2
+            if check(m) <= M: r = m
+            else: l = m+1
+        return l
