10277b7f9c867a7ed5bf7d04974eedbec978fce1
Si Yong Kim <zzandland@gmail.com>
2020-09-12 14:28:31 +0000

    Sync LeetCode submission - Combination Sum III (python3)

--- problems/combination_sum_iii/solution.py
+++ problems/combination_sum_iii/solution.py
@@ -1,14 +1,15 @@
 class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
-        run, res = [], []
-        def bt(i: int, s: int) -> None:
-            if s > n or len(run) > k: return
-            if s == n and len(run) == k:
-                res.append(run[:])
+        res = []
+        def dfs(i: int, sm: int, tmp: [int]) -> None:
+            nonlocal res
+            if sm > n: return
+            if sm == n:
+                if len(tmp) == k: res.append(tmp[:])
                 return
-            for j in range(i, 10):
-                run.append(j)
-                bt(j+1, s + j)
-                run.pop()
-        bt(1, 0)
+            for j in range(i+1, 10):
+                tmp.append(j)
+                dfs(j, sm+j, tmp)
+                tmp.pop()
+        dfs(0, 0, [])
         return res
