4a253188b56d3d8d75d5ac1c5d75365a72f68d2e
Si Yong Kim <zzandland@gmail.com>
2020-09-13 03:01:55 +0000

    Sync LeetCode submission - Find Median from Data Stream (python3)

--- problems/find_median_from_data_stream/solution.py
+++ problems/find_median_from_data_stream/solution.py
@@ -1,23 +1,23 @@
+from heapq import heappush, heappop
+
 class MedianFinder:
 
     def __init__(self):
         """
         initialize your data structure here.
         """
-        self.l = []
-        self.r = []
-        self.ln = 0
-        
+        self.hp = [], []
+
     def addNum(self, num: int) -> None:
-        heappush(self.l, -num)
-        heappush(self.r, -heappop(self.l))
-        if self.ln & 1 == 0: heappush(self.l, -heappop(self.r))
-        self.ln += 1
+        l, r = self.hp
+        heappush(l, -num)
+        heappush(r, -heappop(l))
+        if len(l) < len(r): heappush(l, -heappop(r))
 
     def findMedian(self) -> float:
-        if not self.ln: return 0
-        if self.ln & 1 == 1: return -self.l[0]
-        return (-self.l[0] + self.r[0]) / 2
+        l, r = self.hp
+        if len(l) == len(r): return (-l[0] + r[0]) / 2
+        return -l[0]
 
 # Your MedianFinder object will be instantiated and called as such:
 # obj = MedianFinder()
