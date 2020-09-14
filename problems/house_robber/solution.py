a566a8d6340e2b1be17dadc084deb2925ae174ec
Si Yong Kim <zzandland@gmail.com>
2020-09-14 12:11:35 +0000

    Sync LeetCode submission - House Robber (python3)

--- problems/house_robber/solution.py
+++ problems/house_robber/solution.py
@@ -1,6 +1,16 @@
+from functools import lru_cache
+
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        dp = [0, 0, 0]
-        for v in nums:
-            dp.append(max(dp[-3], dp[-2]) + v)
-        return max(dp[-2], dp[-1])
+        # stole: just stole. Must rest
+        # rest: can rest of steal again
+        rest = 0
+        stole = float('-inf')
+        res = 0
+        
+        for n in nums:
+            ns = max(stole, n + rest)
+            nr = max(stole, res)
+            stole, rest = ns, nr
+            res = max(res, stole, rest)
+        return res
