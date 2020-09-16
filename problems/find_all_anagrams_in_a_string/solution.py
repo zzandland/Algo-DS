b7c98d0a8abc61718e7aa99244c5b7a0f2e141f5
Si Yong Kim <zzandland@gmail.com>
2020-09-16 20:28:47 +0000

    Sync LeetCode submission - Find All Anagrams in a String (python3)

--- problems/find_all_anagrams_in_a_string/solution.py
+++ problems/find_all_anagrams_in_a_string/solution.py
@@ -2,17 +2,15 @@ from collections import Counter
 
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        S, P, res = len(s), len(p), []
-        if S < P or not p: return res
-        sd, pd = Counter(s[:P]), Counter(p)
-        def compare() -> bool:
-            for c, f in sd.items():
-                if c not in pd or pd[c] != f: return False
-            return True    
-        if compare(): res.append(0)
-        for i in range(P, S):
-            sd[s[i]] += 1
-            sd[s[i-P]] -= 1
-            if sd[s[i-P]] == 0: del sd[s[i-P]]
-            if compare(): res.append(i-P+1)
+        t = Counter(p)
+        
+        N = len(p)
+        roll = Counter()
+        res = []
+        for i, c in enumerate(s):
+            if i >= N:
+                roll[s[i-N]] -= 1
+                if not roll[s[i-N]]: del roll[s[i-N]]
+            roll[s[i]] += 1
+            if t == roll: res.append(i-N+1)
         return res
