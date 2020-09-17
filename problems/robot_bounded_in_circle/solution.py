5bb40c9b76504035c12f39a746bd682fe8aa4153
Si Yong Kim <zzandland@gmail.com>
2020-09-17 16:28:11 +0000

    Sync LeetCode submission - Robot Bounded In Circle (python3)

--- problems/robot_bounded_in_circle/solution.py
+++ problems/robot_bounded_in_circle/solution.py
@@ -1,26 +1,15 @@
 class Solution:
     def isRobotBounded(self, instructions: str) -> bool:
-        # 0 N 1 E 2 S 3 W
-        # if dir pointing is clock/counterclockwise or N/S circle
-        if 'G' not in instructions: return True
-        new_coord, new_d = self.run(instructions, [0, 0], 0)
-        if new_d == 0:
-            y, x = new_coord
-            if x == 0 and y == 0: return True
-            return False
-        return new_d in (1, 2, 3)
-        
-    def run(self, instructions: str, coord: List[int], d: int) -> List[int]:
-        dir_ = ((-1, 0), (0, 1), (1, 0), (0, -1))
-        for c in instructions:
-            if c == 'L':
-                d -= 1
-                if d == -1: d = 3
-            elif c == 'R':
-                d += 1
-                if d == 4: d = 0
-            else:
-                r, c = dir_[d]
-                coord[0] += r
-                coord[1] += c
-        return [coord, d]
+        d = y = x = 0
+        dir_ = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+        seen = {(0, 0)}
+        for i in range(4):
+            for c in instructions:
+                if c == 'L':
+                    d = (d+3) % 4
+                elif c == 'R':
+                    d = (d+1) % 4
+                else:
+                    y += dir_[d][0]
+                    x += dir_[d][1]
+        return (x == 0 and y == 0) or d != 0
