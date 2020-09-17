e8938a904c4edb84bda274d4e01af664a5345ee9
Si Yong Kim <zzandland@gmail.com>
2020-09-17 04:14:34 +0000

    Sync LeetCode submission - Edit Distance (python3)

--- problems/edit_distance/solution.py
+++ problems/edit_distance/solution.py
@@ -1,15 +1,11 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        M, N = len(word1), len(word2)
-        dp = [[0]*(M+1) for _ in range(N+1)]
-        for i in range(1, M+1):
-            dp[0][i] = dp[0][i-1] + 1
-        for j in range(1, N+1):    
-            dp[j][0] = dp[j-1][0] + 1
-        for r in range(1, N+1):
-            for c in range(1, M+1):
-                if word2[r-1] == word1[c-1]:
-                    dp[r][c] = dp[r-1][c-1]
-                else:
-                    dp[r][c] = 1 + min(dp[r-1][c], dp[r][c-1], dp[r-1][c-1])
-        return dp[N][M]
+        A, B = len(word1), len(word2)
+        dp = [[0]*(B+1) for _ in range(A+1)] 
+        for a in range(A+1): dp[a][0] = a
+        for b in range(B+1): dp[0][b] = b
+        for a in range(A):
+            for b in range(B):
+                if word1[a] == word2[b]: dp[a+1][b+1] = dp[a][b]
+                else: dp[a+1][b+1] = 1 + min(dp[a+1][b], dp[a][b], dp[a][b+1])
+        return dp[A][B]
