cc2e2094246d72c675ad8ea0e69a48ebba99a725
Si Yong Kim <zzandland@gmail.com>
2020-09-17 19:21:04 +0000

    Sync LeetCode submission - Word Ladder (python3)

--- problems/word_ladder/solution.py
+++ problems/word_ladder/solution.py
@@ -1,20 +1,22 @@
+from heapq import heappush, heappop
+
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        ws = set(wordList)
-        ln = len(endWord)
+        ws = {*wordList}
         if endWord not in ws: return 0
-        q = set([beginWord])
+        q1 = {beginWord}
+        q2 = {endWord}
         res = 0
-        while q:
-            ws -= q
+        while q1:
+            if len(q1) > len(q2): q1, q2 = q2, q1
             res += 1
+            ws -= q1
             nq = set()
-            for w in q:
-                if w == endWord: return res
-                for i in range(ln):
+            for word in q1:
+                if word in q2: return res
+                for i in range(len(word)):
                     for c in string.ascii_lowercase:
-                        nw = w[:i] + c + w[i+1:]
-                        if nw in ws:
-                            nq.add(nw)
-            q = nq
+                        tmp = word[:i] + c + word[i+1:]
+                        if tmp in ws: nq.add(tmp)
+            q1 = nq
         return 0
