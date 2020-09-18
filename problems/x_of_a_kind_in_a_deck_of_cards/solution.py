86b6c93569f6b3bf9b98b3150ddd889576a97b4d
Si Yong Kim <zzandland@gmail.com>
2020-09-18 17:23:48 +0000

    Sync LeetCode submission - X of a Kind in a Deck of Cards (python3)

--- problems/x_of_a_kind_in_a_deck_of_cards/solution.py
+++ problems/x_of_a_kind_in_a_deck_of_cards/solution.py
@@ -2,24 +2,23 @@ from collections import Counter
 
 class Solution:
     def hasGroupsSizeX(self, deck: List[int]) -> bool:
-        
-        def getPrimes(n: int) -> [int]: 
-            sieve = [True] * (n + 1)
-            res = []
-            for i in range(2, n+1):
-                if sieve[i]:
-                    j = 1
-                    res.append(i)
-                    while i*j < n+1:
-                        sieve[i*j] = False
-                        j += 1
-            return res
-        
+        if len(deck) < 2: return False
         c = Counter(deck)
-        vals = c.values()
-        primes = getPrimes(min(vals))
+        if len(c) == 1: return True
+        mn = min(c.values())
+        
+        primes = []
+        sieve = [True]*(mn+1)
+        for i in range(2, mn+1):
+            if sieve[i]:
+                primes.append(i)
+                j = 1
+                while i*j <= mn:
+                    sieve[i*j] = False
+                    j += 1
+        
         for p in primes:
-            for v in c.values():
-                if v % p != 0: break
+            for n in c:
+                if c[n] % p != 0: break
             else: return True
         return False
