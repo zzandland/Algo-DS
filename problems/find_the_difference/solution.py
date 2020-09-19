e36a515ffd5aee322cca17a7de04a348c4ccebd0
Si Yong Kim <zzandland@gmail.com>
2020-09-19 03:36:54 +0000

    Sync LeetCode submission - Find the Difference (python3)

--- problems/find_the_difference/solution.py
+++ problems/find_the_difference/solution.py
@@ -2,8 +2,6 @@ from collections import Counter
 
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        freq = Counter(s)
-        for c in t:
-            freq[c] -= 1
-            if freq[c] == 0: del freq[c]
-        return list(freq.keys())[0]
+        sc, tc = Counter(s), Counter(t)
+        for c in tc:
+            if c not in sc or sc[c] != tc[c]: return c
