6960d650b4f5f658faf4a04f8ce0dbd9bdc1dc23
Si Yong Kim <zzandland@gmail.com>
2020-09-19 15:34:37 +0000

    Sync LeetCode submission - Queue Reconstruction by Height (python3)

--- problems/queue_reconstruction_by_height/solution.py
+++ problems/queue_reconstruction_by_height/solution.py
@@ -2,13 +2,17 @@ from collections import defaultdict
 
 class Solution:
     def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
-        N, dic = len(people), defaultdict(list)
-        for h, l in people:
-            dic[h].append(l)
-        st, res = set(range(N)), [None]*N
+        N = len(people)
+        dic = defaultdict(list)
+        res = [None]*N
+        rem = {*range(N)}
+        for h, k in people:
+            dic[h].append(k)
+        
         for h in sorted(dic):
-            tmp = sorted(st)
+            tmp = sorted(rem)
             for i in dic[h]:
-                res[tmp[i]] = (h, i)
-                st.remove(tmp[i])
-        return res        
+                res[tmp[i]] = [h, i]
+                rem.remove(tmp[i])
+                
+        return res
