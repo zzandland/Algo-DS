4e2aac59df5041bf9e26e7fe6b6bce5b9a900730
Si Yong Kim <zzandland@gmail.com>
2020-09-19 19:39:12 +0000

    Sync LeetCode submission - Exclusive Time of Functions (python3)

--- problems/exclusive_time_of_functions/solution.py
+++ problems/exclusive_time_of_functions/solution.py
@@ -1,13 +1,16 @@
 class Solution:
     def exclusiveTime(self, n: int, logs: List[str]) -> List[int]:
-        output, stack, sub = [0 for _ in range(n)], [], [0 for _ in range(n)]
-        for id_, met, t in [log.split(':') for log in logs]:
-            stack.append((int(id_), met, int(t)))
-            if met == 'end':
-                id1, met1, t1 = stack.pop()
-                _, met2, t2 = stack.pop()
-                diff = t1 - t2 + 1
-                if stack: sub[stack[-1][0]] += diff
-                output[id1] += diff - sub[id1]
-                sub[id1] = 0
-        return output        
+        st = []
+        res = [0]*n
+        for x in logs:
+            fn, com, ts = x.split(':')
+            fn, ts = int(fn), int(ts)
+            if st: res[st[-1]] += ts - p
+            p = ts
+            if com == 'start':
+                st.append(fn)
+            else:
+                res[st[-1]] += 1
+                p += 1
+                st.pop()
+        return res
