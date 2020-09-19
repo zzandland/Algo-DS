a20213e8d22d571fa555b3f940c05f202d281891
Si Yong Kim <zzandland@gmail.com>
2020-09-19 01:12:58 +0000

    Sync LeetCode submission - Remove Invalid Parentheses (python3)

--- problems/remove_invalid_parentheses/solution.py
+++ problems/remove_invalid_parentheses/solution.py
@@ -1,31 +1,31 @@
 class Solution:
     def removeInvalidParentheses(self, s: str) -> List[str]:
-        res = set()
+        res = {''}
         ln = 0
-        def dfs(i: int, cnt: int, tmp: List[str]) -> None:
+        def dfs(i: int, c: int, tmp: [str]) -> None:
             nonlocal res, ln
-            if cnt < 0: return
+            if c < 0: return
             if i == len(s):
-                if cnt != 0: return
-                if len(tmp) > ln:
-                    res = set()
-                    ln = len(tmp)
-                if len(tmp) == ln:
-                    res.add(''.join(tmp))
+                if c == 0:
+                    if len(tmp) > ln:
+                        res.clear()
+                        ln = len(tmp)
+                    if len(tmp) == ln:
+                        res.add(''.join(tmp))
                 return
             if s[i] == '(':
                 tmp.append('(')
-                dfs(i+1, cnt+1, tmp)
+                dfs(i+1, c+1, tmp)
                 tmp.pop()
-                dfs(i+1, cnt, tmp)
+                dfs(i+1, c, tmp)
             elif s[i] == ')':
                 tmp.append(')')
-                dfs(i+1, cnt-1, tmp)
+                dfs(i+1, c-1, tmp)
                 tmp.pop()
-                dfs(i+1, cnt, tmp)
+                dfs(i+1, c, tmp)
             else:
                 tmp.append(s[i])
-                dfs(i+1, cnt, tmp)
+                dfs(i+1, c, tmp)
                 tmp.pop()
         dfs(0, 0, [])
         return res
