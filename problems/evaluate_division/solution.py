bb38d514a4dbcde27efa91c029d203772250cdb8
Si Yong Kim <zzandland@gmail.com>
2020-09-19 15:02:12 +0000

    Sync LeetCode submission - Evaluate Division (python3)

--- problems/evaluate_division/solution.py
+++ problems/evaluate_division/solution.py
@@ -2,32 +2,30 @@ from collections import defaultdict
 
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
-        adj = defaultdict(list)
-        def div(a: int, b: int) -> float:
-            if b == 0: return 0
-            return a / b
-        for (u, v), val in zip(equations, values):
-            adj[u].append((v, val))
-            adj[v].append((u, div(1, val)))
-        res = []
-        def dfs(n: str, t: str) -> float:
-            if n == t: return 1
-            if n in seen: return float('inf')
-            seen.add(n)
-            for nn, v in adj[n]:
-                tmp = dfs(nn, t)
-                if tmp != float('inf'): return v * tmp
+        adj = defaultdict(dict)
+        for (a, b), v in zip(equations, values):
+            adj[a][b] = v
+            if v != 0: adj[b][a] = 1 / v
+                
+        def dfs(n: str, t: str, seen: set) -> float:
+            if t in adj[n]: return adj[n][t]
+            for nxt in adj[n]:
+                if nxt not in seen:
+                    seen.add(nxt)
+                    tmp = dfs(nxt, t, seen)
+                    if tmp != float('inf'): return adj[n][nxt] * tmp
             return float('inf')
-        for u, v in queries:
-            if u not in adj:
-                res.append(-1.0)
+            
+        res = []
+        for a, b in queries:
+            if not adj[b]:
+                res.append(-1)
             else:
-                seen = set()
-                val = dfs(u, v)
-                if val == float('inf'):
-                    res.append(-1)
+                tmp = dfs(a, b, {a})
+                if tmp != float('inf'):
+                    res.append(tmp)
+                    adj[a][b] = tmp
+                    adj[b][a] = 1 / tmp
                 else:
-                    res. append(val)
-                    adj[u].append((v, val))
-                    adj[v].append((u, div(1, val)))
+                    res.append(-1)
         return res
