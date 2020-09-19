ccf236662828c20be73d83a23bd299ccafd560cc
Si Yong Kim <zzandland@gmail.com>
2020-09-19 18:15:28 +0000

    Sync LeetCode submission - Task Scheduler (python3)

--- problems/task_scheduler/solution.py
+++ problems/task_scheduler/solution.py
@@ -1,13 +1,7 @@
-from collections import Counter
-
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
         c = Counter(tasks)
-        mx_freq, mx_arr = 0, []
-        for task, freq in c.items():
-            if mx_freq < freq:
-                mx_arr.clear()
-                mx_freq = freq
-            if mx_freq == freq:
-                mx_arr.append(task)
-        return max(len(tasks), mx_freq * (n+1) - (n+1-len(mx_arr)))
+        b = Counter(c.values())
+        mx = max(b)
+        
+        return max(len(tasks), (n+1) * (mx-1) + b[mx])
