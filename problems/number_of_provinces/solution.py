9e65db62b0022470661a11feb10784d9f5c46795
Si Yong Kim <zzandland@gmail.com>
2020-09-19 17:11:34 +0000

    Sync LeetCode submission - Number of Provinces (python3)

--- problems/number_of_provinces/solution.py
+++ problems/number_of_provinces/solution.py
@@ -1,24 +1,23 @@
-from collections import deque
-
 class Solution:
     def findCircleNum(self, M: List[List[int]]) -> int:
-        # dfs all connected friends O(N)
-        seen = set()
-        def dfs(n: int) -> None:
-            if n in seen: return
-            seen.add(n)
-            for i in range(len(M)):
-                if M[n][i]: dfs(i)
-        res = 0
-        for i in range(len(M)):
-            if i not in seen:
-                # bfs all connected friends O(N)
-                res += 1
-                q = deque([i])
-                while q:
-                    n = q.popleft()
-                    if n in seen: continue
-                    seen.add(n)
-                    for j in range(len(M)):
-                        if M[n][j]: q.append(j)
-        return res
+        if not M: return 0
+        N = len(M)
+        
+        parent = [*range(N)]
+        size = [1]*N
+        
+        def find(u: int) -> int:
+            if parent[u] != u: parent[u] = find(parent[u])
+            return parent[u]
+            
+        def union(u: int, v: int) -> None:
+            uf, vf = find(u), find(v)
+            if size[uf] > size[vf]: uf, vf = vf, uf
+            parent[uf] = vf
+            size[vf] += size[uf]
+            
+        for y in range(N):
+            for x in range(y+1, N):
+                if M[y][x] == 1: union(y, x)                    
+                    
+        return len([1 for i in range(N) if find(i) == i])
