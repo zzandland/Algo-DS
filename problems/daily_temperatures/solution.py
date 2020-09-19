227628fb63ec608dafc49ec28053a916626cb4b3
Si Yong Kim <zzandland@gmail.com>
2020-09-19 20:39:02 +0000

    Sync LeetCode submission - Daily Temperatures (python3)

--- problems/daily_temperatures/solution.py
+++ problems/daily_temperatures/solution.py
@@ -1,11 +1,11 @@
 class Solution:
     def dailyTemperatures(self, T: List[int]) -> List[int]:
         st = []
-        nxt = [0]*len(T)
-        # iterate the temps and get next greater val O(n)
-        for i, temp in enumerate(T):
-            while st and st[-1][0] < temp:
-                _, prev = st.pop()
-                nxt[prev] = i - prev
-            st.append((temp, i))
-        return nxt
+        res = [0]*len(T)
+        
+        for i, t in enumerate(T):
+            while st and st[-1][0] < t:
+                d = st.pop()[1]
+                res[d] = i-d
+            st.append((t, i))
+        return res
