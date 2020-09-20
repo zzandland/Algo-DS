5e0f9c0defc00ef50bcd180b66eb236c20f34ff8
Si Yong Kim <zzandland@gmail.com>
2020-09-20 18:45:40 +0000

    Sync LeetCode submission - Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit (python3)

--- problems/longest_continuous_subarray_with_absolute_diff_less_than_or_equal_to_limit/solution.py
+++ problems/longest_continuous_subarray_with_absolute_diff_less_than_or_equal_to_limit/solution.py
@@ -2,15 +2,18 @@ from collections import deque
 
 class Solution:
     def longestSubarray(self, nums: List[int], limit: int) -> int:
-        mx, mn = deque(), deque()
-        l = res = 0
-        for r, num in enumerate(nums):
-            while mx and mx[-1][0] < num: mx.pop()
-            while mn and mn[-1][0] > num: mn.pop()
-            mx.append((num, r))
-            mn.append((num, r))
-            if mx[0][0] - mn[0][0] > limit: l += 1
-            while mx[0][1] < l: mx.popleft()
-            while mn[0][1] < l: mn.popleft()
-            res = max(res, r-l+1)
+        mx = deque()
+        mn = deque()
+        l = 0
+        res = 0
+        for r, rn in enumerate(nums):
+            while mx and mx[-1][0] < rn: mx.pop()
+            while mn and mn[-1][0] > rn: mn.pop()
+            mx.append((rn, r))
+            mn.append((rn, r))
+            while abs(mx[0][0] - mn[0][0]) > limit:
+                l += 1
+                while mx[0][1] < l: mx.popleft()
+                while mn[0][1] < l: mn.popleft()
+            res = max(res, r - l + 1)
         return res
