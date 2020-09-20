70b3eb4d5950283ff864344e558c6286c51df4ee
Si Yong Kim <zzandland@gmail.com>
2020-09-20 01:33:08 +0000

    Sync LeetCode submission - Maximum Sum of Two Non-Overlapping Subarrays (python3)

--- problems/maximum_sum_of_two_non-overlapping_subarrays/solution.py
+++ problems/maximum_sum_of_two_non-overlapping_subarrays/solution.py
@@ -1,11 +1,28 @@
 class Solution:
     def maxSumTwoNoOverlap(self, A: List[int], L: int, M: int) -> int:
+        # 0 6 5 2 2 1 9 2 3 L 3 R 2
+        
+        # 0 0 6 11 13 15 16 25 27 30
+        # 0 6 5 / 2 2 1 9 2 3
+        # 0 6 5 2 / 2 1 9 2 3
+        # 0 6 5 2 2 1 9 / 2 3
         N = len(A)
-        for i in range(1, N):
-            A[i] += A[i-1]
-        res, lmax, mmax = A[L+M-1], A[L-1], A[M-1]
-        for j in range(L+M, N):
-            lmax = max(lmax, A[j-M]-A[j-L-M])
-            mmax = max(mmax, A[j-L]-A[j-L-M])
-            res = max(res, lmax + A[j]-A[j-M], mmax + A[j]-A[j-L])
-        return res
+        
+        # get prefix sum
+        pf = [0]
+        for n in A:
+            pf.append(pf[-1] + n)
+            
+        def helper(L: int, M: int) -> int:
+            # get max sm for each interval L and M
+            left = [0]
+            for i in range(L, N-M+1):
+                left.append(max(left[-1], pf[i] - pf[i-L]))
+            left = left[1:]
+            right = [0]
+            for i in range(N-M, L-1, -1):
+                right.append(max(right[-1], pf[i+M] - pf[i]))
+            right = right[1:][::-1]
+            return max(a + b for a, b in zip(left, right))
+        
+        return max(helper(L, M), helper(M, L))
