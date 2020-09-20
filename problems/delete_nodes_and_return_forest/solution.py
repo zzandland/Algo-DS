5d7b9a962baa484e150e3cc0340172b5df31a144
Si Yong Kim <zzandland@gmail.com>
2020-09-20 03:08:30 +0000

    Sync LeetCode submission - Delete Nodes And Return Forest (python3)

--- problems/delete_nodes_and_return_forest/solution.py
+++ problems/delete_nodes_and_return_forest/solution.py
@@ -6,21 +6,14 @@
 #         self.right = right
 class Solution:
     def delNodes(self, root: TreeNode, to_delete: List[int]) -> List[TreeNode]:
-        if not root: return []
-        st, res = set(to_delete), set([root])
-        def dfs(n: TreeNode, p: TreeNode) -> None:
-            nonlocal res
-            if not n: return
-            l, r = n.left, n.right
-            if n.val in st:
-                if p:
-                    if n == p.left: p.left = None
-                    else: p.right = None
-                if n in res: res.remove(n)
-                if n.left: res.add(l)
-                if n.right: res.add(r)
-                n.left = n.right = None
-            dfs(l, n)
-            dfs(r, n)
-        dfs(root, None)
-        return list(res)
+        ts = {*to_delete}
+        res = []
+        def dfs(n: TreeNode, is_root: bool) -> TreeNode:
+            if not n: return None
+            found = n.val in ts
+            if is_root and not found: res.append(n)
+            n.left = dfs(n.left, found)
+            n.right = dfs(n.right, found)
+            return None if found else n
+        dfs(root, True)
+        return res
