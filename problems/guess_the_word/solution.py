fe2f461b1489632225d396a07aa3ecafcf96bcdb
Si Yong Kim <zzandland@gmail.com>
2020-09-20 00:03:34 +0000

    Sync LeetCode submission - Guess the Word (python3)

--- problems/guess_the_word/solution.py
+++ problems/guess_the_word/solution.py
@@ -1,6 +1,3 @@
-from collections import defaultdict
-import heapq
-
 # """
 # This is Master's API interface.
 # You should not implement it, or speculate about its implementation
@@ -10,34 +7,15 @@ import heapq
 
 class Solution:
     def findSecretWord(self, wordlist: List[str], master: 'Master') -> None:
-        W, st = len(wordlist), set(wordlist)
-        diff, hp = {}, []
-        def cntDiff(s1: str, s2: str) -> int:
-            return sum([1 for i in range(6) if s1[i] == s2[i]])
-        for i, w1 in enumerate(wordlist):
-            for j in range(i):
-                w2 = wordlist[j]
-                diff.setdefault(w1, defaultdict(set))
-                diff.setdefault(w2, defaultdict(set))
-                cnt = cntDiff(w1, w2)
-                diff[w1][cnt].add(w2)
-                diff[w2][cnt].add(w1)
-                heapq.heappush(hp, (-cnt, w1))
-                heapq.heappush(hp, (-cnt, w2))
-        useHp = True        
-        for _ in range(10):
-            if useHp:
-                ct, w = heapq.heappop(hp)
-                while w not in st:
-                    ct, w = heapq.heappop(hp)
-                if ct == 0: useHp = False   
-            else: w = random.choice(list(st))        
-            cnt = master.guess(w)
-            if cnt == 6: break
-            st.remove(w)
-            if cnt > 0:
-                for i in range(cnt):
-                    st -= diff[w][i]
-            else:
-                for i in range(1, 7):
-                    st -= diff[w][i]        
+        match = lambda a, b: len([1 for ca, cb in zip(a, b) if ca == cb])
+        while True:
+            ws = {*wordlist}
+            t = random.choice(wordlist)
+            guess = master.guess(t)
+            print(guess, len(wordlist))
+            if guess == 6: break
+            for w in wordlist:
+                m = match(t, w)
+                if guess == -1 and m != 0: ws.remove(w)
+                elif m != guess: ws.remove(w)
+            wordlist = list(ws)
