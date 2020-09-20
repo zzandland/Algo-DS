ceceb88f66b8f2d214ce0ac742df963c282f6e64
Si Yong Kim <zzandland@gmail.com>
2020-09-20 00:59:16 +0000

    Sync LeetCode submission - Verifying an Alien Dictionary (python3)

--- problems/verifying_an_alien_dictionary/solution.py
+++ problems/verifying_an_alien_dictionary/solution.py
@@ -1,13 +1,10 @@
 class Solution:
     def isAlienSorted(self, words: List[str], order: str) -> bool:
-        W = len(words)
-        dic = {c: i for i, c in enumerate(order)}
-        for i in range(1, W):
-            a, b, found = words[i-1], words[i], False
-            for j in range(min(len(a), len(b))):
-                if dic[a[j]] > dic[b[j]]: return False
-                if dic[a[j]] < dic[b[j]]: 
-                    found = True
-                    break
-            if not found and j == len(b)-1: return False        
-        return True    
+        idx = {h: i for i, h in enumerate(order)}
+        for a, b in zip(words, words[1:]):
+            for ca, cb in zip(a, b):
+                if idx[ca] > idx[cb]: return False
+                if idx[ca] < idx[cb]: break
+            else:
+                if len(a) > len(b): return False
+        return True
