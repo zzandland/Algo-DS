2709159d72c63b174688d3b59395a8a30cab229c
Si Yong Kim <zzandland@gmail.com>
2020-09-20 00:49:50 +0000

    Sync LeetCode submission - Range Sum of BST (python3)

--- problems/range_sum_of_bst/solution.py
+++ problems/range_sum_of_bst/solution.py
@@ -8,6 +8,6 @@ class Solution:
     def rangeSumBST(self, root: TreeNode, L: int, R: int) -> int:
         if not root: return 0
         res = root.val if L <= root.val <= R else 0
-        if L < root.val: res += self.rangeSumBST(root.left, L, R)
+        if root.val > L: res += self.rangeSumBST(root.left, L, R)
         if root.val < R: res += self.rangeSumBST(root.right, L, R)
         return res
