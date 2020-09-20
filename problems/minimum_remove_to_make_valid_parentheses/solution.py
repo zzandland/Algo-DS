4d4f8047f4b3e3dc8e0418c652eae0975aa1f54a
Si Yong Kim <zzandland@gmail.com>
2020-09-20 03:35:37 +0000

    Sync LeetCode submission - Minimum Remove to Make Valid Parentheses (python3)

--- problems/minimum_remove_to_make_valid_parentheses/solution.py
+++ problems/minimum_remove_to_make_valid_parentheses/solution.py
@@ -1,21 +1,25 @@
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
-        cnt, tmp, res, S = 0, [], [], len(s)
-        for c in s:
-            if c == ')':
-                if cnt > 0:
-                    tmp.append(c)
-                    cnt -= 1
-            else:
-                if c == '(': cnt += 1
-                tmp.append(c)
-        cnt = 0        
-        for c in tmp[::-1]:        
-            if c == '(':
-                if cnt > 0:
-                    res.append(c)
-                    cnt -= 1
-            else:
-                if c == ')': cnt += 1
-                res.append(c)
-        return ''.join(res[::-1])
+        N = len(s)
+        tmp = [True]*N
+        def helper(front: bool):
+            ran = range(N) if front else range(N-1, -1, -1)
+            cnt = 0
+            for i in ran:
+                c = s[i]
+                if front:
+                    if c == '(': cnt += 1
+                    elif c == ')':
+                        if cnt == 0: tmp[i] = False
+                        else: cnt -= 1
+                else:
+                    if c == ')': cnt += 1
+                    elif c == '(':
+                        if cnt == 0: tmp[i] = False
+                        else: cnt -= 1
+        helper(True)
+        helper(False)
+        res = []
+        for i in range(N):
+            if tmp[i]: res.append(s[i])
+        return ''.join(res)
