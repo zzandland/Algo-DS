8679b26d6482b7239b2cf69e350f42719ee70bdc
Si Yong Kim <zzandland@gmail.com>
2020-12-07 21:50:28 +0000

    Sync LeetCode submission - Spiral Matrix II (cpp)

--- problems/spiral_matrix_ii/solution.cpp
+++ problems/spiral_matrix_ii/solution.cpp
@@ -1,69 +1,22 @@
 class Solution {
 public:
-  void right(vector<vector<int>> &matrix, int y, int x, int num) {
-    matrix[y][x] = ++num;
-    while (x < matrix[0].size() - 1 && !matrix[y][x + 1]) {
-      matrix[y][++x] = ++num;
-      if (x == matrix[0].size() - 1) {
-        break;
-      }
+    vector<vector<int>> generateMatrix(int n) {
+        vector<vector<int>> res(n, vector<int>(n, -1));
+        int y, x, d, ny, nx;
+        y = x = d = 0;
+        pair<int, int> dir[4] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        for (int i = 1; i <= n*n; ++i) {
+            res[y][x] = i;
+            ny = y + dir[d].first;
+            nx = x + dir[d].second;
+            if (0 > ny || ny >= n || 0 > nx || nx >= n || res[ny][nx] != -1) {
+                d = (d+1) % 4;
+                ny = y + dir[d].first;
+                nx = x + dir[d].second;
+            }
+            y = ny;
+            x = nx;
+        }
+        return res;
     }
-    if (matrix[y + 1][x] == 0) {
-      ++y;
-      down(matrix, y, x, num);
-    }
-  }
-
-  void down(vector<vector<int>> &matrix,  int y, int x, int num) {
-    matrix[y][x] = ++num;
-    while (y < matrix[0].size() - 1 && !matrix[y + 1][x]) {
-      matrix[++y][x] = ++num;
-      if (y == matrix[0].size() - 1) {
-        break;
-      }
-    }
-    if (matrix[y][x - 1] == 0) {
-      --x;
-      left(matrix, y, x, num);
-    }
-  }
-
-  void left(vector<vector<int>> &matrix,  int y, int x, int num) {
-    matrix[y][x] = ++num;
-    while (x > 0 && !matrix[y][x - 1]) {
-      matrix[y][--x] = ++num;
-      if (x == 0) {
-        break;
-      }
-    }
-    if (matrix[y - 1][x] == 0) {
-      --y;
-      up(matrix, y, x, num);
-    }
-  }
-
-  void up(vector<vector<int>> &matrix,  int y, int x, int num) {
-    matrix[y][x] = ++num;
-    while (y > 0 && !matrix[y - 1][x]) {
-      matrix[--y][x] = ++num;
-      if (y == 0) {
-        break;
-      }
-    }
-    if (matrix[y][x + 1] == 0) {
-      ++x;
-      right(matrix, y, x, num);
-    }
-  }
-
-  vector<vector<int>> generateMatrix(int n) {
-    vector<vector<int>> matrix(n, vector<int>(n, 0));
-    int num = 0;
-    if (n == 1) {
-      matrix[0][0] = 1;
-    } else if (n > 1) {
-      right(matrix, 0, 0, num);
-    }
-    return matrix;
-  }
 };
