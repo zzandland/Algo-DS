97c03777b6a25b0d2876e447de113d54fdee28ac
Si Yong Kim <zzandland@gmail.com>
2021-01-12 18:01:31 +0000

    Sync LeetCode submission - Add Two Numbers (cpp)

--- problems/add_two_numbers/solution.cpp
+++ problems/add_two_numbers/solution.cpp
@@ -3,39 +3,33 @@
  * struct ListNode {
  *     int val;
  *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
 class Solution {
 public:
-  ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-    bool roof = false;
-    ListNode *r1 = l1, *r2 = l2, *r = nullptr, *head = nullptr;
-    while (r1 != nullptr || r2 != nullptr) {
-      int val1 = 0, val2 = 0, up = roof ? 1 : 0;
-      if (r1 != nullptr) {
-        val1 = r1->val;
-        r1 = r1->next;
-      }
-      if (r2 != nullptr) {
-        val2 = r2->val;
-        r2 = r2->next;
-      }
-      int total = val1 + val2 + up;
-      if (total < 10)
-        roof = false;
-      else {
-        roof = true;
-        total -= 10;
-      }
-      ListNode* tmp = new ListNode(total);
-      if (r != nullptr) {
-        r->next = tmp;
-      }
-      r = tmp;
-      if (head == nullptr) head = r;
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* dummy = new ListNode();
+        ListNode* run = dummy;
+        int lift = 0;
+        while (l1 || l2) {
+            int t = lift;
+            if (l1) {
+                t += l1->val;
+                l1 = l1->next;
+            }
+            if (l2) {
+                t += l2->val;
+                l2 = l2->next;
+            }
+            lift = t / 10;
+            t %= 10;
+            run->next = new ListNode(t);
+            run = run->next;
+        }
+        if (lift) run->next = new ListNode(lift);
+        return dummy->next;
     }
-    if (roof) r->next = new ListNode(1);
-    return head;
-  }
 };
