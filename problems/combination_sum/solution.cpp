1aa384bff9dc921dec38aaa19be569e03dcc105d
Si Yong Kim <zzandland@gmail.com>
2021-02-16 08:58:00 +0000

    Sync LeetCode submission - Combination Sum (cpp)

--- problems/combination_sum/solution.cpp
+++ problems/combination_sum/solution.cpp
@@ -1,77 +1,24 @@
-/*
- * @lc app=leetcode id=39 lang=cpp
- *
- * [39] Combination Sum
- *
- * https://leetcode.com/problems/combination-sum/description/
- *
- * algorithms
- * Medium (48.54%)
- * Total Accepted:    343.4K
- * Total Submissions: 707.2K
- * Testcase Example:  '[2,3,6,7]\n7'
- *
- * Given a set of candidate numbers (candidates) (without duplicates) and a
- * target number (target), find all unique combinations in candidates where the
- * candidate numbers sums to target.
- *
- * The same repeated number may be chosen from candidates unlimited number of
- * times.
- *
- * Note:
- *
- *
- * All numbers (including target) will be positive integers.
- * The solution set must not contain duplicate combinations.
- *
- *
- * Example 1:
- *
- *
- * Input: candidates = [2,3,6,7], target = 7,
- * A solution set is:
- * [
- * ⁠ [7],
- * ⁠ [2,2,3]
- * ]
- *
- *
- * Example 2:
- *
- *
- * Input: candidates = [2,3,5], target = 8,
- * A solution set is:
- * [
- * [2,2,2,2],
- * [2,3,3],
- * [3,5]
- * ]
- *
- *
- */
 class Solution {
 public:
-  vector<vector<int>> combinationSum(vector<int> &candidates, int target) {
-    vector<vector<int>> output;
-    vector<int> v;
-    recurse(candidates, v, target, 0, 0, output);
-    return output;
-  }
-
-  void recurse(vector<int> &candidates, vector<int> &v, int target, int i,
-               int sum, vector<vector<int>> &output) {
-    if (i >= candidates.size())
-      return;
-    if (sum == target) {
-      vector<int> tmp(v.begin(), v.end());
-      output.push_back(tmp);
-      return;
+    vector<int> tmp;
+    
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+        vector<vector<int>> res;
+        dfs(0, candidates, target, res);
+        return res;
     }
-    if (sum + candidates[i] <= target) {
-      v.push_back(candidates[i]);
-      recurse(candidates, v, target, i, sum + candidates[i], output);
-      v.pop_back();
+    
+    void dfs(int i, vector<int> &candidates, int n, vector<vector<int>> &res) {
+        if (n < 0 || i == candidates.size()) return;
+        if (n == 0) {
+            vector<int> cpy = tmp;
+            res.push_back(cpy);
+            return;
+        }
+        tmp.push_back(candidates[i]);
+        dfs(i, candidates, n - candidates[i], res);
+        tmp.pop_back();
+        dfs(i+1, candidates, n, res);
     }
-    recurse(candidates, v, target, i + 1, sum, output);
-  }
 };
