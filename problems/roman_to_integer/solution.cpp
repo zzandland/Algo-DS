ab943262f459e9792efa4406efdd69f2c8c49d75
Si Yong Kim <zzandland@gmail.com>
2021-02-20 19:03:36 +0000

    Sync LeetCode submission - Roman to Integer (cpp)

--- problems/roman_to_integer/solution.cpp
+++ problems/roman_to_integer/solution.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
-  int romanToInt(string s) {
-    int output = 0;
-    map<char, int> roman = {{'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}};
-    for (int i = 0; i < (int)s.length(); ++i) {
-      if (i + 1 < (int)s.length() && roman[s[i + 1]] > roman[s[i]])
-        output -= roman[s[i]];
-      else
-        output += roman[s[i]];
+    int romanToInt(string s) {
+        int res = 0;
+        unordered_map<char, int> d = { {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000} };
+        for (char c : s) res += d[c];
+        for (int i = 0; i < s.length()-1; ++i) {
+            string ss = s.substr(i, 2);
+            if (ss == "IV" || ss == "IX") res -= 2;
+            else if (ss == "XL" || ss == "XC") res -= 20;
+            else if (ss == "CD" || ss == "CM") res -= 200;
+        }
+        return res;
     }
-    return output;
-  }
 };
