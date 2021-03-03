8edb07d69e80a0e522450cc86db532ca17253d26
Si Yong Kim <zzandland@gmail.com>
2021-03-03 17:46:14 +0000

    Sync LeetCode submission - Missing Number (cpp)

--- problems/missing_number/solution.cpp
+++ problems/missing_number/solution.cpp
@@ -1,24 +1,11 @@
-#include <set>
-
 class Solution {
 public:
-  int missingNumber(vector<int>& nums) {
-    int arr_sum = 0;
-    int greatest = INT_MIN;
-    bool zero = false;
-    for (int num : nums) {
-      if (num == 0) zero = true;
-      arr_sum += num;
-      greatest = max(greatest, num);
+    int missingNumber(vector<int>& nums) {
+        int res = nums.size();
+        for (int i = 0; i < nums.size(); ++i) {
+            res ^= nums[i];
+            res ^= i;
+        }
+        return res;
     }
-    int sum = getIncrementalSum(greatest);
-    return (zero && sum == arr_sum) ? greatest + 1 : sum - arr_sum;
-  }
-  
-  int getIncrementalSum(int num) {
-    int output = 0;
-    for (int i = 1; i <= num; ++i)
-      output += i;
-    return output;
-  }
 };
