b6b484073120df2796ace3447394abc95475b1a1
Si Yong Kim <zzandland@gmail.com>
2021-03-11 00:59:27 +0000

    Sync LeetCode submission - Integer to Roman (cpp)

--- problems/integer_to_roman/solution.cpp
+++ problems/integer_to_roman/solution.cpp
@@ -1,18 +1,28 @@
 class Solution {
 public:
-  string intToRoman(int num) {
-    vector<int> nums = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-    vector<string> letters = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-    int i = 0;
-    string output = "";
-    while (i < (int)nums.size() && num > 0) {
-      if (num >= nums[i]) {
-        output += letters[i];
-        num -= nums[i];
-      } else {
-        ++i;
-      }
+    string intToRoman(int num) {
+        string res;
+        helper(num, res, 1000, "M");
+        helper(num, res, 900, "CM");
+        helper(num, res, 500, "D");
+        helper(num, res, 400, "CD");
+        helper(num, res, 100, "C");
+        helper(num, res, 90, "XC");
+        helper(num, res, 50, "L");
+        helper(num, res, 40, "XL");
+        helper(num, res, 10, "X");
+        helper(num, res, 9, "IX");
+        helper(num, res, 5, "V");
+        helper(num, res, 4, "IV");
+        helper(num, res, 1, "I");
+        return res;
     }
-    return output;
-  }
+    
+    string helper(int& num, string& res, int val, string add) {
+        while (num >= val) {
+            num -= val;
+            res += add;
+        }
+        return res;
+    }
 };
