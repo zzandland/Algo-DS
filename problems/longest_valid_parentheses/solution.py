a1bba39919273299b789ac75e8ffa69f6485aedc
Si Yong Kim <zzandland@gmail.com>
2021-04-04 03:45:02 +0000

    Sync LeetCode submission - Longest Valid Parentheses (python3)

--- problems/longest_valid_parentheses/solution.py
+++ problems/longest_valid_parentheses/solution.py
@@ -1,10 +1,13 @@
 class Solution:
     def longestValidParentheses(self, s: str) -> int:
-        S, mx, st = len(s), 0, [-1]
+        res, st = 0, [-1]
         for i, c in enumerate(s):
-            if c == '(': st.append(i)
+            if c == '(':
+                st.append(i)
             else:
-                last = st.pop()
-                if not st: st.append(i)
-                else: mx = max(mx, i - st[-1])
-        return mx
+                lst = st.pop()
+                if not st: 
+                    st.append(i)
+                else:
+                    res = max(res, i - st[-1])
+        return res
