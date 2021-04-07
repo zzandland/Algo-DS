28a62f8d636f6453a3186172a5824e159ab1eb1b
Si Yong Kim <zzandland@gmail.com>
2021-04-07 18:02:30 +0000

    Sync LeetCode submission - String to Integer (atoi) (javascript)

--- problems/string_to_integer_(atoi)/solution.js
+++ problems/string_to_integer_(atoi)/solution.js
@@ -1,12 +1,24 @@
 /**
- * @param {string} str
+ * @param {string} s
  * @return {number}
  */
-var myAtoi = function(str) {
-  var regex = /^\s*[-+]?\d+/;
-  if (!regex.exec(str)) return 0;
-  var num = parseInt(regex.exec(str)[0]);
-  if (num > Math.pow(2, 31) - 1) return Math.pow(2, 31) - 1;
-  if (num < Math.pow(2, 31) * -1) return Math.pow(2, 31) * -1;
-  return num;
+var myAtoi = function(s) {
+    let i = 0;
+    let res = [];
+    let neg = false;
+    while (s[i] === ' ') ++i;
+    if (s[i] === '+' || s[i] === '-') {
+        if (s[i] === '-') neg = true;
+        ++i;
+    }
+    while (s[i] >= '0' && s[i] <= '9') {
+        res.push(s[i++]);
+    }
+    if (!res.length) return 0;
+    let n = parseInt(res.join(''), 10) * (neg ? -1 : 1);
+    if (n >= 0) {
+        const MAX = Math.pow(2, 31) - 1;
+        return Math.min(MAX, n);
+    }
+    return Math.max(Math.pow(2, 31) * -1, n);
 };
