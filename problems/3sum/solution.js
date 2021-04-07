527e6954e568c23ba9e03f594cde7b5a6906ed0a
Si Yong Kim <zzandland@gmail.com>
2021-04-07 18:39:41 +0000

    Sync LeetCode submission - 3Sum (javascript)

--- problems/3sum/solution.js
+++ problems/3sum/solution.js
@@ -3,49 +3,22 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-  var output = [];
-  var temp;
-  var sorted;
-  for (var i = 0; i < nums.length; i++) {
-    sorted = true;
-    for (var j = 0; j < nums.length - 1; j++) {
-      if (nums[j] > nums[j + 1]) {
-        temp = nums[j + 1];
-        nums[j + 1] = nums[j];
-        nums[j] = temp;
-        sorted = false;
-      }
+    nums.sort((a, b) => a - b);
+    res = [];
+    for (let i = 0; i < nums.length - 2; ++i) {
+        if (i > 0 && nums[i] == nums[i-1]) continue;
+        let l = i + 1, r = nums.length - 1, t = -nums[i];
+        while (l < r) {
+            let t2 = nums[l] + nums[r];
+            if (t2 === t) {
+                res.push([-t, nums[l++], nums[r]]);  
+                while (l < r && nums[l] === nums[l-1]) ++l;
+            } else if (t2 >= t) {
+                --r;  
+            } else {
+                ++l;  
+            } 
+        }
     }
-    if (sorted) break;
-  }
-  var target, left, right, sum, subArr;
-  for (var j = 0; j < nums.length - 2; j++) {
-    if (nums[j] === nums[j - 1] && j < nums.length - 2) {
-      continue;
-    }
-    target = 0 - nums[j];
-    left = j + 1; 
-    right = nums.length - 1;
-    while (right > left) {
-      if (nums[left] === nums[left - 1] && left < nums.length - 1 && left - 1 !== j) {
-        left++;
-        continue;
-      }
-      if (nums[right] === nums[right + 1] && right > left) {
-        right--;
-        continue;
-      }
-      sum = nums[left] + nums[right];
-      if (sum === target) {
-        subArr = [nums[j], nums[left], nums[right]];
-        output.push(subArr);
-        left++;
-      } else if (sum > target) {
-        right--;
-      } else {
-        left++;
-      }
-    }
-  }
-  return output;
+    return res;
 };
