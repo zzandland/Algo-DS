b980e80d265400d54d4e321bf0c383fe366aabed
Si Yong Kim <zzandland@gmail.com>
2021-04-07 23:28:50 +0000

    Sync LeetCode submission - Rotate Image (javascript)

--- problems/rotate_image/solution.js
+++ problems/rotate_image/solution.js
@@ -3,17 +3,15 @@
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
 var rotate = function(matrix) {
-  if (matrix.length === 0 || matrix.length !== matrix[0].length) return false;
-  for (var layer = 0; layer < matrix.length / 2; layer++) {
-    var first = layer;
-    var last = matrix.length - 1 - first;
-    for (var i = first; i < last; i++) {
-      var offset = i - first;
-      var top = matrix[first][i];
-      matrix[first][i] = matrix[last - offset][first];
-      matrix[last - offset][first] = matrix[last][last - offset];
-      matrix[last][last - offset] = matrix[i][last];
-      matrix[i][last] = top;
+    N = matrix.length;
+    for (let i = 0; i < Math.floor(N / 2); ++i) {
+        for (let j = i; j < N - i - 1; ++j) {
+            let offset = N-1-i;
+            let tmp = matrix[i][j];
+            matrix[i][j] = matrix[offset-j+i][i];
+            matrix[offset-j+i][i] = matrix[offset][offset-j+i];
+            matrix[offset][offset-j+i] = matrix[j][offset];
+            matrix[j][offset] = tmp;
+        }
     }
-  }
 };
