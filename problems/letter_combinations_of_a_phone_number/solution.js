f36b61a71ee8783e13dacecbbe835665260f7345
Si Yong Kim <zzandland@gmail.com>
2021-04-07 18:46:24 +0000

    Sync LeetCode submission - Letter Combinations of a Phone Number (javascript)

--- problems/letter_combinations_of_a_phone_number/solution.js
+++ problems/letter_combinations_of_a_phone_number/solution.js
@@ -2,35 +2,30 @@
  * @param {string} digits
  * @return {string[]}
  */
-const numbers = {
-  2: ['a', 'b', 'c'],
-  3: ['d', 'e', 'f'],
-  4: ['g', 'h', 'i'],
-  5: ['j', 'k', 'l'],
-  6: ['m', 'n', 'o'],
-  7: ['p', 'q', 'r', 's'],
-  8: ['t', 'u', 'v'],
-  9: ['w', 'x', 'y', 'z'],
-}
-
 var letterCombinations = function(digits) {
-  const output = [];
-  if (digits === '') return output;
-  helper(output, digits, [], 0);
-  return output;
+    if (!digits) return [];
+    const lts = {
+        '2': ['a', 'b', 'c'],
+        '3': ['d', 'e', 'f'],
+        '4': ['g', 'h', 'i'],
+        '5': ['j', 'k', 'l'],
+        '6': ['m', 'n', 'o'],
+        '7': ['p', 'q', 'r', 's'],
+        '8': ['t', 'u', 'v'],
+        '9': ['w', 'x', 'y', 'z'],
+    }
+    let st = [], res = [];
+    const helper = i => {
+        if (i === digits.length) {
+            res.push(st.join(''));
+            return;
+        }
+        for (let c of lts[digits[i]]) {
+            st.push(c);
+            helper(i+1);
+            st.pop();
+        }
+    }
+    helper(0);
+    return res;
 };
-
-function helper(output, digits, arr, index) {
-  if (index === digits.length) {
-    output.push(arr.join(''));
-    return;
-  }
-  const chars = numbers[digits[index]];
-  for (let i = 0; i < chars.length; i++) {
-    arr.push(chars[i]);
-    index++;
-    helper(output, digits, arr, index);
-    arr.pop();
-    index--;
-  }
-}
