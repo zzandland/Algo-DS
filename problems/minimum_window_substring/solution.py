bfb3c3d98b58f184c28c3213f3f0b327cbcfa72a
Si Yong Kim <zzandland@gmail.com>
2021-04-08 03:56:58 +0000

    Sync LeetCode submission - Minimum Window Substring (python3)

--- problems/minimum_window_substring/solution.py
+++ problems/minimum_window_substring/solution.py
@@ -1,20 +1,26 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        T = Counter(t)
-        S = Counter()
-        def comp() -> bool:
-            for c in T:
-                if c not in S or S[c] < T[c]: return False
-            return True
-        l = 0
-        res = s + 'A'
-        for r, rc in enumerate(s):
-            S[rc] += 1
-            while comp():
-                res = min(res, s[l:r+1], key=len)
-                lc = s[l]
-                S[lc] -= 1
-                if not S[lc]: del S[lc]
+        dic = Counter()
+        t_dic = Counter(t)
+        required = len(t_dic)
+        
+        l = cnt = 0
+        mn = float('inf')
+        res = ''
+        
+        for r in range(len(s)):
+            c = s[r]
+            dic[c] += 1
+            if c in t_dic and dic[c] == t_dic[c]:
+                cnt += 1
+            while required == cnt:
+                if mn > r - l + 1:
+                    mn = r - l + 1
+                    res = s[l:r+1]
+                c = s[l]
+                dic[c] -= 1
+                if c in t_dic and dic[c] < t_dic[c]:
+                    cnt -= 1
                 l += 1
                 
-        return res if res != s + 'A' else ''
+        return res if mn != float('inf') else ''
