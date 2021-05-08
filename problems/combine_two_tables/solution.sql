b4875cc7725f12ca8a69396d5873f30329d0449c
Si Yong Kim <zzandland@gmail.com>
2021-05-08 04:36:03 +0000

    Sync LeetCode submission - Combine Two Tables (mysql)

--- problems/combine_two_tables/solution.sql
+++ problems/combine_two_tables/solution.sql
@@ -1,2 +1,8 @@
 # Write your MySQL query statement below
-select t1.FirstName,t1.LastName,t2.City,t2.State from Person as t1 left join Address as t2 on t1.PersonId = t2.PersonId;
+SELECT
+Person.FirstName,
+Person.LastName,
+Address.City,
+Address.State
+FROM
+Person LEFT JOIN Address ON Person.PersonId = Address.PersonId;
