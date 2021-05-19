dfdc070e44d9e1009acce010329e3bda674dfe42
Si Yong Kim <zzandland@gmail.com>
2021-05-19 00:55:09 +0000

    Sync LeetCode submission - Find Duplicate File in System (python3)

--- problems/find_duplicate_file_in_system/solution.py
+++ problems/find_duplicate_file_in_system/solution.py
@@ -1,11 +1,9 @@
-from collections import defaultdict
-
 class Solution:
     def findDuplicate(self, paths: List[str]) -> List[List[str]]:
         dic = defaultdict(list)
         for path in paths:
-            dir_, files = path.split(' ', 1)
+            dir, files = path.split(' ', 1)
             for file in files.split(' '):
-                name, content = file.split('(')
-                dic[content[:-1]].append(dir_ + '/' + name)
-        return filter(lambda x: len(x) > 1, dic.values())
+                name, content = file.split('(', 1)
+                dic[content].append(dir + '/' + name)
+        return [lst for lst in dic.values() if len(lst) > 1]
