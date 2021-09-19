9b5e30c2f3a7266a6ee7adc40cda0ac8edc612f5
Si Yong Kim <zzandland@gmail.com>
2021-09-19 22:01:38 +0000

    Sync LeetCode submission - Distinct Subsequences (cpp)

--- problems/distinct_subsequences/solution.cpp
+++ problems/distinct_subsequences/solution.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     int numDistinct(string s, string t) {
         int S = s.length(), T = t.length();
-        vector<vector<unsigned long>> dp(T+1, vector<unsigned long>(S+1, 0));
+        vector<vector<unsigned>> dp(T + 1, vector<unsigned>(S + 1, 0));
         for (int i = 0; i <= S; ++i) {
-            dp[T][i] = 1;
+            dp[0][i] = 1;
         }
-        for (int i = T-1; i >= 0; --i) {
-            for (int j = S-1; j >= 0; --j) {
-                dp[i][j] = dp[i][j+1];
-                if (s[j] == t[i]) dp[i][j] += dp[i+1][j+1];
+        for (int y = 1; y <= T; ++y) {
+            for (int x = 1; x <= S; ++x) {
+                if (t[y-1] == s[x-1]) dp[y][x] += dp[y-1][x-1];
+                dp[y][x] += dp[y][x-1];
             }
         }
-        return dp[0][0];
+        return dp[T][S];
     }
-};
+};    
